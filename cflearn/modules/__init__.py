216a4be00628d3d58bc965ec271d4257d45b3c81
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-25 12:46:14 +0800

    🎨Expose modules in __init__

---
+++ cflearn/modules/__init__.py
@@ -0,0 +1,12 @@
+from .auxiliary import EMA
+from .encoders import *
+from .optimizers import *
+from .schedulers import *
+
+
+__all__ = [
+    "EMA",
+    "EncoderBase", "EncoderStack", "encoder_dict",
+    "optimizer_dict", "register_optimizer",
+    "scheduler_dict", "register_scheduler"
+]
