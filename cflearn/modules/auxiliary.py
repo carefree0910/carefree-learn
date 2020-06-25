c4c0ab911e1f986456f9b973c829453aded2991a
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-25 11:49:52 +0800

    ✨Re-Implemented `BN`
    For 3-dimensional input, (B, T, D) is more user friendly than (B, D, T)

--- cflearn/modules/auxiliary.py
+++ cflearn/modules/auxiliary.py
@@ -3,3 +3,13 @@ import torch.nn as nn
 
 from functools import partial
 from torch.nn.functional import softplus
+
+
+class BN(nn.BatchNorm1d):
+    def forward(self, net):
+        if len(net.shape) == 3:
+            net = net.transpose(1, 2)
+        net = super().forward(net)
+        if len(net.shape) == 3:
+            net = net.transpose(1, 2)
+        return net
