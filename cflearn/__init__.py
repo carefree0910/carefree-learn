dfac62537e040f96eba3599233d77f2ee273de4c
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-25 13:04:31 +0800

    ✨Implemented `register_initializer`

--- cflearn/__init__.py
+++ cflearn/__init__.py
@@ -13,3 +13,12 @@ from .bases import *
 from .models import *
 from .modules import *
 from .misc.toolkit import eval_context, Initializer
+
+
+# register
+
+def register_initializer(name):
+    def _register(f):
+        Initializer.add_initializer(f, name)
+        return f
+    return _register
