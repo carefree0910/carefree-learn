388ea66d75d08ac8bd8b6acba54fe364bf56fbbc
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-25 11:43:02 +0800

    ✨Implemented `to_torch`, `to_numpy`

--- cflearn/misc/toolkit.py
+++ cflearn/misc/toolkit.py
@@ -8,3 +8,11 @@ import torch.nn as nn
 from typing import *
 
 from cftool.misc import *
+
+
+def to_torch(arr: np.ndarray) -> torch.Tensor:
+    return torch.from_numpy(arr.astype(np.float32))
+
+
+def to_numpy(tensor: torch.Tensor) -> np.ndarray:
+    return tensor.detach().cpu().numpy()
