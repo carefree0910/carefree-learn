c6239ec044fd17a46ec48638347fc19694d73db6
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-25 13:04:06 +0800

    🎉Introduced `cflearn`

---
+++ cflearn/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from typing import *
+from cftool.misc import *
+from cftool.ml.utils import *
+from cftool.ml.param_utils import *
+from cfdata.tabular import *
+from functools import partial
+from cftool.ml.hpo import HPOBase
+
+from .dist import *
+from .bases import *
+from .models import *
+from .modules import *
+from .misc.toolkit import eval_context, Initializer
