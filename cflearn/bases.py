baec2c2389c6706a91e47a8b0faab372d7d377b6
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-25 12:50:30 +0800

    ✨Implemented `Pipeline`
    In `carefree-learn`, `Pipeline` is responsible to construct the training loop

--- cflearn/bases.py
+++ cflearn/bases.py
@@ -184,3 +184,398 @@ class ModelBase(nn.Module, LoggingMixin, metaclass=ABCMeta):
         global model_dict
         def before(cls_): cls_.__identifier__ = name
         return register_core(name, model_dict, before_register=before)
+
+
+class Pipeline(nn.Module, LoggingMixin):
+    def __init__(self,
+                 model: ModelBase,
+                 wrapper_config: Dict[str, Any],
+                 verbose_level: int):
+        super().__init__()
+        self._init_config(wrapper_config)
+        self.model = model
+        self._verbose_level = verbose_level
+
+    def _init_config(self, wrapper_config):
+        self._wrapper_config = wrapper_config
+        self.config = wrapper_config.setdefault("pipeline_config", {})
+        self.batch_size = self.config.setdefault("batch_size", 128)
+        self.cv_batch_size = self.config.setdefault("cv_batch_size", 5 * self.batch_size)
+        self.use_tqdm = self.config.setdefault("use_tqdm", True)
+        self.min_epoch = int(self.config.setdefault("min_epoch", 0))
+        self.num_epoch = int(self.config.setdefault("num_epoch", max(40, self.min_epoch)))
+        self.max_epoch = int(self.config.setdefault("max_epoch", max(200, self.num_epoch)))
+        self.snapshot_start_step = int(self.config.setdefault("snapshot_start_step", 0))
+        self._num_step_per_snapshot = int(self.config.setdefault("num_step_per_snapshot", 0))
+        self.num_snapshot_per_epoch = int(self.config.setdefault("num_snapshot_per_epoch", 2))
+        self.max_step_per_snapshot = int(self.config.setdefault("max_step_per_snapshot", 1000))
+        self.plateau_monitor_start_snapshot = int(
+            self.config.setdefault("plateau_monitor_start_snapshot", self.num_epoch))
+
+        self._clip_norm = self.config.setdefault("clip_norm", 0.)
+        ema_decay = self.config.setdefault("ema_decay", 0.)
+        if not 0. < ema_decay < 1.:
+            self.ema_decay = None
+        else:
+            self.ema_decay = EMA(ema_decay, self.model.named_parameters())
+
+        self._logging_path_ = wrapper_config["logging_path"]
+        self.log_folder = self.config.setdefault("log_folder", os.path.split(self._logging_path_)[0])
+        self.checkpoint_folder = self.config.setdefault(
+            "checkpoint_folder",
+            os.path.join(self.log_folder, "checkpoints")
+        )
+
+    def _init_data(self, tr_data, cv_data):
+        self.tr_data, self.cv_data = tr_data, cv_data
+        tr_sampler = ImbalancedSampler(tr_data, verbose_level=self._verbose_level)
+        self.tr_loader = DataLoader(self.batch_size, tr_sampler, verbose_level=self._verbose_level)
+        if cv_data is None:
+            self.cv_loader = None
+        else:
+            cv_sampler = ImbalancedSampler(cv_data, shuffle=False, verbose_level=self._verbose_level)
+            self.cv_loader = DataLoader(self.cv_batch_size, cv_sampler, verbose_level=self._verbose_level)
+
+    def _define_optimizer(self, params_name, optimizer_base, optimizer_config):
+        if params_name == "all":
+            parameters = self.model.parameters()
+        else:
+            parameters = getattr(self.model, params_name)
+        opt = self.optimizers[params_name] = optimizer_base(parameters, **optimizer_config)
+        return opt
+
+    def _init_optimizers(self):
+        optimizers = self.config.setdefault("optimizers", {"all": {}})
+        self.optimizers, self.schedulers = {}, {}
+        for params_name, params_optimizer_config in optimizers.items():
+            optimizer = params_optimizer_config.setdefault("optimizer", "adam")
+            optimizer_config = params_optimizer_config.setdefault("optimizer_config", {})
+            scheduler = params_optimizer_config.setdefault("scheduler", "plateau")
+            scheduler_config = params_optimizer_config.setdefault("scheduler_config", {})
+            # optimizer
+            optimizer_config.setdefault("lr", 1e-3)
+            if optimizer == "nag":
+                optimizer_config.setdefault("momentum", 0.999)
+                optimizer_config.setdefault("weight_decay", 1e-7)
+            if scheduler == "warmup":
+                multiplier = scheduler_config.setdefault("multiplier", 3)
+                default_warm_up_step = min(
+                    10 * len(self.tr_loader),
+                    int(0.25 * self.plateau_monitor_start_snapshot * self.num_step_per_snapshot)
+                )
+                warmup_step = scheduler_config.setdefault("warmup_step", default_warm_up_step)
+                self.plateau_monitor_start_snapshot += int(warmup_step / self.num_step_per_snapshot)
+                self.snapshot_start_step += warmup_step
+                optimizer_config["lr"] /= multiplier
+            optimizer_base = optimizer_dict[optimizer] if isinstance(optimizer, str) else optimizer
+            opt = self._define_optimizer(params_name, optimizer_base, optimizer_config)
+            self.config["optimizer_config"] = optimizer_config
+            self._optimizer_type = optimizer
+            # scheduler
+            plateau_default_config: Dict[str, Any] = {"mode": "max"}
+            plateau_default_config.setdefault("verbose", self._verbose_level_ >= 3)
+            plateau_default_config.setdefault(
+                "patience",
+                max(10, self.snapshot_start_step // self.num_step_per_snapshot)
+            )
+            if scheduler == "plateau":
+                scheduler_config = update_dict(scheduler_config, plateau_default_config)
+            elif scheduler == "warmup":
+                scheduler_afterwards_base = scheduler_config.get("scheduler_afterwards_base", "plateau")
+                scheduler_afterwards_config = scheduler_config.get("scheduler_afterwards_config", {})
+                if scheduler_afterwards_base is not None and isinstance(scheduler_afterwards_base, str):
+                    if scheduler_afterwards_base == "plateau":
+                        scheduler_afterwards_config = update_dict(scheduler_afterwards_config, plateau_default_config)
+                    scheduler_afterwards_base = scheduler_dict[scheduler_afterwards_base]
+                scheduler_config["scheduler_afterwards_base"] = scheduler_afterwards_base
+                scheduler_config["scheduler_afterwards_config"] = scheduler_afterwards_config
+            if scheduler is None:
+                self.schedulers[params_name] = None
+            else:
+                if isinstance(scheduler, str):
+                    scheduler = scheduler_dict[scheduler]
+                self.schedulers[params_name] = scheduler(opt, **scheduler_config)
+
+    def _init_metrics(self):
+        # metrics
+        metric_config = self.config.setdefault("metric_config", {})
+        metric_types = metric_config.setdefault("types", "auto")
+        if not isinstance(metric_types, (list, tuple)):
+            metric_types = [metric_types]
+        self._metrics_need_loss = False
+        self.metrics: Dict[str, Union[Metrics, None]] = {}
+        self.metrics_decay: Dict[str, ScalarEMA] = {}
+        metric_decay = metric_config.setdefault("decay", 0.1)
+        for i, metric_type in enumerate(metric_types):
+            if metric_type == "auto":
+                if self.tr_data.is_reg:
+                    metric_type = "mae"
+                else:
+                    metric_type = "auc"
+                metric_types[i] = metric_type
+            if metric_type not in Metrics.sign_dict:
+                # here, sub_metric must be one of the `loss_dict` keys
+                self._metrics_need_loss = True
+                self.metrics[metric_type] = None
+            else:
+                sub_metric_config = metric_config.setdefault(f"{metric_type}_config", {})
+                self.metrics[metric_type] = Metrics(metric_type, sub_metric_config)
+            if not 0. < metric_decay < 1.:
+                self.metrics_decay = None
+            else:
+                self.metrics_decay[metric_type] = ScalarEMA(metric_decay)
+        self.metrics_weights = metric_config.setdefault("weights", {})
+        for metric_type in metric_types:
+            self.metrics_weights.setdefault(metric_type, 1.)
+
+    @property
+    def start_snapshot(self):
+        return self._step_count >= self.snapshot_start_step and self._epoch_count > self.min_epoch
+
+    @property
+    def start_monitor_plateau(self):
+        return self._step_count >= self.plateau_monitor_start_snapshot * self.num_step_per_snapshot
+
+    @property
+    def num_step_per_snapshot(self):
+        if self._num_step_per_snapshot > 0:
+            return self._num_step_per_snapshot
+        return max(1, min(
+            self.max_step_per_snapshot,
+            int(len(self.tr_loader) / self.num_snapshot_per_epoch)
+        ))
+
+    # core
+
+    def _collect_info(self, *, return_only: bool):
+        msg = "\n".join(["=" * 100, "configurations", "-" * 100, ""])
+        msg += pprint.pformat(self._wrapper_config, compact=True) + "\n" + "-" * 100 + "\n"
+        msg += "\n".join(["=" * 100, "parameters", "-" * 100, ""])
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                msg += name + "\n"
+        msg += "\n".join(["-" * 100, "=" * 100, "buffers", "-" * 100, ""])
+        for name, param in self.named_buffers():
+            msg += name + "\n"
+        msg += "\n".join(["-" * 100, "=" * 100, "structure", "-" * 100, str(self), "-" * 100, ""])
+        if not return_only:
+            self.log_block_msg(msg, verbose_level=4)
+        all_msg, msg = msg, "=" * 100 + "\n"
+        n_tr = len(self.tr_data)
+        n_cv = None if self.cv_data is None else len(self.cv_data)
+        msg += f"{self.info_prefix}training data : {n_tr}\n"
+        msg += f"{self.info_prefix}valid    data : {n_cv}\n"
+        msg += "-" * 100
+        if not return_only:
+            self.log_block_msg(msg, verbose_level=3)
+        return "\n".join([all_msg, msg])
+
+    def _to_device(self, arr: Union[np.ndarray, None]) -> Union[torch.Tensor, None]:
+        if arr is None:
+            return arr
+        return torch.from_numpy(arr).to(self.model.device)
+
+    def _collate_batch(self,
+                       x_batch: np.ndarray,
+                       y_batch: np.ndarray) -> Dict[str, torch.Tensor]:
+        tensors = list(map(self._to_device, [x_batch, y_batch]))
+        if y_batch is not None and self.tr_data.is_clf:
+            tensors[-1] = tensors[-1].to(torch.int64)
+        return dict(zip(["x_batch", "y_batch"], tensors))
+
+    def _clip_norm_step(self):
+        self._gradient_norm = torch.nn.utils.clip_grad_norm_(
+            self.model.parameters(), self._clip_norm)
+
+    def _optimizer_step(self):
+        for opt in self.optimizers.values():
+            opt.step()
+            opt.zero_grad()
+
+    def _monitor_step(self):
+        if self._step_count % self.num_step_per_snapshot == 0:
+            score, metrics = self._get_metrics()
+            if self.start_monitor_plateau:
+                if not self._monitor.plateau_flag:
+                    self.log_msg("start monitoring plateau", self.info_prefix, 2)
+                self._monitor.plateau_flag = True
+            if self.start_snapshot:
+                if self._monitor.check_terminate(score):
+                    return True
+                for scheduler in self.schedulers.values():
+                    if scheduler is not None:
+                        scheduler.step(metrics=score)
+        return False
+
+    def _get_metrics(self) -> Tuple[float, Dict[str, float]]:
+        # TODO : check whether restored metrics can correspond to previous metrics
+        tr_loader, cv_loader = self.tr_loader, self.cv_loader
+        if cv_loader is None and self.tr_loader._n_siamese > 1:
+            raise ValueError("cv set should be provided when n_siamese > 1")
+        if cv_loader is not None:
+            loader = cv_loader
+        else:
+            loader = tr_loader.copy()
+            loader.enabled_sampling = False
+        if self.use_tqdm:
+            loader = tqdm(loader, total=len(loader), leave=False, position=2)
+        if not self._metrics_need_loss:
+            losses = None
+            results = self._predict(loader=loader)
+            predictions, labels = map(results.get, ["predictions", "labels"])
+        else:
+            predictions = None
+            forward_dicts, loss_dicts, labels = [], [], []
+            for x_batch, y_batch in loader:
+                labels.append(y_batch)
+                batch = self._collate_batch(x_batch, y_batch)
+                with eval_context(self):
+                    forward_dicts.append(self.model(batch))
+                    loss_dicts.append(self.model.loss_function(batch, forward_dicts[-1]))
+            losses, forwards = map(self.model._collate_tensor_dicts, [loss_dicts, forward_dicts])
+            losses = {k: v.mean().item() for k, v in losses.items()}
+        signs, metrics = {}, {}
+        for metric_type, metric_ins in self.metrics.items():
+            if metric_ins is None:
+                signs[metric_type] = -1
+                sub_metric = metrics[metric_type] = losses[metric_type]
+            else:
+                signs[metric_type] = metric_ins.sign
+                if self.tr_data.is_reg:
+                    metric_predictions = predictions
+                else:
+                    if metric_ins.requires_prob:
+                        metric_predictions = self.model.to_prob(predictions)
+                    else:
+                        metric_predictions = predictions.argmax(1).reshape([-1, 1])
+                sub_metric = metrics[metric_type] = metric_ins.metric(labels, metric_predictions)
+            if self.metrics_decay is not None and self.start_snapshot:
+                metrics[metric_type] = self.metrics_decay[metric_type].update("metric", sub_metric)
+        weighted_scores = [v * signs[k] * self.metrics_weights[k] for k, v in metrics.items()]
+        score = sum(weighted_scores) / len(weighted_scores)
+
+        if self._epoch_tqdm is not None:
+            self._epoch_tqdm.set_postfix(metrics)
+        msg = (
+            f"| epoch {self._epoch_count:^4d} - step {self._step_count:^6d} | "
+            f"{' | '.join([f'{k} : {fix_float_to_length(metrics[k], 6)}' for k in sorted(metrics)])} | "
+            f"score : {fix_float_to_length(score, 6)} |"
+        )
+        with open(self._log_file, "a") as f:
+            f.write(f"{msg}\n")
+        self.log_msg(msg, verbose_level=None)
+
+        return score, metrics
+
+    def _predict(self,
+                 loader: DataLoader,
+                 **kwargs) -> Dict[str, np.ndarray]:
+        with eval_context(self):
+            results, labels = [], []
+            for x_batch, y_batch in loader:
+                if y_batch is not None:
+                    labels.append(y_batch)
+                results.append(self.model(self._collate_batch(x_batch, y_batch), **kwargs))
+        results = self.model._collate_tensor_dicts(results)
+        results = {k: v.cpu().numpy() for k, v in results.items()}
+        if labels:
+            labels = np.vstack(labels)
+            results["labels"] = labels
+        return results
+
+    # api
+
+    def forward(self,
+                tr_data: TabularData,
+                cv_data: TabularData = None):
+        # data
+        self._init_data(tr_data, cv_data)
+        # optimizer
+        self._init_optimizers()
+        # metrics
+        self._init_metrics()
+        # monitor
+        self._monitor = TrainMonitor(1).register_pipeline(self)
+        # train
+        self._collect_info(return_only=False)
+        terminate = False
+        self._step_count = self._epoch_count = 0
+        tuple(map(lambda n: os.makedirs(n, exist_ok=True), [self.log_folder, self.checkpoint_folder]))
+        log_name = f"{timestamp()}.txt"
+        self._log_file = os.path.join(self.log_folder, log_name)
+        with open(self._log_file, "w"):
+            pass
+        self._step_tqdm = self._epoch_tqdm = None
+        if self.use_tqdm:
+            self._epoch_tqdm = tqdm(list(range(self.num_epoch)), position=0)
+        while self._epoch_count < self.num_epoch:
+            try:
+                self._epoch_count += 1
+                self._step_tqdm = iter(self.tr_loader)
+                if self.use_tqdm:
+                    self._step_tqdm = tqdm(self._step_tqdm, total=len(self.tr_loader), position=1, leave=False)
+                for x_batch, y_batch in self._step_tqdm:
+                    self._step_count += 1
+                    with timing_context(self, "collate batch"):
+                        batch = self._collate_batch(x_batch, y_batch)
+                    with timing_context(self, "model.forward"):
+                        forward_results = self.model(batch)
+                    with timing_context(self, "loss.forward"):
+                        loss_dict = self.model.loss_function(batch, forward_results)
+                    with timing_context(self, "loss.backward"):
+                        loss_dict["loss"].backward()
+                    if self._clip_norm > 0.:
+                        with timing_context(self, "clip_norm_step"):
+                            self._clip_norm_step()
+                    with timing_context(self, "optimizer_step"):
+                        self._optimizer_step()
+                    if self.ema_decay is not None:
+                        with timing_context(self, "EMA"):
+                            self.ema_decay()
+                    with timing_context(self, "monitor_step"):
+                        terminate = self._monitor_step()
+                    if terminate:
+                        break
+            except KeyboardInterrupt:
+                self.log_msg("keyboard interrupted", self.error_prefix, msg_level=logging.ERROR)
+                terminate = True
+            if terminate:
+                if os.path.isdir(self.checkpoint_folder):
+                    self.log_msg("rolling back to the best checkpoint", self.info_prefix, 3)
+                    self.restore_checkpoint()
+                break
+            if self.use_tqdm:
+                self._epoch_tqdm.total = self.num_epoch
+                self._epoch_tqdm.update()
+        if self.use_tqdm:
+            if self._step_tqdm is not None:
+                self._step_tqdm.close()
+            self._epoch_tqdm.close()
+        self._step_count = self._epoch_count = -1
+        self._get_metrics()
+
+    def predict(self,
+                x: data_type,
+                **kwargs) -> np.ndarray:
+        data = self.tr_data.copy_to(x, None)
+        loader = DataLoader(self.cv_batch_size, ImbalancedSampler(data, shuffle=False))
+        return self._predict(loader, **kwargs)["predictions"]
+
+    def save_checkpoint(self, folder=None):
+        if folder is None:
+            folder = self.checkpoint_folder
+        torch.save(self.state_dict(), os.path.join(folder, "pipeline.pt"))
+
+    def restore_checkpoint(self, folder=None):
+        if folder is None:
+            folder = self.checkpoint_folder
+        pipeline_file = os.path.join(folder, "pipeline.pt")
+        if not os.path.isfile(pipeline_file):
+            self.log_msg(
+                f"no pipeline file found in {self.checkpoint_folder}",
+                self.warning_prefix, msg_level=logging.WARNING
+            )
+            return self
+        self.log_msg(f"restoring from {folder}", self.info_prefix, 4)
+        self.load_state_dict(torch.load(pipeline_file, map_location=self.model.device))
+        return self
