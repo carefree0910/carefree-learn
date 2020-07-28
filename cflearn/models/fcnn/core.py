a3d486d470cb8aeb7e12c0c13bd7fd0b25985540
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-07-28 15:45:38 +0800

    ⚡️Improved default settings of `hidden_units`

--- cflearn/models/fcnn/core.py
+++ cflearn/models/fcnn/core.py
@@ -32,12 +32,14 @@ class FCNN(ModelBase):
             else:
                 hidden_units = [2 * self._fc_in_dim, 2 * self._fc_in_dim]
         else:
-            if len(self.tr_data) >= 100000:
+            num_tr_data = len(self.tr_data)
+            if num_tr_data >= 100000:
                 hidden_units = [768, 768]
-            elif len(self.tr_data) >= 10000:
+            elif num_tr_data >= 10000:
                 hidden_units = [512, 512]
             else:
-                hidden_units = [2 * self._fc_in_dim, 2 * self._fc_in_dim]
+                hidden_dim = max(64 if num_tr_data >= 1000 else 32, 2 * self._fc_in_dim)
+                hidden_units = [hidden_dim, hidden_dim]
         self.hidden_units = self.config.setdefault("hidden_units", hidden_units)
         self.mapping_configs = self.config.setdefault("mapping_configs", {})
         if isinstance(self.mapping_configs, dict):
