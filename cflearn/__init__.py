c8394fa6ab6f67c6c8aae20de5c928867bc38f61
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-07-28 12:38:20 +0800

    ✨Implemented cflearn.zoo

--- cflearn/__init__.py
+++ cflearn/__init__.py
@@ -9,6 +9,7 @@ from functools import partial
 from cftool.ml.hpo import HPOBase
 from cftool.ml import register_metric
 from cfdata.tabular.processors.base import Processor
+from abc import ABCMeta, abstractmethod
 
 from .dist import *
 from .bases import *
@@ -470,6 +471,152 @@ def _remove(identifier: str = "cflearn",
         path = f"{path}.zip"
         print(f"{LoggingMixin.info_prefix}removing {path}...")
         os.remove(path)
+
+
+# zoo
+
+zoo_dict: Dict[str, Type["ZooBase"]] = {}
+
+
+class ZooBase(metaclass=ABCMeta):
+    def __init__(self,
+                 *,
+                 model_type: str = "default",
+                 increment_config: Dict[str, Any] = None):
+        self._model_type = model_type
+        self._increment_config = increment_config
+
+    @property
+    @abstractmethod
+    def benchmarks(self) -> Dict[str, dict]:
+        """
+        this method should return a dict of configs (which represent benchmarks)
+        * Note that "default" key should always be included in the returned dict
+        """
+        raise NotImplementedError
+
+    @property
+    def config(self) -> dict:
+        """ return corresponding config of self._model_type, update with increment_config if provided """
+        config_dict = self.benchmarks
+        assert "default" in config_dict, "'default' should be included in config_dict"
+        config = config_dict.get(self._model_type)
+        if config is None:
+            if self._model_type != "default":
+                print(
+                    f"{LoggingMixin.warning_prefix}model_type '{self._model_type}' is not recognized, "
+                    "'default' model_type will be used"
+                )
+                self._model_type = "default"
+            config = self.benchmarks["default"]
+        config = shallow_copy_dict(config)
+        if self._increment_config is not None:
+            update_dict(self._increment_config, config)
+        return config
+
+    @property
+    def model(self) -> str:
+        return self.__identifier__
+
+    @property
+    def m(self) -> Wrapper:
+        """ return corresponding model of self.config """
+        return make(self.model, **self.config)
+
+    def switch(self, model_type) -> "ZooBase":
+        """ switch to another model_type """
+        self._model_type = model_type
+        return self
+
+    @classmethod
+    def register(cls, name: str):
+        global zoo_dict
+        def before(cls_): cls_.__identifier__ = name
+        return register_core(name, zoo_dict, before_register=before)
+
+
+@ZooBase.register("fcnn")
+class FCNNZoo(ZooBase):
+    @property
+    def benchmarks(self) -> Dict[str, dict]:
+        return {
+            "default": {},
+            "light_bn": {
+                "model_config": {
+                    "hidden_units": [128]
+                }
+            },
+            "on_large": {
+                "model_config": {
+                    "mapping_configs": {"dropout": 0.1, "batch_norm": False}
+                }
+            },
+            "light": {
+                "model_config": {
+                    "hidden_units": [128],
+                    "mapping_configs": {"batch_norm": False}
+                }
+            },
+            "on_sparse": {
+                "optimizer_config": {"lr": 1e-4},
+                "model_config": {
+                    "hidden_units": [128],
+                    "mapping_configs": {"dropout": 0.9, "batch_norm": False}
+                }
+            }
+        }
+
+
+@ZooBase.register("tree_dnn")
+class TreeDNNZoo(ZooBase):
+    @property
+    def benchmarks(self) -> Dict[str, dict]:
+        return {
+            "default": {},
+            "on_large": {
+                "model_config": {
+                    "dndf_config": None,
+                    "mapping_configs": {"dropout": 0.1}
+                }
+            },
+            "light": {
+                "model_config": {
+                    "dndf_config": None,
+                    "mapping_configs": {"batch_norm": False},
+                    "default_encoding_configs": {"embedding_dim": 8}
+                }
+            },
+            "on_sparse": {
+                "optimizer_config": {"lr": 1e-4},
+                "model_config": {
+                    "dndf_config": None,
+                    "mapping_configs": {
+                        "dropout": 0.9,
+                        "batch_norm": False,
+                        "pruner_config": None
+                    },
+                    "default_encoding_configs": {"embedding_dim": 8}
+                }
+            }
+        }
+
+
+@ZooBase.register("ddr")
+class DDRZoo(ZooBase):
+    @property
+    def benchmarks(self) -> Dict[str, dict]:
+        return {
+            "default": {},
+            "disjoint": {"joint_training": False},
+            "q_only": {"fetches": ["quantile"]}
+        }
+
+
+def zoo(model: str = "fcnn",
+        *,
+        model_type: str = "default",
+        increment_config: Dict[str, Any] = None) -> ZooBase:
+    return zoo_dict[model](model_type=model_type, increment_config=increment_config)
 
 
 # others
