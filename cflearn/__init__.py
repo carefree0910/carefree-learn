9f0460a3a523ff85403f78d194c0af3aa75c4ae1
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-08-26 04:51:31 +0800

    ✨⚡️Enhanced cflearn.ensemble & Implemented cflearn.Ensemble

--- cflearn/__init__.py
+++ cflearn/__init__.py
@@ -1,7 +1,11 @@
 import os
-import random
+import math
+import tqdm
+import torch
 import shutil
 import logging
+
+import numpy as np
 
 from typing import *
 from cftool.misc import *
@@ -11,6 +15,7 @@ from cfdata.tabular import *
 from cftool.ml.hpo import HPOBase
 from cftool.ml import register_metric
 from cfdata.tabular.processors.base import Processor
+from torch.nn.functional import one_hot
 from abc import ABCMeta, abstractmethod
 
 from .dist import *
@@ -234,11 +239,6 @@ def repeat_with(x: data_type,
     if patterns is not None:
         data = patterns[identifiers[0]][0].model.tr_data
     return RepeatResult(experiments, data, patterns)
-
-
-def ensemble(patterns: List[ModelPattern],
-             ensemble_method: Union[str, collate_fn_type] = "default") -> EnsemblePattern:
-    return EnsemblePattern(patterns, ensemble_method)
 
 
 def tune_with(x: data_type,
@@ -609,6 +609,7 @@ def zoo(model: str = "fcnn",
 # benchmark
 
 class BenchmarkResults(NamedTuple):
+    data: TabularData
     best_configs: Dict[str, Dict[str, Any]]
     best_methods: Dict[str, str]
     experiments: Experiments
@@ -624,7 +625,15 @@ class Benchmark(LoggingMixin):
                  project_name: str = "carefree-learn",
                  models: Union[str, List[str]] = "fcnn",
                  increment_config: Dict[str, Any] = None,
+                 data_config: Dict[str, Any] = None,
+                 read_config: Dict[str, Any] = None,
                  use_cuda: bool = True):
+        self.data = None
+        if data_config is None:
+            data_config = {}
+        if read_config is None:
+            read_config = {}
+        self.data_config, self.read_config = data_config, read_config
         self.task_name, self.task_type = task_name, task_type
         if temp_folder is None:
             temp_folder = f"__{task_name}__"
@@ -700,7 +709,17 @@ class Benchmark(LoggingMixin):
             metric: self.configs[identifier]
             for metric, identifier in best_methods.items()
         }
-        return BenchmarkResults(best_configs, best_methods, self.experiments, comparer)
+        return BenchmarkResults(self.data, best_configs, best_methods, self.experiments, comparer)
+
+    def _pre_process(self,
+                     x: data_type,
+                     y: data_type = None) -> TabularDataset:
+        data_config = shallow_copy_dict(self.data_config)
+        task_type = data_config.pop("task_type", None)
+        if task_type is not None:
+            assert task_type is self.task_type
+        self.data = TabularData.simple(self.task_type, **data_config).read(x, y, **self.read_config)
+        return self.data.to_dataset()
 
     def _k_core(self,
                 k_iterator: Iterable,
@@ -733,8 +752,8 @@ class Benchmark(LoggingMixin):
                run_tasks: bool = True,
                predict_config: Dict[str, Any] = None,
                benchmarks: Dict[str, Dict[str, Dict[str, Any]]] = None) -> BenchmarkResults:
-        k_fold = KFold(k, TabularDataset.from_xy(x, y, task_type=self.task_type))
-        return self._k_core(k_fold, num_jobs, run_tasks, predict_config, benchmarks)
+        dataset = self._pre_process(x, y)
+        return self._k_core(KFold(k, dataset), num_jobs, run_tasks, predict_config, benchmarks)
 
     def k_random(self,
                  k: int,
@@ -746,8 +765,8 @@ class Benchmark(LoggingMixin):
                  run_tasks: bool = True,
                  predict_config: Dict[str, Any] = None,
                  benchmarks: Dict[str, Dict[str, Dict[str, Any]]] = None) -> BenchmarkResults:
-        k_random = KRandom(k, num_test, TabularDataset.from_xy(x, y, task_type=self.task_type))
-        return self._k_core(k_random, num_jobs, run_tasks, predict_config, benchmarks)
+        dataset = self._pre_process(x, y)
+        return self._k_core(KRandom(k, num_test, dataset), num_jobs, run_tasks, predict_config, benchmarks)
 
     def save(self,
              saving_folder: str,
@@ -794,9 +813,136 @@ class Benchmark(LoggingMixin):
         return benchmark, results
 
 
+# ensemble
+
+def ensemble(patterns: List[ModelPattern],
+             *,
+             pattern_weights: np.ndarray = None,
+             ensemble_method: Union[str, collate_fn_type] = None) -> EnsemblePattern:
+    if ensemble_method is None:
+        if pattern_weights is None:
+            ensemble_method = "default"
+        else:
+            pattern_weights = pattern_weights.reshape([-1, 1, 1])
+
+            def ensemble_method(arrays: List[np.ndarray],
+                                requires_prob: bool) -> np.ndarray:
+                predictions = np.array(arrays).reshape([len(arrays), len(arrays[0]), -1])
+                if requires_prob or not np.issubdtype(predictions.dtype, np.integer):
+                    return (predictions * pattern_weights).sum(axis=0)
+                encodings = one_hot(to_torch(predictions).to(torch.long).squeeze()).to(torch.float32)
+                weighted = (encodings * pattern_weights).sum(dim=0)
+                return to_numpy(weighted.argmax(1)).reshape([-1, 1])
+
+    return EnsemblePattern(patterns, ensemble_method)
+
+
+class EnsembleResults(NamedTuple):
+    data: TabularData
+    pattern: EnsemblePattern
+    experiments: Union[Experiments, None]
+
+
+class Ensemble:
+    def __init__(self,
+                 task_type: TaskTypes,
+                 config: Dict[str, Any] = None):
+        self.task_type = task_type
+        if config is None:
+            config = {}
+        self.config = config
+
+    def bagging(self,
+                x: data_type,
+                y: data_type = None,
+                *,
+                k: int = 10,
+                num_test: Union[int, float] = 0.1,
+                num_jobs: int = 4,
+                run_tasks: bool = True,
+                predict_config: Dict[str, Any] = None,
+                temp_folder: str = None,
+                project_name: str = "carefree-learn",
+                task_name: str = "bagging",
+                models: Union[str, List[str]] = "fcnn",
+                increment_config: Dict[str, Any] = None,
+                use_cuda: bool = True) -> EnsembleResults:
+        if isinstance(models, str):
+            models = [models]
+
+        data_config, read_config = map(self.config.get, ["data_config", "read_config"], [{}, {}])
+        benchmark = Benchmark(
+            task_name, self.task_type,
+            temp_folder=temp_folder, project_name=project_name,
+            models=models, increment_config=increment_config, use_cuda=use_cuda,
+            data_config=data_config, read_config=read_config
+        )
+        dataset = benchmark._pre_process(x, y)
+        k_bootstrap = KBootstrap(k, num_test, dataset)
+        benchmark_results = benchmark._k_core(
+            k_bootstrap, num_jobs, run_tasks, predict_config,
+            {model: {"config": shallow_copy_dict(self.config)} for model in models}
+        )
+
+        def _pre_process(x_):
+            return benchmark_results.data.transform(x_, contains_labels=False).x
+
+        experiments = benchmark_results.experiments
+        ms_dict = transform_experiments(experiments)
+        all_models = sum(ms_dict.values(), [])
+        all_patterns = [m.to_pattern(pre_process=_pre_process) for m in all_models]
+        ensemble_pattern = ensemble(all_patterns)
+
+        return EnsembleResults(benchmark_results.data, ensemble_pattern, experiments)
+
+    def adaboost(self,
+                 x: data_type,
+                 y: data_type = None,
+                 *,
+                 k: int = 10,
+                 eps: float = 1e-12,
+                 model: str = "fcnn",
+                 increment_config: Dict[str, Any] = None,
+                 sample_weights: Union[np.ndarray, None] = None,
+                 num_test: Union[int, float] = 0.1) -> EnsembleResults:
+        if increment_config is None:
+            increment_config = {}
+        config = shallow_copy_dict(self.config)
+        update_dict(increment_config, config)
+        config["cv_split"] = num_test
+        config.setdefault("use_tqdm", False)
+        config.setdefault("verbose_level", 0)
+
+        data = None
+        patterns, pattern_weights = [], []
+        for _ in tqdm.tqdm(list(range(k))):
+            m = make(model=model, **config)
+            m.fit(x, y, sample_weights=sample_weights)
+            predictions = m.predict(x, contains_labels=True).astype(np.float32)
+            target = m._original_data.processed.y.astype(np.float32)
+            errors = (predictions != target).ravel()
+            if sample_weights is None:
+                e = errors.mean()
+            else:
+                e = errors.dot(sample_weights) / len(errors)
+            em = min(max(e, eps), 1. - eps)
+            am = 0.5 * math.log(1. / em - 1.)
+            if sample_weights is None:
+                sample_weights = np.ones_like(predictions).ravel()
+            target[target == 0.] = predictions[predictions == 0.] = -1.
+            sample_weights *= np.exp(-am * target * predictions).ravel()
+            sample_weights /= np.mean(sample_weights)
+            patterns.append(m.to_pattern())
+            pattern_weights.append(am)
+            if data is None:
+                data = m._original_data
+
+        pattern_weights = np.array(pattern_weights, np.float32)
+        ensemble_pattern = ensemble(patterns, pattern_weights=pattern_weights)
+        return EnsembleResults(data, ensemble_pattern, None)
+
+
 # others
-
-
 
 def make_toy_model(model: str = "fcnn",
                    config: Dict[str, Any] = None,
