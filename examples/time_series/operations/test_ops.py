8d3589dbe79755d61ab92fb91568ef962b13d78e
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-09-21 14:09:56 +0800

    ✅Introduced many time series examples

---
+++ examples/time_series/operations/test_ops.py
@@ -0,0 +1,84 @@
+import os
+import random
+import cflearn
+
+import numpy as np
+
+from cfdata.tabular import *
+from datetime import datetime, timedelta
+
+num_case = 10
+num_sample = 10000
+num_history = 5
+sf_name = f"sum_{num_history}"
+pf_name = f"prod_{num_history}"
+file_folder = os.path.dirname(__file__)
+sf_file = os.path.join(file_folder, f"{sf_name}.csv")
+pf_file = os.path.join(file_folder, f"{pf_name}.csv")
+sf_te_file = os.path.join(file_folder, f"{sf_name}_te.csv")
+pf_te_file = os.path.join(file_folder, f"{pf_name}_te.csv")
+
+
+def make_datasets():
+    def write(num_id, num_line, sum_f, prod_f):
+        header = "id,Date,Value,Label\n"
+        sum_f.write(header)
+        prod_f.write(header)
+        begin = datetime(2020, 1, 1)
+        for i in range(num_id):
+            last = None
+            s_label, p_label = 0, 1
+            id_ = f"case{i}"
+            values = []
+            for j in range(num_line):
+                delta = timedelta(j)
+                current = (begin + delta).strftime("%Y-%m-%d")
+                value = 2 * random.random() - 1
+                prefix = f"{id_},{current},{value}"
+                if last is not None:
+                    s_label -= last
+                    p_label /= last
+                s_label += value
+                p_label *= value
+                sum_f.write(f"{prefix},{s_label}\n")
+                prod_f.write(f"{prefix},{p_label}\n")
+                values.append(value)
+                if j >= num_history - 1:
+                    last = values[j - num_history + 1]
+
+    with open(sf_file, "w") as sf, open(pf_file, "w") as pf:
+        write(num_case, num_sample, sf, pf)
+    with open(sf_te_file, "w") as sf_te, open(pf_te_file, "w") as pf_te:
+        write(num_case, num_history + 1, sf_te, pf_te)
+
+
+def test_ops():
+
+    make_datasets()
+
+    ts_config = TimeSeriesConfig("id", "Date")
+    aggregation_config = {"num_history": num_history}
+    data_config = {
+        "default_numerical_process": "identical",
+        "label_process_method": "identical",
+    }
+
+    for op, model in zip(["sum", "prod"], ["linear", "rnn"]):
+        task = f"{op}_{num_history}"
+
+        m = cflearn.make(
+            model,
+            ts_config=ts_config,
+            aggregation_config=aggregation_config,
+            data_config=data_config,
+        )
+
+        m.fit(os.path.join(file_folder, f"{task}.csv"))
+        predictions = m.predict(os.path.join(file_folder, f"{task}_te.csv"), contains_labels=True)
+        labels = np.array(m.tr_data.read_file(f"{task}_te.csv")[1], np.float32)
+        labels = labels.reshape([-1, num_history + 1])[..., -2:].reshape([-1, 1])
+        print(np.hstack([predictions, labels]))
+
+
+if __name__ == '__main__':
+    test_ops()
---
+++ examples/time_series/shampoo/test_shampoo.py
@@ -0,0 +1,33 @@
+import os
+import cflearn
+
+from cfdata.tabular import *
+
+file_folder = os.path.dirname(__file__)
+
+
+def test_shampoo():
+    src_file = os.path.join(file_folder, "data.csv")
+    tgt_file = os.path.join(file_folder, "new_data.csv")
+    ts_config = TimeSeriesConfig(TimeSeriesModifier.id_name, "\"Month\"")
+    modifier = TimeSeriesModifier(
+        src_file,
+        TaskTypes.TIME_SERIES_REG,
+    )
+    modifier.pad_id()
+    modifier.pad_labels(
+        lambda batch: batch[..., -1, :],
+        offset=1,
+        ts_config=ts_config,
+    )
+    modifier.export_to(tgt_file)
+    aggregation_config = {"num_history": 2}
+    m = cflearn.make(
+        ts_config=ts_config,
+        aggregation_config=aggregation_config,
+    )
+    m.fit(os.path.join(file_folder, tgt_file))
+
+
+if __name__ == '__main__':
+    test_shampoo()
