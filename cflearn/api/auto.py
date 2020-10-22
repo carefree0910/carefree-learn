3d0a25fd28b3785083b02cabd8aaa4da4df4f0ae
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-10-23 07:30:09 +0800

    ⚡️SIGNIFICANTLY optimized `Auto`
    1) multiple algorithms will be used
    2) `num_repeat` is reduced (20 -> 3)
    3) shared data across different experiments

--- cflearn/api/auto.py
+++ cflearn/api/auto.py
@@ -2,16 +2,17 @@ import os
 import torch
 import optuna
 
-import numpy as np
 import optuna.visualization as vis
 
 from typing import *
+from tqdm import tqdm
 from functools import partial
 from cftool.misc import shallow_copy_dict
 from cftool.misc import lock_manager
 from cftool.misc import Saving
 from cftool.ml.utils import scoring_fn_type
 from cfdata.tabular import task_type_type
+from cfdata.tabular import parse_task_type
 from cfdata.tabular import TabularData
 from optuna.trial import TrialState
 from optuna.trial import FrozenTrial
@@ -26,18 +27,16 @@ from .hpo import OptunaPresetParams
 from .production import Pack
 from .production import Predictor
 from ..types import data_type
-from ..pipeline.core import Pipeline
 
 
 class Auto:
     data_folder = "__data__"
-    pattern_weights_file = "pattern_weights.npy"
 
     def __init__(
         self,
         task_type: task_type_type,
         *,
-        model: str = "fcnn",
+        models: Union[str, List[str]] = "auto",
         tune_lr: bool = True,
         tune_optimizer: bool = True,
         tune_ema_decay: bool = True,
@@ -45,10 +44,7 @@ class Auto:
         tune_init_method: bool = True,
         **kwargs: Any,
     ):
-        self.model = model
         self.task_type = task_type
-        self.pipelines: Optional[List[Pipeline]] = None
-        self.pattern_weights: Optional[np.ndarray] = None
         self.preset_params = OptunaPresetParams(
             tune_lr=tune_lr,
             tune_optimizer=tune_optimizer,
@@ -57,15 +53,33 @@ class Auto:
             tune_init_method=tune_init_method,
             **kwargs,
         )
+        # models
+        if isinstance(models, list):
+            self.models = models
+        else:
+            if models != "auto":
+                self.models = [models]
+            else:
+                self.models = ["fcnn", "tree_dnn"]
+                parsed_task_type = parse_task_type(task_type)
+                if parsed_task_type.is_ts:
+                    self.models += ["rnn", "transformer"]
+                elif parsed_task_type.is_clf:
+                    self.models += ["nnb", "ndt"]
+                else:
+                    self.models.append("ddr")
+        if not self.models:
+            raise ValueError("`models` should be provided")
 
     def __str__(self) -> str:
-        return f"Auto_{self.model}({self.task_type})"
+        model_str = ", ".join(self.models)
+        return f"Auto({model_str})({self.task_type})"
 
     __repr__ = __str__
 
     @property
-    def study(self) -> optuna.study.Study:
-        return self.optuna_result.study
+    def studies(self) -> Dict[str, optuna.study.Study]:
+        return {k: v.study for k, v in self.optuna_results.items()}
 
     @property
     def predict(self) -> Callable:
@@ -76,12 +90,18 @@ class Auto:
         return partial(self.pattern.predict, requires_prob=True)
 
     @property
-    def pruned_trials(self) -> List[FrozenTrial]:
-        return [t for t in self.study.trials if t.state == TrialState.PRUNED]
+    def pruned_trials(self) -> Dict[str, List[FrozenTrial]]:
+        return {
+            k: [t for t in v.trials if t.state == TrialState.PRUNED]
+            for k, v in self.studies.items()
+        }
 
     @property
-    def complete_trials(self) -> List[FrozenTrial]:
-        return [t for t in self.study.trials if t.state == TrialState.COMPLETE]
+    def complete_trials(self) -> Dict[str, List[FrozenTrial]]:
+        return {
+            k: [t for t in v.trials if t.state == TrialState.COMPLETE]
+            for k, v in self.studies.items()
+        }
 
     # api
 
@@ -92,101 +112,93 @@ class Auto:
         x_cv: data_type = None,
         y_cv: data_type = None,
         *,
-        params: Optional[optuna_params_type] = None,
         study_config: Optional[Dict[str, Any]] = None,
         predict_config: Optional[Dict[str, Any]] = None,
         metrics: Optional[Union[str, List[str]]] = None,
+        params: Optional[Dict[str, optuna_params_type]] = None,
         num_jobs: int = 1,
         num_trial: int = 50,
-        num_repeat: int = 20,
+        num_repeat: int = 3,
         num_parallel: int = 0,
         timeout: Optional[float] = None,
         score_weights: Optional[Dict[str, float]] = None,
         estimator_scoring_function: Union[str, scoring_fn_type] = "mean",
         temp_folder: str = "__tmp__",
         num_final_repeat: int = 20,
-        bagging_config: Optional[Dict[str, Any]] = None,
         extra_config: Optional[Dict[str, Any]] = None,
         cuda: Optional[Union[str, int]] = None,
     ) -> "Auto":
-        model = self.model
-        optuna_temp_folder = os.path.join(temp_folder, "__optuna__")
+        self.best_params = {}
+        self.optuna_results = {}
 
-        if params is None:
-            params = self.preset_params.get(model)
+        tuner = None
+        optuna_folder = os.path.join(temp_folder, "__optuna__")
+        for model in self.models:
+            if params is not None:
+                model_params = params[model]
+            else:
+                model_params = self.preset_params.get(model)
 
-        self.optuna_result = optuna_tune(
-            x,
-            y,
-            x_cv,
-            y_cv,
-            model=model,
-            task_type=self.task_type,
-            params=params,
-            study_config=study_config,
-            metrics=metrics,
-            num_jobs=num_jobs,
-            num_trial=num_trial,
-            num_repeat=num_repeat,
-            num_parallel=num_parallel,
-            timeout=timeout,
-            score_weights=score_weights,
-            estimator_scoring_function=estimator_scoring_function,
-            temp_folder=optuna_temp_folder,
-            extra_config=extra_config,
-            cuda=cuda,
-        )
-        num_jobs = max(num_jobs, num_parallel)
-        self.best_param = self.optuna_result.best_param
-        self.repeat_result: Optional[RepeatResult] = None
-        self.bagging_result: Optional[EnsembleResults] = None
-        if bagging_config is not None:
-            bagging_temp_folder = os.path.join(temp_folder, "__bagging__")
-            bagging = Ensemble(self.task_type, self.best_param).bagging
-            bagging_config.setdefault("task_name", f"{model}_opt")
-            increment_config = bagging_config.setdefault("increment_config", {})
-            increment_config.setdefault("trigger_logging", False)
-            increment_config.setdefault("verbose_level", 0)
-            bagging_config["temp_folder"] = bagging_temp_folder
-            bagging_config["predict_config"] = predict_config
-            bagging_config["use_tracker"] = False
-            bagging_config["num_jobs"] = num_jobs
-            bagging_config["models"] = model
-            bagging_config["k"] = num_final_repeat
-            x_merged, y_merged = self._merge_data(x, y, x_cv, y_cv)
-            self.bagging_result = bagging(x_merged, y_merged, **bagging_config)
-            self.pattern_weights = self.bagging_result.pattern_weights
-            self.pipelines = self.bagging_result.pipelines
-            self.pattern = self.bagging_result.pattern
-            self.data = self.bagging_result.data
-        else:
-            repeat_temp_folder = os.path.join(temp_folder, "__repeat__")
-            repeat_config = shallow_copy_dict(self.best_param)
-            repeat_config.update(
-                {
-                    "models": model,
-                    "sequential": num_jobs <= 1,
-                    "predict_config": predict_config,
-                    "temp_folder": repeat_temp_folder,
-                    "num_repeat": num_final_repeat,
-                    "num_jobs": num_jobs,
-                }
+            args = () if tuner is not None else (x, y, x_cv, y_cv)
+            optuna_result = optuna_tune(
+                *args,
+                model=model,
+                task_type=self.task_type,
+                tuner=tuner,
+                params=model_params,
+                study_config=study_config,
+                metrics=metrics,
+                num_jobs=num_jobs,
+                num_trial=num_trial,
+                num_repeat=num_repeat,
+                num_parallel=num_parallel,
+                timeout=timeout,
+                score_weights=score_weights,
+                estimator_scoring_function=estimator_scoring_function,
+                temp_folder=optuna_folder,
+                extra_config=extra_config,
+                cuda=cuda,
             )
-            x, y, x_cv, y_cv = self.optuna_result.tuner.make_data()
-            self.repeat_result = repeat_with(x, y, x_cv, y_cv, **repeat_config)
-            patterns_dict = self.repeat_result.patterns
-            pipelines_dict = self.repeat_result.pipelines
-            assert patterns_dict is not None and pipelines_dict is not None
-            data = self.repeat_result.data
-            self.pipelines = pipelines_dict[model]
-            self.pattern = ensemble(patterns_dict[model])
-            self.data = data
+            tuner = optuna_result.tuner
+            best_param = optuna_result.best_param
+            self.best_params[model] = best_param
+            self.optuna_results[model] = optuna_result
+
+        num_jobs = max(num_jobs, num_parallel)
+        repeat_temp_folder = os.path.join(temp_folder, "__repeat__")
+        os.makedirs(repeat_temp_folder, exist_ok=True)
+        model_configs = shallow_copy_dict(self.best_params)
+        repeat_config = {
+            "models": self.models,
+            "model_configs": model_configs,
+            "sequential": num_jobs <= 1,
+            "predict_config": predict_config,
+            "temp_folder": repeat_temp_folder,
+            "num_repeat": num_final_repeat,
+            "num_jobs": num_jobs,
+        }
+        assert tuner is not None
+        x, y, x_cv, y_cv = tuner.make_data()
+        self.repeat_result = repeat_with(x, y, x_cv, y_cv, **repeat_config)
+        self.pipelines = self.repeat_result.pipelines
+        self.patterns = self.repeat_result.patterns
+        self.data = self.repeat_result.data
+        assert self.pipelines is not None
+        assert self.patterns is not None
+
+        all_patterns = []
+        for v in self.patterns.values():
+            all_patterns.extend(v)
+        self.pattern = ensemble(all_patterns)
+
         return self
 
     def pack(
         self,
         export_folder: str,
         *,
+        verbose: bool = False,
+        use_tqdm: bool = True,
         compress: bool = True,
         retain_data: bool = False,
         remove_original: bool = True,
@@ -198,25 +210,28 @@ class Auto:
         with lock_manager(base_folder, [export_folder]):
             Saving.prepare_folder(self, export_folder)
             data_folder = os.path.join(export_folder, self.data_folder)
-            first_inference = self.pipelines[0].inference
-            if first_inference is None:
-                raise ValueError("`inference` in pipeline is not yet generated")
-            first_inference.data.save(
+            if self.data is None:
+                raise ValueError("`data` is not generated yet")
+            self.data.save(
                 data_folder,
                 retain_data=retain_data,
                 compress=False,
             )
-            for i, pipeline in enumerate(self.pipelines):
-                local_export_folder = os.path.join(export_folder, f"m_{i:04d}")
-                Pack.pack(
-                    pipeline,
-                    local_export_folder,
-                    pack_data=False,
-                    compress=False,
-                )
-            if self.pattern_weights is not None:
-                path = os.path.join(export_folder, self.pattern_weights_file)
-                np.save(path, self.pattern_weights)
+            iterator = self.models
+            if use_tqdm:
+                iterator = tqdm(iterator, "pack")
+            for model in iterator:
+                pipelines = self.pipelines[model]
+                model_folder = os.path.join(export_folder, model)
+                for i, pipeline in enumerate(pipelines):
+                    local_export_folder = os.path.join(model_folder, f"m_{i:04d}")
+                    Pack.pack(
+                        pipeline,
+                        local_export_folder,
+                        verbose=verbose,
+                        pack_data=False,
+                        compress=False,
+                    )
             if compress:
                 Saving.compress(abs_folder, remove_original=remove_original)
         return self
@@ -229,7 +244,7 @@ class Auto:
         *,
         compress: bool = True,
         use_tqdm: bool = False,
-    ) -> Tuple[List[Predictor], Optional[np.ndarray]]:
+    ) -> Dict[str, List[Predictor]]:
         base_folder = os.path.dirname(os.path.abspath(export_folder))
         with lock_manager(base_folder, [export_folder]):
             with Saving.compress_loader(
@@ -239,35 +254,35 @@ class Auto:
             ):
                 data_folder = os.path.join(export_folder, cls.data_folder)
                 data = TabularData.load(data_folder, compress=False)
-                predictors = []
-                pattern_weights = None
+                predictors = {}
                 for stuff in os.listdir(export_folder):
-                    if stuff == cls.pattern_weights_file:
-                        pattern_weights = np.load(os.path.join(export_folder, stuff))
-                    else:
-                        if stuff == cls.data_folder:
-                            continue
-                        local_folder = os.path.join(export_folder, stuff)
-                        predictors.append(
+                    if stuff == cls.data_folder:
+                        continue
+                    local_predictors = []
+                    model_folder = os.path.join(export_folder, stuff)
+                    for sub_folder in os.listdir(model_folder):
+                        local_predictors.append(
                             Pack.get_predictor(
-                                local_folder,
+                                sub_folder,
                                 device,
                                 data=data,
                                 compress=False,
                                 use_tqdm=use_tqdm,
                             )
                         )
-        return predictors, pattern_weights
+                    predictors[stuff] = local_predictors
+        return predictors
 
     # visualization
 
     def plot_param_importances(
         self,
+        model: str,
         evaluator: Optional[BaseImportanceEvaluator] = None,
         params: Optional[List[str]] = None,
         export_folder: Optional[str] = None,
     ) -> Figure:
-        fig = vis.plot_param_importances(self.study, evaluator, params)
+        fig = vis.plot_param_importances(self.studies[model], evaluator, params)
         if export_folder is not None:
             os.makedirs(export_folder, exist_ok=True)
             html_path = os.path.join(export_folder, "param_importances.html")
@@ -277,10 +292,11 @@ class Auto:
 
     def plot_contour(
         self,
+        model: str,
         params: Optional[List[str]] = None,
         export_folder: Optional[str] = None,
     ) -> Figure:
-        fig = vis.plot_contour(self.study, params)
+        fig = vis.plot_contour(self.studies[model], params)
         if export_folder is not None:
             os.makedirs(export_folder, exist_ok=True)
             html_path = os.path.join(export_folder, "contour.html")
@@ -290,10 +306,11 @@ class Auto:
 
     def plot_parallel_coordinate(
         self,
+        model: str,
         params: Optional[List[str]] = None,
         export_folder: Optional[str] = None,
     ) -> Figure:
-        fig = vis.plot_parallel_coordinate(self.study, params)
+        fig = vis.plot_parallel_coordinate(self.studies[model], params)
         if export_folder is not None:
             os.makedirs(export_folder, exist_ok=True)
             html_path = os.path.join(export_folder, "parallel_coordinate.html")
@@ -303,10 +320,11 @@ class Auto:
 
     def plot_slice(
         self,
+        model: str,
         params: Optional[List[str]] = None,
         export_folder: Optional[str] = None,
     ) -> Figure:
-        fig = vis.plot_slice(self.study, params)
+        fig = vis.plot_slice(self.studies[model], params)
         if export_folder is not None:
             os.makedirs(export_folder, exist_ok=True)
             html_path = os.path.join(export_folder, "slice.html")
@@ -316,9 +334,10 @@ class Auto:
 
     def plot_optimization_history(
         self,
+        model: str,
         export_folder: Optional[str] = None,
     ) -> Figure:
-        fig = vis.plot_optimization_history(self.study)
+        fig = vis.plot_optimization_history(self.studies[model])
         if export_folder is not None:
             os.makedirs(export_folder, exist_ok=True)
             html_path = os.path.join(export_folder, "optimization_history.html")
@@ -328,9 +347,10 @@ class Auto:
 
     def plot_intermediate_values(
         self,
+        model: str,
         export_folder: Optional[str] = None,
     ) -> Figure:
-        fig = vis.plot_intermediate_values(self.study)
+        fig = vis.plot_intermediate_values(self.studies[model])
         if export_folder is not None:
             os.makedirs(export_folder, exist_ok=True)
             html_path = os.path.join(export_folder, "intermediate_values.html")
@@ -338,8 +358,8 @@ class Auto:
                 f.write(fig.to_html())
         return fig
 
-    def plot_edf(self, export_folder: str = None) -> Figure:
-        fig = vis.plot_edf(self.study)
+    def plot_edf(self, model: str, export_folder: str = None) -> Figure:
+        fig = vis.plot_edf(self.studies[model])
         if export_folder is not None:
             os.makedirs(export_folder, exist_ok=True)
             html_path = os.path.join(export_folder, "edf.html")
