d2e7e535bea9de99cbc868310df5606869e405f2
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-10-23 15:51:06 +0800

    ⬆️Upgraded dependency of `carefree-data`

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "plotly",
         "trains",
         "carefree-ml>=0.1.0",
-        "carefree-data>=0.2.0rc.9",
+        "carefree-data>=0.2.0rc.10",
         "carefree-toolkit>=0.2.0rc.5",
         "dill",
         "future",
