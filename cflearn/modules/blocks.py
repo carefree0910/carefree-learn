4be1e398020227b5937143fd83bfe6f480946289
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-11-02 09:39:42 +0800

    🔥Removed redundant import

--- cflearn/modules/blocks.py
+++ cflearn/modules/blocks.py
@@ -7,7 +7,6 @@ import torch.nn.functional as F
 from typing import Any
 from typing import Dict
 from typing import List
-from typing import Tuple
 from typing import Union
 from typing import Callable
 from typing import Optional
