616a45fe3943d07582a6d2fa2faa227a261ae3a6
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-11-07 12:02:44 +0800

    🐛Fixed typo

--- cflearn/models/ddr/loss.py
+++ cflearn/models/ddr/loss.py
@@ -14,7 +14,7 @@ class DDRLoss(LossBase, LoggingMixin):
     def _init_config(self, config: Dict[str, Any]) -> None:
         self.fetch_q = config["fetch_q"]
         self.fetch_cdf = config["fetch_cdf"]
-        self.mtl = MTL(8, config["mtl_method"])
+        self.mtl = MTL(18, config["mtl_method"])
         self._lb_pdf = config.setdefault("lambda_pdf", 0.01)
         self._pdf_eps = config.setdefault("pdf_eps", 1.0e-8)
         self._lb_recover = config.setdefault("lambda_recover", 1.0)
