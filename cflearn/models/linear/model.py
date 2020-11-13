0144ddde2fe1da807bb06b0ad41f7d634e6726f0
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-11-13 21:26:07 +0800

    🎨Slightly beautified the codes

--- cflearn/models/linear/model.py
+++ cflearn/models/linear/model.py
@@ -6,8 +6,7 @@ from ..base import ModelBase
 class LinearModel(ModelBase):
     def define_pipe_configs(self) -> None:
         cfg = self.get_core_config(self)
-        linear_config = self.config.setdefault("linear_config", {})
-        cfg["linear_config"] = linear_config
+        cfg["linear_config"] = self.config.setdefault("linear_config", {})
         self.define_head_config("linear", cfg)
 
 
