f06976ababd0b806f2a5be7265ef2e6d72b71ad8
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-11-20 14:48:38 +0800

    ♻️Moved Environment.from_elements logics to Elements.to_config

--- cflearn/configs.py
+++ cflearn/configs.py
@@ -84,44 +84,185 @@ def _parse_config(config: general_config_type) -> Dict[str, Any]:
 
 class Elements(NamedTuple):
     model: str = "fcnn"
-    use_amp: Optional[bool] = None
-    use_simplify_data: Optional[bool] = None
-    delim: Optional[str] = None
     task_type: Optional[task_type_type] = None
-    skip_first: Optional[bool] = None
+    use_simplify_data: bool = False
+    data_config: Optional[Dict[str, Any]] = None
+    delim: Optional[str] = None
+    has_column_names: Optional[bool] = None
+    read_config: Optional[Dict[str, Any]] = None
+    batch_size: int = 128
     cv_split: Optional[Union[float, int]] = None
+    logging_folder: Optional[str] = None
+    logging_file: Optional[str] = None
+    use_amp: bool = False
     min_epoch: Optional[int] = None
     num_epoch: Optional[int] = None
     max_epoch: Optional[int] = None
     fixed_epoch: Optional[int] = None
-    batch_size: Optional[int] = None
-    max_snapshot_file: Optional[int] = None
-    clip_norm: Optional[float] = None
-    ema_decay: Optional[float] = None
+    max_snapshot_file: int = 5
+    clip_norm: float = 0.0
+    ema_decay: float = 0.0
     ts_config: Optional[TimeSeriesConfig] = None
     aggregation: Optional[str] = None
     aggregation_config: Optional[Dict[str, Any]] = None
     ts_label_collator_config: Optional[Dict[str, Any]] = None
-    data_config: Optional[Dict[str, Any]] = None
-    read_config: Optional[Dict[str, Any]] = None
     model_config: Optional[Dict[str, Any]] = None
-    metrics: Optional[Union[str, List[str]]] = None
+    metrics: Union[str, List[str]] = "auto"
     metric_config: Optional[Dict[str, Any]] = None
     optimizer: Optional[str] = None
     scheduler: Optional[str] = None
     optimizer_config: Optional[Dict[str, Any]] = None
     scheduler_config: Optional[Dict[str, Any]] = None
     optimizers: Optional[Dict[str, Any]] = None
-    logging_file: Optional[str] = None
-    logging_folder: Optional[str] = None
-    trigger_logging: Optional[bool] = None
+    trigger_logging: bool = False
     trial: Optional[Trial] = None
     tracker_config: Optional[Dict[str, Any]] = None
     cuda: Optional[Union[int, str]] = None
-    verbose_level: Optional[int] = None
-    use_timing_context: Optional[bool] = None
-    use_tqdm: Optional[bool] = None
+    verbose_level: int = 2
+    use_timing_context: bool = True
+    use_tqdm: bool = True
     extra_config: Optional[Dict[str, Any]] = None
+
+    def to_config(self) -> Dict[str, Any]:
+        if self.extra_config is None:
+            kwargs = {}
+        else:
+            kwargs = shallow_copy_dict(self.extra_config)
+        # inject fields
+        spec = inspect.getfullargspec(type(self)).args[1:-1]
+        for key in spec:
+            value = getattr(self, key)
+            if value is not None or key not in kwargs:
+                kwargs[key] = value
+        # data
+        data_config = self.data_config or {}
+        data_config["simplify"] = kwargs.pop("use_simplify_data")
+        data_config["time_series_config"] = kwargs.pop("ts_config")
+        task_type = kwargs.pop("task_type")
+        if task_type is not None:
+            data_config["task_type"] = task_type
+        read_config = self.read_config or {}
+        read_config["delim"] = kwargs.pop("delim")
+        read_config["has_column_names"] = kwargs.pop("has_column_names")
+        kwargs["data_config"] = data_config
+        kwargs["read_config"] = read_config
+        sampler_config = kwargs.setdefault("sampler_config", {})
+        sampler_config["aggregation"] = kwargs.pop("aggregation")
+        sampler_config["aggregation_config"] = kwargs.pop("aggregation_config")
+        # logging
+        if self.logging_folder is not None:
+            if self.logging_file is not None:
+                logging_file = self.logging_file
+            else:
+                logging_file = f"{self.model}_{timestamp()}.log"
+            kwargs["logging_file"] = logging_file
+        # trainer
+        trainer_config = kwargs.setdefault("trainer_config", {})
+        trainer_config["use_amp"] = kwargs.pop("use_amp")
+        min_epoch = kwargs.pop("min_epoch")
+        num_epoch = kwargs.pop("num_epoch")
+        max_epoch = kwargs.pop("max_epoch")
+        if self.fixed_epoch is not None:
+            msg = "`{}` should not be provided when `fixed_epoch` is provided"
+            if min_epoch is not None:
+                raise ValueError(msg.format("min_epoch"))
+            if num_epoch is not None:
+                raise ValueError(msg.format("num_epoch"))
+            if max_epoch is not None:
+                raise ValueError(msg.format("max_epoch"))
+            min_epoch = num_epoch = max_epoch = self.fixed_epoch
+        no_min, no_num, no_max = min_epoch is None, num_epoch is None, max_epoch is None
+        default_min, default_num, default_max = 0, 40, 200
+        if no_min and no_num and no_max:
+            min_epoch = default_min
+            num_epoch = default_num
+            max_epoch = default_max
+        elif no_min and no_num:
+            min_epoch = min(default_min, max_epoch)
+            num_epoch = min(default_num, max_epoch)
+        elif no_min and no_max:
+            min_epoch = min(default_min, num_epoch)
+            max_epoch = max(default_max, num_epoch)
+        elif no_num and no_max:
+            num_epoch = max(default_num, min_epoch)
+            max_epoch = max(default_max, min_epoch)
+        elif no_min:
+            if num_epoch > max_epoch:
+                raise ValueError("`num_epoch` should not be greater than `max_epoch`")
+            min_epoch = min(default_min, num_epoch)
+        elif no_num:
+            if min_epoch > max_epoch:
+                raise ValueError("`min_epoch` should not be greater than `max_epoch`")
+            num_epoch = max(min(default_num, max_epoch), min_epoch)
+        elif no_max:
+            if min_epoch > num_epoch:
+                raise ValueError("`min_epoch` should not be greater than `num_epoch`")
+            max_epoch = max(default_max, num_epoch)
+        trainer_config["min_epoch"] = min_epoch
+        trainer_config["num_epoch"] = num_epoch
+        trainer_config["max_epoch"] = max_epoch
+        trainer_config["max_snapshot_file"] = kwargs.pop("max_snapshot_file")
+        trainer_config["clip_norm"] = kwargs.pop("clip_norm")
+        # model
+        model_config = self.model_config or {}
+        model_config["ema_decay"] = kwargs.pop("ema_decay")
+        kwargs["model_config"] = model_config
+        # metrics
+        metric_config = kwargs.pop("metric_config") or {}
+        if self.metric_config is None and self.metrics is not None:
+            metric_config["types"] = kwargs.pop("metrics")
+        trainer_config["metric_config"] = metric_config
+        # optimizers
+        optimizer = kwargs.pop("optimizer")
+        scheduler = kwargs.pop("scheduler")
+        optimizers = kwargs.pop("optimizers")
+        optimizer_config = kwargs.pop("optimizer_config")
+        scheduler_config = kwargs.pop("scheduler_config")
+        if optimizers is not None:
+            if optimizer is not None:
+                print(
+                    f"{LoggingMixin.warning_prefix}`optimizer` is set to "
+                    f"'{optimizer}' but `optimizers` is provided, so "
+                    "`optimizer` will be ignored"
+                )
+            if optimizer_config is not None:
+                print(
+                    f"{LoggingMixin.warning_prefix}`optimizer_config` is "
+                    f"set to '{optimizer_config}' but `optimizers` is provided, "
+                    "so `optimizer_config` will be ignored"
+                )
+            if scheduler is not None:
+                print(
+                    f"{LoggingMixin.warning_prefix}`scheduler` is set to "
+                    f"'{scheduler}' but `optimizers` is provided, so "
+                    "`scheduler` will be ignored"
+                )
+            if scheduler_config is not None:
+                print(
+                    f"{LoggingMixin.warning_prefix}`scheduler_config` is "
+                    f"set to '{scheduler_config}' but `optimizers` is provided, "
+                    "so `scheduler_config` will be ignored"
+                )
+        else:
+            preset_optimizer = {}
+            if optimizer is not None:
+                if optimizer_config is None:
+                    optimizer_config = {}
+                preset_optimizer = {
+                    "optimizer": optimizer,
+                    "optimizer_config": optimizer_config,
+                }
+            if scheduler is not None:
+                if scheduler_config is None:
+                    scheduler_config = {}
+                preset_optimizer.update(
+                    {"scheduler": scheduler, "scheduler_config": scheduler_config}
+                )
+            if preset_optimizer:
+                optimizers = {"all": preset_optimizer}
+        if optimizers is not None:
+            trainer_config["optimizers"] = optimizers
+        return kwargs
 
     @classmethod
     def make(
@@ -133,13 +274,9 @@ class Elements(NamedTuple):
         kwargs = update_dict(inc_cfg, cfg)
         # pipeline general
         model = kwargs.setdefault("model", "fcnn")
-        kwargs.setdefault("model", "fcnn")
-        kwargs.setdefault("use_tqdm", True)
-        kwargs.setdefault("use_timing_context", True)
         kwargs.setdefault("use_binary_threshold", True)
         kwargs.setdefault("data_config", {})
         kwargs.setdefault("read_config", {})
-        kwargs.setdefault("cv_split", None)
         kwargs.setdefault("min_cv_split", 100)
         kwargs.setdefault("max_cv_split", 10000)
         kwargs.setdefault("max_cv_split_ratio", 0.5)
@@ -148,7 +285,6 @@ class Elements(NamedTuple):
         kwargs.setdefault("shuffle_tr", True)
         batch_size = kwargs.setdefault("batch_size", 128)
         kwargs.setdefault("cv_batch_size", 5 * batch_size)
-        kwargs.setdefault("sampler_config", {})
         kwargs.setdefault("ts_label_collator_config", {})
         log_folder = kwargs.setdefault("logging_folder", os.path.join("_logs", model))
         log_file = kwargs.get("logging_file")
@@ -157,16 +293,9 @@ class Elements(NamedTuple):
         else:
             log_path = os.path.abspath(os.path.join(log_folder, f"{timestamp()}.log"))
         kwargs["_logging_path_"] = log_path
-        kwargs.setdefault("trigger_logging", False)
-        # pipeline -> misc
-        kwargs.setdefault("cuda", None)
-        kwargs.setdefault("trial", None)
-        kwargs.setdefault("tracker_config", None)
-        kwargs.setdefault("verbose_level", 2)
         # trainer general
         trainer_config = kwargs.setdefault("trainer_config", {})
         trainer_config.setdefault("update_binary_threshold_at_runtime", False)
-        trainer_config.setdefault("clip_norm", 0.0)
         use_amp = trainer_config.get("use_amp", False)
         trainer_config["use_amp"] = use_amp and amp is not None
         default_checkpoint_folder = os.path.join(log_folder, "checkpoints")
@@ -181,11 +310,9 @@ class Elements(NamedTuple):
             optimizer_config.setdefault("lr", 1e-3)
         # trainer -> metrics
         metric_config = trainer_config.setdefault("metric_config", {})
-        metric_config.setdefault("types", "auto")
         metric_config.setdefault("decay", 0.1)
         # model general
         model_config = kwargs.setdefault("model_config", {})
-        model_config.setdefault("ema_decay", 0.0)
         encoding_methods = model_config.get("encoding_methods", {})
         encoding_configs = model_config.get("encoding_configs", {})
         encoding_methods = {str(k): v for k, v in encoding_methods.items()}
@@ -235,162 +362,7 @@ class Environment:
 
     @classmethod
     def from_elements(cls, elements: Elements) -> "Environment":
-        if elements.extra_config is None:
-            kwargs = {}
-        else:
-            kwargs = shallow_copy_dict(elements.extra_config)
-        # pipeline general
-        kwargs["model"] = elements.model
-        if elements.cv_split is not None:
-            kwargs["cv_split"] = elements.cv_split
-        if elements.use_tqdm is not None:
-            kwargs["use_tqdm"] = elements.use_tqdm
-        if elements.use_timing_context is not None:
-            kwargs["use_timing_context"] = elements.use_timing_context
-        if elements.batch_size is not None:
-            kwargs["batch_size"] = elements.batch_size
-        if elements.ts_label_collator_config is not None:
-            kwargs["ts_label_collator_config"] = elements.ts_label_collator_config
-        data_config = elements.data_config or {}
-        if elements.use_simplify_data is not None:
-            data_config["simplify"] = elements.use_simplify_data
-        if elements.ts_config is not None:
-            data_config["time_series_config"] = elements.ts_config
-        if elements.task_type is not None:
-            data_config["task_type"] = elements.task_type
-        read_config = elements.read_config or {}
-        if elements.delim is not None:
-            read_config["delim"] = elements.delim
-        if elements.skip_first is not None:
-            read_config["skip_first"] = elements.skip_first
-        kwargs["data_config"] = data_config
-        kwargs["read_config"] = read_config
-        sampler_config = kwargs.setdefault("sampler_config", {})
-        if elements.aggregation is not None:
-            sampler_config["aggregation"] = elements.aggregation
-        if elements.aggregation_config is not None:
-            sampler_config["aggregation_config"] = elements.aggregation_config
-        if elements.logging_folder is not None:
-            if elements.logging_file is not None:
-                logging_file = elements.logging_file
-            else:
-                logging_file = f"{elements.model}_{timestamp()}.log"
-            kwargs["logging_folder"] = elements.logging_folder
-            kwargs["logging_file"] = logging_file
-        if elements.trigger_logging is not None:
-            kwargs["trigger_logging"] = elements.trigger_logging
-        # trainer general
-        trainer_config = kwargs.setdefault("trainer_config", {})
-        if elements.use_amp is not None:
-            trainer_config["use_amp"] = elements.use_amp
-        min_epoch = elements.min_epoch
-        num_epoch = elements.num_epoch
-        max_epoch = elements.max_epoch
-        if elements.fixed_epoch is not None:
-            msg = "`{}` should not be provided when `fixed_epoch` is provided"
-            if min_epoch is not None:
-                raise ValueError(msg.format("min_epoch"))
-            if num_epoch is not None:
-                raise ValueError(msg.format("num_epoch"))
-            if max_epoch is not None:
-                raise ValueError(msg.format("max_epoch"))
-            min_epoch = num_epoch = max_epoch = elements.fixed_epoch
-        no_min, no_num, no_max = min_epoch is None, num_epoch is None, max_epoch is None
-        default_min, default_num, default_max = 0, 40, 200
-        if no_min and no_num and no_max:
-            min_epoch = default_min
-            num_epoch = default_num
-            max_epoch = default_max
-        elif no_min and no_num:
-            min_epoch = min(default_min, max_epoch)
-            num_epoch = min(default_num, max_epoch)
-        elif no_min and no_max:
-            min_epoch = min(default_min, num_epoch)
-            max_epoch = max(default_max, num_epoch)
-        elif no_num and no_max:
-            num_epoch = max(default_num, min_epoch)
-            max_epoch = max(default_max, min_epoch)
-        elif no_min:
-            if num_epoch > max_epoch:
-                raise ValueError("`num_epoch` should not be greater than `max_epoch`")
-            min_epoch = min(default_min, num_epoch)
-        elif no_num:
-            if min_epoch > max_epoch:
-                raise ValueError("`min_epoch` should not be greater than `max_epoch`")
-            num_epoch = max(min(default_num, max_epoch), min_epoch)
-        elif no_max:
-            if min_epoch > num_epoch:
-                raise ValueError("`min_epoch` should not be greater than `num_epoch`")
-            max_epoch = max(default_max, num_epoch)
-        trainer_config["min_epoch"] = min_epoch
-        trainer_config["num_epoch"] = num_epoch
-        trainer_config["max_epoch"] = max_epoch
-        if elements.max_snapshot_file is not None:
-            trainer_config["max_snapshot_file"] = elements.max_snapshot_file
-        if elements.clip_norm is not None:
-            trainer_config["clip_norm"] = elements.clip_norm
-        # model general
-        model_config = elements.model_config or {}
-        if elements.ema_decay is not None:
-            model_config["ema_decay"] = elements.ema_decay
-        kwargs["model_config"] = model_config
-        # metrics
-        metric_config_: Dict[str, Any] = {}
-        if elements.metric_config is not None:
-            metric_config_ = elements.metric_config
-        elif elements.metrics is not None:
-            metric_config_["types"] = elements.metrics
-        if metric_config_:
-            trainer_config["metric_config"] = metric_config_
-        # optimizers
-        optimizer = elements.optimizer
-        scheduler = elements.scheduler
-        optimizers = elements.optimizers
-        optimizer_config = elements.optimizer_config
-        scheduler_config = elements.scheduler_config
-        if optimizers is not None:
-            if optimizer is not None:
-                print(
-                    f"{LoggingMixin.warning_prefix}`optimizer` is set to "
-                    f"'{optimizer}' but `optimizers` is provided, so "
-                    "`optimizer` will be ignored"
-                )
-            if optimizer_config is not None:
-                print(
-                    f"{LoggingMixin.warning_prefix}`optimizer_config` is "
-                    f"set to '{optimizer_config}' but `optimizers` is provided, "
-                    "so `optimizer_config` will be ignored"
-                )
-        else:
-            preset_optimizer = {}
-            if optimizer is not None:
-                if optimizer_config is None:
-                    optimizer_config = {}
-                preset_optimizer = {
-                    "optimizer": optimizer,
-                    "optimizer_config": optimizer_config,
-                }
-            if scheduler is not None:
-                if scheduler_config is None:
-                    scheduler_config = {}
-                preset_optimizer.update(
-                    {"scheduler": scheduler, "scheduler_config": scheduler_config}
-                )
-            if preset_optimizer:
-                optimizers = {"all": preset_optimizer}
-        if optimizers is not None:
-            trainer_config["optimizers"] = optimizers
-        # misc
-        kwargs.update(
-            {
-                "cuda": elements.cuda,
-                "trial": elements.trial,
-                "tracker_config": elements.tracker_config,
-            }
-        )
-        if elements.verbose_level is not None:
-            kwargs["verbose_level"] = elements.verbose_level
-        return cls(kwargs)
+        return cls(elements.to_config())
 
 
 __all__ = ["configs_dict", "Configs", "Elements", "Environment"]
