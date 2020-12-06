1e3ecb571b3a199467e05d16dfc4897a67cb3746
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-12-06 09:03:47 +0800

    ♻️Let `inference` requires `export_file`

--- cli/inference.py
+++ cli/inference.py
@@ -1,4 +1,3 @@
-import os
 import json
 import argparse
 import requests
@@ -18,7 +17,7 @@ if __name__ == "__main__":
     parser.add_argument("--adapter", default="http")
     parser.add_argument("--config", default=None)
     parser.add_argument("--test_file", required=True)
-    parser.add_argument("--export_file", default=None)
+    parser.add_argument("--export_file", required=True)
     args = parse_args(parser.parse_args())
     config = _parse_config(args.config)
     config["file"] = args.test_file
@@ -30,8 +29,4 @@ if __name__ == "__main__":
     )
     if response.status_code != 200:
         raise Exception(f"Status Code {response.status_code}. {response.text}")
-    export_file = args.export_file
-    if export_file is None:
-        export_file = "response.npy"
-    export_path = os.path.join(os.path.dirname(args.test_file), export_file)
-    np.save(export_path, np.array(json.loads(response.content)))
+    np.save(args.export_file, np.array(json.loads(response.content)))
