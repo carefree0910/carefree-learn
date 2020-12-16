ccfbcb2d13d84a502c4e1d27249a10e4a7c36f2b
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-12-16 18:48:10 +0800

    🔖Released `v0.1.8`

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.7.1"
+VERSION = "0.1.8"
 DESCRIPTION = "A minimal Automatic Machine Learning (AutoML) solution for tabular datasets based on PyTorch"
 with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
