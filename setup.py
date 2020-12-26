08a89321e464add4027119d7f345037246775749
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-12-26 12:33:43 +0800

    🔖Released `v0.1.9`

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.8"
+VERSION = "0.1.9"
 DESCRIPTION = "A minimal Automatic Machine Learning (AutoML) solution for tabular datasets based on PyTorch"
 with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
