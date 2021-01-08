924305081e685ef405d5ace190287a2aee9d0cb0
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-01-09 00:22:35 +0800

    ✨Implemented `TreeRNN`

--- cflearn/models/rnn.py
+++ cflearn/models/rnn.py
@@ -7,4 +7,11 @@ class RNN(ModelBase):
     pass
 
 
+@ModelBase.register("tree_rnn")
+@ModelBase.register_pipe("rnn_dndf", extractor="rnn", head="dndf")
+@ModelBase.register_pipe("rnn_fcnn", extractor="rnn", head="fcnn", head_config="pruned")
+class TreeRNN(ModelBase):
+    pass
+
+
 __all__ = ["RNN"]
