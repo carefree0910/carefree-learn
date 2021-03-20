00bd2c44d218f8828132b003eb0a8c0cb21fb187
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-03-21 07:50:59 +0800

    ⚡️Optimized `Transformer`
    `highway` fcnn head should be used in order to prevent rank collapse

--- cflearn/models/transformer.py
+++ cflearn/models/transformer.py
@@ -2,7 +2,7 @@ from .base import ModelBase
 
 
 @ModelBase.register("transformer")
-@ModelBase.register_pipe("transformer", head="linear")
+@ModelBase.register_pipe("transformer", head="fcnn", head_config="highway")
 class Transformer(ModelBase):
     pass
 
