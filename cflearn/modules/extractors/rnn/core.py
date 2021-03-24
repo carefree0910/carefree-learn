be974dffb676da4e3e1be4438b3f11bfd401da1b
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-03-24 19:47:25 +0800

    🐛Fixed `RNN` when `bidirectional` is `True`

--- cflearn/modules/extractors/rnn/core.py
+++ cflearn/modules/extractors/rnn/core.py
@@ -32,6 +32,7 @@ class RNN(ExtractorBase):
         rnn_base = rnn_dict[cell]
         input_dimensions = [self.in_dim]
         self.hidden_size = cell_config["hidden_size"]
+        self.bidirectional = cell_config.setdefault("bidirectional", False)
         input_dimensions += [self.hidden_size] * (num_layers - 1)
         rnn_list = []
         for dim in input_dimensions:
@@ -51,7 +52,9 @@ class RNN(ExtractorBase):
 
     @property
     def out_dim(self) -> int:
-        return self.hidden_size
+        if not self.bidirectional:
+            return self.hidden_size
+        return 2 * self.hidden_size
 
     def forward(self, net: torch.Tensor) -> torch.Tensor:
         for rnn in self.rnn_list:
