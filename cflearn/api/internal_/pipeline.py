c60ff7a7d9fcff73c7284e23b12329d4ee660efa
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-04-19 22:50:47 +0800

    ✨🚧Implemented `DLPipeline`
    Which might need to be inherited by all deep learning pipelines in the future

--- cflearn/api/internal_/pipeline.py
+++ cflearn/api/internal_/pipeline.py
@@ -1,24 +1,35 @@
+import os
+import json
 import torch
 
 from abc import abstractmethod
 from abc import ABC
+from abc import ABCMeta
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Type
 from typing import Union
 from typing import Optional
 from cftool.misc import shallow_copy_dict
+from cftool.misc import lock_manager
+from cftool.misc import Saving
 
 from .trainer import make_trainer
 from ...types import data_type
 from ...types import np_dict_type
 from ...types import states_callback_type
+from ...trainer import get_sorted_checkpoints
 from ...trainer import Trainer
 from ...trainer import DeviceInfo
+from ...protocol import ONNX
 from ...protocol import LossProtocol
 from ...protocol import ModelProtocol
 from ...protocol import InferenceProtocol
 from ...protocol import DataLoaderProtocol
+from ...constants import SCORES_FILE
+from ...constants import WARNING_PREFIX
+from ...misc.toolkit import eval_context
 
 
 class PipelineProtocol(ABC):
@@ -26,6 +37,7 @@ class PipelineProtocol(ABC):
     model: ModelProtocol
     trainer: Trainer
     inference: InferenceProtocol
+    inference_base: Type[InferenceProtocol]
     device_info: DeviceInfo
 
     train_loader: DataLoaderProtocol
@@ -194,6 +206,173 @@ class PipelineProtocol(ABC):
         pass
 
 
+class DLPipeline(PipelineProtocol, metaclass=ABCMeta):
+    config: Dict[str, Any]
+    input_dim: Optional[int]
+
+    @abstractmethod
+    def _prepare_modules(self) -> None:
+        pass
+
+    def _save_misc(self, export_folder: str, retain_data: bool) -> float:
+        os.makedirs(export_folder, exist_ok=True)
+        # final results
+        final_results = self.trainer.final_results
+        if final_results is None:
+            raise ValueError("`final_results` are not generated yet")
+        with open(os.path.join(export_folder, self.final_results_file), "w") as f:
+            json.dump(final_results, f)
+        # config bundle
+        config_bundle = {
+            "config": shallow_copy_dict(self.config),
+            "device_info": self.device_info,
+            "input_dim": self.input_dim,
+        }
+        Saving.save_dict(config_bundle, self.config_bundle_name, export_folder)
+        return final_results.final_score
+
+    def save(
+        self,
+        export_folder: str,
+        *,
+        compress: bool = True,
+        retain_data: bool = False,
+        remove_original: bool = True,
+    ) -> "DLPipeline":
+        abs_folder = os.path.abspath(export_folder)
+        base_folder = os.path.dirname(abs_folder)
+        with lock_manager(base_folder, [export_folder]):
+            score = self._save_misc(export_folder, retain_data)
+            self.trainer.save_checkpoint(score, export_folder)
+            if self.inference is None:
+                raise ValueError("`inference` is not yet generated")
+            if compress:
+                Saving.compress(abs_folder, remove_original=remove_original)
+        return self
+
+    @classmethod
+    def _load_infrastructure(cls, export_folder: str) -> "DLPipeline":
+        config_bundle = Saving.load_dict(cls.config_bundle_name, export_folder)
+        config = config_bundle["config"]
+        config["in_loading"] = True
+        m = cls(**config)
+        m.device_info = DeviceInfo(*config_bundle["device_info"])
+        m.input_dim = config_bundle["input_dim"]
+        return m
+
+    @classmethod
+    def load(
+        cls,
+        export_folder: str,
+        *,
+        compress: bool = True,
+        states_callback: states_callback_type = None,
+    ) -> "DLPipeline":
+        base_folder = os.path.dirname(os.path.abspath(export_folder))
+        with lock_manager(base_folder, [export_folder]):
+            with Saving.compress_loader(export_folder, compress):
+                m = cls._load_infrastructure(export_folder)
+                m._prepare_modules()
+                # restore checkpoint
+                score_path = os.path.join(export_folder, SCORES_FILE)
+                checkpoints = get_sorted_checkpoints(score_path)
+                if not checkpoints:
+                    msg = f"{WARNING_PREFIX}no model file found in {export_folder}"
+                    raise ValueError(msg)
+                checkpoint_path = os.path.join(export_folder, checkpoints[0])
+                states = torch.load(checkpoint_path, map_location=m.device)
+                if states_callback is not None:
+                    states = states_callback(m, states)
+                m.model.load_state_dict(states)
+        return m
+
+    def to_onnx(
+        self,
+        export_folder: str,
+        dynamic_axes: Optional[Union[List[int], Dict[int, str]]] = None,
+        *,
+        compress: bool = True,
+        remove_original: bool = True,
+        verbose: bool = True,
+        **kwargs: Any,
+    ) -> "DLPipeline":
+        # prepare
+        model = self.model.cpu()
+        input_sample = self.trainer.input_sample
+        input_sample.pop("batch_indices")
+        with eval_context(model):
+            forward_results = model(0, shallow_copy_dict(input_sample))
+        input_names = sorted(input_sample.keys())
+        output_names = sorted(forward_results.keys())
+        # setup
+        kwargs = shallow_copy_dict(kwargs)
+        kwargs["input_names"] = input_names
+        kwargs["output_names"] = output_names
+        kwargs["opset_version"] = 11
+        kwargs["export_params"] = True
+        kwargs["do_constant_folding"] = True
+        if dynamic_axes is None:
+            dynamic_axes = {}
+        elif isinstance(dynamic_axes, list):
+            dynamic_axes = {axis: f"axis.{axis}" for axis in dynamic_axes}
+        dynamic_axes[0] = "batch_size"
+        dynamic_axes_settings = {}
+        for name in input_names + output_names:
+            dynamic_axes_settings[name] = dynamic_axes
+        kwargs["dynamic_axes"] = dynamic_axes_settings
+        kwargs["verbose"] = verbose
+        # export
+        abs_folder = os.path.abspath(export_folder)
+        base_folder = os.path.dirname(abs_folder)
+
+        class ONNXWrapper(torch.nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
+                self.model = model
+
+            def forward(self, batch: Dict[str, Any]) -> Any:
+                return self.model(0, batch)
+
+        with lock_manager(base_folder, [export_folder]):
+            self._save_misc(export_folder, False)
+            with open(os.path.join(export_folder, self.onnx_kwargs_file), "w") as f:
+                json.dump(kwargs, f)
+            onnx = ONNXWrapper()
+            onnx_path = os.path.join(export_folder, self.onnx_file)
+            with eval_context(onnx):
+                torch.onnx.export(
+                    onnx,
+                    (input_sample, {}),
+                    onnx_path,
+                    **shallow_copy_dict(kwargs),
+                )
+            if compress:
+                Saving.compress(abs_folder, remove_original=remove_original)
+        self.model.to(self.device)
+        return self
+
+    @classmethod
+    def from_onnx(
+        cls,
+        export_folder: str,
+        *,
+        compress: bool = True,
+    ) -> "DLPipeline":
+        base_folder = os.path.dirname(os.path.abspath(export_folder))
+        with lock_manager(base_folder, [export_folder]):
+            with Saving.compress_loader(export_folder, compress):
+                m = cls._load_infrastructure(export_folder)
+                with open(os.path.join(export_folder, cls.onnx_kwargs_file), "r") as f:
+                    onnx_kwargs = json.load(f)
+                onnx = ONNX(
+                    onnx_path=os.path.join(export_folder, cls.onnx_file),
+                    output_names=onnx_kwargs["output_names"],
+                )
+                m.inference = cls.inference_base(onnx=onnx)
+        return m
+
+
 __all__ = [
     "PipelineProtocol",
+    "DLPipeline",
 ]
--- cflearn/api/ml/pipeline.py
+++ cflearn/api/ml/pipeline.py
@@ -1,7 +1,6 @@
 import os
 import copy
 import json
-import torch
 
 import numpy as np
 
@@ -15,27 +14,18 @@ from functools import partial
 from cfdata.tabular import ColumnTypes
 from cfdata.tabular import TabularData
 from cftool.ml import ModelPattern
-from cftool.misc import shallow_copy_dict
-from cftool.misc import lock_manager
-from cftool.misc import Saving
 
 from ...types import data_type
 from ...types import np_dict_type
 from ...types import states_callback_type
-from ...trainer import get_sorted_checkpoints
-from ...trainer import DeviceInfo
 from ...protocol import loss_dict
-from ...protocol import ONNX
 from ...protocol import MetricProtocol
 from ...protocol import InferenceOutputs
-from ...constants import SCORES_FILE
-from ...constants import WARNING_PREFIX
 from ...constants import PREDICTIONS_KEY
-from ..internal_.pipeline import PipelineProtocol
+from ..internal_.pipeline import DLPipeline
 from ...misc.toolkit import is_float
 from ...misc.toolkit import get_arguments
 from ...misc.toolkit import prepare_workplace_from
-from ...misc.toolkit import eval_context
 from ...misc.internal_ import MLData
 from ...misc.internal_ import MLLoader
 from ...misc.internal_ import MLInference
@@ -43,9 +33,10 @@ from ...models.ml.encoders import Encoder
 from ...models.ml.protocol import MLModel
 
 
-class SimplePipeline(PipelineProtocol):
+class SimplePipeline(DLPipeline):
     model: MLModel
     inference: MLInference
+    inference_base = MLInference
 
     train_loader: MLLoader
     train_loader_copy: MLLoader
@@ -141,7 +132,7 @@ class SimplePipeline(PipelineProtocol):
         )
         self.core_name = core_name
         self.core_config = core_config or {}
-        self.input_dim: Optional[int] = None
+        self.input_dim = None
         self.output_dim = output_dim
         self.is_classification = is_classification
         self.only_categorical = only_categorical
@@ -293,163 +284,6 @@ class SimplePipeline(PipelineProtocol):
             predict_method=_predict,
             predict_prob_method=_predict_prob,
         )
-
-    def _save_misc(self, export_folder: str, retain_data: bool) -> float:
-        os.makedirs(export_folder, exist_ok=True)
-        # final results
-        final_results = self.trainer.final_results
-        if final_results is None:
-            raise ValueError("`final_results` are not generated yet")
-        with open(os.path.join(export_folder, self.final_results_file), "w") as f:
-            json.dump(final_results, f)
-        # config bundle
-        config_bundle = {
-            "config": shallow_copy_dict(self.config),
-            "device_info": self.device_info,
-            "input_dim": self.input_dim,
-        }
-        Saving.save_dict(config_bundle, self.config_bundle_name, export_folder)
-        return final_results.final_score
-
-    def save(
-        self,
-        export_folder: str,
-        *,
-        compress: bool = True,
-        retain_data: bool = False,
-        remove_original: bool = True,
-    ) -> "SimplePipeline":
-        abs_folder = os.path.abspath(export_folder)
-        base_folder = os.path.dirname(abs_folder)
-        with lock_manager(base_folder, [export_folder]):
-            score = self._save_misc(export_folder, retain_data)
-            self.trainer.save_checkpoint(score, export_folder)
-            if self.inference is None:
-                raise ValueError("`inference` is not yet generated")
-            if compress:
-                Saving.compress(abs_folder, remove_original=remove_original)
-        return self
-
-    @classmethod
-    def _load_infrastructure(cls, export_folder: str) -> "SimplePipeline":
-        config_bundle = Saving.load_dict(cls.config_bundle_name, export_folder)
-        config = config_bundle["config"]
-        config["in_loading"] = True
-        m = cls(**config)
-        m.device_info = DeviceInfo(*config_bundle["device_info"])
-        m.input_dim = config_bundle["input_dim"]
-        return m
-
-    @classmethod
-    def load(
-        cls,
-        export_folder: str,
-        *,
-        compress: bool = True,
-        states_callback: states_callback_type = None,
-    ) -> "SimplePipeline":
-        base_folder = os.path.dirname(os.path.abspath(export_folder))
-        with lock_manager(base_folder, [export_folder]):
-            with Saving.compress_loader(export_folder, compress):
-                m = cls._load_infrastructure(export_folder)
-                m._prepare_modules()
-                # restore checkpoint
-                score_path = os.path.join(export_folder, SCORES_FILE)
-                checkpoints = get_sorted_checkpoints(score_path)
-                if not checkpoints:
-                    msg = f"{WARNING_PREFIX}no model file found in {export_folder}"
-                    raise ValueError(msg)
-                checkpoint_path = os.path.join(export_folder, checkpoints[0])
-                states = torch.load(checkpoint_path, map_location=m.device)
-                if states_callback is not None:
-                    states = states_callback(m, states)
-                m.model.load_state_dict(states)
-        return m
-
-    def to_onnx(
-        self,
-        export_folder: str,
-        dynamic_axes: Optional[Union[List[int], Dict[int, str]]] = None,
-        *,
-        compress: bool = True,
-        remove_original: bool = True,
-        verbose: bool = True,
-        **kwargs: Any,
-    ) -> "SimplePipeline":
-        # prepare
-        model = self.model.cpu()
-        input_sample = self.trainer.input_sample
-        input_sample.pop("batch_indices")
-        with eval_context(model):
-            forward_results = model(0, shallow_copy_dict(input_sample))
-        input_names = sorted(input_sample.keys())
-        output_names = sorted(forward_results.keys())
-        # setup
-        kwargs = shallow_copy_dict(kwargs)
-        kwargs["input_names"] = input_names
-        kwargs["output_names"] = output_names
-        kwargs["opset_version"] = 11
-        kwargs["export_params"] = True
-        kwargs["do_constant_folding"] = True
-        if dynamic_axes is None:
-            dynamic_axes = {}
-        elif isinstance(dynamic_axes, list):
-            dynamic_axes = {axis: f"axis.{axis}" for axis in dynamic_axes}
-        dynamic_axes[0] = "batch_size"
-        dynamic_axes_settings = {}
-        for name in input_names + output_names:
-            dynamic_axes_settings[name] = dynamic_axes
-        kwargs["dynamic_axes"] = dynamic_axes_settings
-        kwargs["verbose"] = verbose
-        # export
-        abs_folder = os.path.abspath(export_folder)
-        base_folder = os.path.dirname(abs_folder)
-
-        class ONNXWrapper(torch.nn.Module):
-            def __init__(self) -> None:
-                super().__init__()
-                self.model = model
-
-            def forward(self, batch: Dict[str, Any]) -> Any:
-                return self.model(0, batch)
-
-        with lock_manager(base_folder, [export_folder]):
-            self._save_misc(export_folder, False)
-            with open(os.path.join(export_folder, self.onnx_kwargs_file), "w") as f:
-                json.dump(kwargs, f)
-            onnx = ONNXWrapper()
-            onnx_path = os.path.join(export_folder, self.onnx_file)
-            with eval_context(onnx):
-                torch.onnx.export(
-                    onnx,
-                    (input_sample, {}),
-                    onnx_path,
-                    **shallow_copy_dict(kwargs),
-                )
-            if compress:
-                Saving.compress(abs_folder, remove_original=remove_original)
-        self.model.to(self.device)
-        return self
-
-    @classmethod
-    def from_onnx(
-        cls,
-        export_folder: str,
-        *,
-        compress: bool = True,
-    ) -> "SimplePipeline":
-        base_folder = os.path.dirname(os.path.abspath(export_folder))
-        with lock_manager(base_folder, [export_folder]):
-            with Saving.compress_loader(export_folder, compress):
-                m = cls._load_infrastructure(export_folder)
-                with open(os.path.join(export_folder, cls.onnx_kwargs_file), "r") as f:
-                    onnx_kwargs = json.load(f)
-                onnx = ONNX(
-                    onnx_path=os.path.join(export_folder, cls.onnx_file),
-                    output_names=onnx_kwargs["output_names"],
-                )
-                m.inference = MLInference(onnx=onnx)
-        return m
 
 
 class CarefreePipeline(SimplePipeline):
