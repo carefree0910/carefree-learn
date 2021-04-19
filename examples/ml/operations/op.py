22215babf945305b1baa6de5889e5df55ac6aa4c
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-04-19 23:51:59 +0800

    ✅Introduced `operations` example

---
+++ examples/ml/operations/op.py
@@ -0,0 +1,40 @@
+import torch
+import cflearn
+
+import numpy as np
+
+
+# for reproduction
+np.random.seed(142857)
+torch.manual_seed(142857)
+
+# prepare
+dim = 5
+num_data = 10000
+metrics = ["mae", "mse"]
+
+x = np.random.random([num_data, dim, 1]) * 2.0
+y_add = np.sum(x, axis=1)
+y_prod = np.prod(x, axis=1)
+y_mix = np.hstack([y_add, y_prod])
+
+kwargs = {
+    "output_dim": 1,
+    "is_classification": False,
+    "loss_name": "mae",
+    "num_history": dim,
+    "metric_names": metrics,
+    "tqdm_settings": {"use_tqdm": True},
+}
+
+# add
+linear = cflearn.ml.SimplePipeline("linear", **kwargs)  # type: ignore
+fcnn = cflearn.ml.SimplePipeline("fcnn", **kwargs)  # type: ignore
+rnn = cflearn.ml.SimplePipeline("rnn", **kwargs)  # type: ignore
+linear.fit(x, y_add)
+fcnn.fit(x, y_add)
+rnn.fit(x, y_add)
+cflearn.ml.evaluate(x, y_add, metrics=metrics, pipelines=[linear, fcnn, rnn])
+
+linear_core = linear.model.core.net
+print(f"w: {linear_core.weight.data}, b: {linear_core.bias.data}")  # type: ignore
