03ab28117d07095c95b532e1abddf86c6142c7d7
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-04-19 19:10:06 +0800

    ✨🚧Implemented `SimplePipeline`
    Which might need to be inherited by `MLPipeline` in the future

--- cflearn/api/ml/pipeline.py
+++ cflearn/api/ml/pipeline.py
@@ -42,6 +42,381 @@ from ...models.ml.encoders import Encoder
 from ...models.ml.protocol import MLModel
 
 
+class SimplePipeline(PipelineProtocol):
+    model: MLModel
+    inference: MLInference
+
+    train_loader: MLLoader
+    train_loader_copy: MLLoader
+    valid_loader: MLLoader
+
+    def __init__(
+        self,
+        core_name: str = "fcnn",
+        core_config: Optional[Dict[str, Any]] = None,
+        *,
+        output_dim: int,
+        is_classification: bool,
+        loss_name: str,
+        loss_config: Optional[Dict[str, Any]] = None,
+        # valid split
+        valid_split: Optional[Union[int, float]] = None,
+        min_valid_split: int = 100,
+        max_valid_split: int = 10000,
+        max_valid_split_ratio: float = 0.5,
+        valid_split_order: str = "auto",
+        # data loader
+        num_history: int = 1,
+        shuffle_train: bool = True,
+        shuffle_valid: bool = False,
+        batch_size: int = 128,
+        valid_batch_size: int = 512,
+        # trainer
+        state_config: Optional[Dict[str, Any]] = None,
+        num_epoch: int = 40,
+        max_epoch: int = 1000,
+        valid_portion: float = 1.0,
+        amp: bool = False,
+        clip_norm: float = 0.0,
+        metric_names: Optional[Union[str, List[str]]] = None,
+        metric_configs: Optional[Dict[str, Any]] = None,
+        monitor_names: Optional[Union[str, List[str]]] = None,
+        monitor_configs: Optional[Dict[str, Any]] = None,
+        callback_names: Optional[Union[str, List[str]]] = None,
+        callback_configs: Optional[Dict[str, Any]] = None,
+        optimizer_settings: Optional[Dict[str, Dict[str, Any]]] = None,
+        workplace: str = "_logs",
+        rank: Optional[int] = None,
+        tqdm_settings: Optional[Dict[str, Any]] = None,
+        # misc
+        in_loading: bool = False,
+    ):
+        self.config = get_arguments()
+        self.config.pop("self")
+        self.config.pop("__class__")
+        super().__init__(
+            loss_name=loss_name,
+            loss_config=loss_config,
+            valid_split=valid_split,
+            min_valid_split=min_valid_split,
+            max_valid_split=max_valid_split,
+            max_valid_split_ratio=max_valid_split_ratio,
+            valid_split_order=valid_split_order,
+            num_history=num_history,
+            shuffle_train=shuffle_train,
+            shuffle_valid=shuffle_valid,
+            batch_size=batch_size,
+            valid_batch_size=valid_batch_size,
+            state_config=state_config,
+            num_epoch=num_epoch,
+            max_epoch=max_epoch,
+            valid_portion=valid_portion,
+            amp=amp,
+            clip_norm=clip_norm,
+            metric_names=metric_names,
+            metric_configs=metric_configs,
+            monitor_names=monitor_names,
+            monitor_configs=monitor_configs,
+            callback_names=callback_names,
+            callback_configs=callback_configs,
+            optimizer_settings=optimizer_settings,
+            workplace=workplace,
+            rank=rank,
+            tqdm_settings=tqdm_settings,
+            in_loading=in_loading,
+        )
+        self.core_name = core_name
+        self.core_config = core_config or {}
+        self.output_dim = output_dim
+        self.is_classification = is_classification
+
+    def _prepare_data(
+        self,
+        x: data_type,
+        y: data_type,
+        x_valid: data_type = None,
+        y_valid: data_type = None,
+    ) -> None:
+        self.input_dim = x.shape[1]
+        train_loader = MLLoader(
+            MLData(x, y),
+            name="train",
+            shuffle=self.shuffle_train,
+            batch_size=self.batch_size,
+        )
+        train_loader_copy = copy.deepcopy(train_loader)
+        train_loader_copy.shuffle = False
+        if x_valid is None or y_valid is None:
+            valid_loader = train_loader_copy
+        else:
+            valid_loader = MLLoader(
+                MLData(x_valid, y_valid),
+                name="valid",
+                shuffle=self.shuffle_valid,
+                batch_size=self.valid_batch_size,
+            )
+        self.train_loader = train_loader
+        self.train_loader_copy = train_loader_copy
+        self.valid_loader = valid_loader
+
+    def _prepare_modules(self) -> None:
+        if not self.in_loading:
+            workplace = prepare_workplace_from(self.trainer_config["workplace"])
+            self.trainer_config["workplace"] = workplace
+            self.trainer_config["metrics_log_file"] = self.metrics_log_file
+            os.makedirs(workplace, exist_ok=True)
+            with open(os.path.join(workplace, self.configs_file), "w") as f:
+                json.dump(self.config, f)
+        # prepare
+        self.loss = loss_dict[self.loss_name](**(self.loss_config or {}))
+        self.model = MLModel(
+            self.input_dim,
+            self.output_dim,
+            self.num_history,
+            encoder=None,
+            numerical_columns_mapping={i: i for i in range(self.input_dim)},
+            categorical_columns_mapping={},
+            use_one_hot=False,
+            use_embedding=False,
+            only_categorical=False,
+            core_name=self.core_name,
+            core_config=self.core_config,
+        )
+        self.inference = MLInference(model=self.model)
+
+    def _prepare_trainer_defaults(self) -> None:
+        # set some trainer defaults to ml tasks which work well in practice
+        if self.trainer_config["monitor_names"] is None:
+            self.trainer_config["monitor_names"] = ["mean_std", "plateau"]
+        auto_callback_setup = False
+        tqdm_settings = self.trainer_config["tqdm_settings"]
+        callback_names = self.trainer_config["callback_names"]
+        optimizer_settings = self.trainer_config["optimizer_settings"]
+        if callback_names is None:
+            callback_names = []
+            auto_callback_setup = True
+        if isinstance(callback_names, str):
+            callback_names = [callback_names]
+        if "log_metrics_msg" not in callback_names and auto_callback_setup:
+            if tqdm_settings is None or not tqdm_settings.get("use_tqdm", False):
+                callback_names.append("log_metrics_msg")
+        if "_default_opt_settings" not in callback_names:
+            callback_names.append("_default_opt_settings")
+        if "_inject_loader_name" not in callback_names:
+            callback_names.append("_inject_loader_name")
+        if optimizer_settings is None:
+            optimizer_settings = {"all": {"optimizer": "adam", "scheduler": "warmup"}}
+        self.trainer_config["tqdm_settings"] = tqdm_settings
+        self.trainer_config["callback_names"] = callback_names
+        self.trainer_config["optimizer_settings"] = optimizer_settings
+
+    # TODO : support sample weights
+    def _before_loop(
+        self,
+        x: data_type,
+        y: data_type = None,
+        x_valid: data_type = None,
+        y_valid: data_type = None,
+        cuda: Optional[str] = None,
+    ) -> None:
+        # internal preparation
+        self._prepare_data(x, y, x_valid, y_valid)
+        self._prepare_modules()
+        self._prepare_trainer_defaults()
+
+    def predict(
+        self,
+        x: np.ndarray,
+        *,
+        batch_size: int = 128,
+        transform_kwargs: Optional[Dict[str, Any]] = None,
+        **predict_kwargs: Any,
+    ) -> np_dict_type:
+        loader = MLLoader(
+            MLData(x, None),
+            shuffle=False,
+            batch_size=batch_size,
+        )
+        predict_kwargs = shallow_copy_dict(predict_kwargs)
+        if self.inference.onnx is None:
+            predict_kwargs["device"] = self.device
+        outputs = self.inference.get_outputs(loader, **predict_kwargs)
+        return outputs.forward_results
+
+    def to_pattern(
+        self,
+        *,
+        pre_process: Optional[Callable] = None,
+        **predict_kwargs: Any,
+    ) -> ModelPattern:
+        def _predict(x: np.ndarray) -> np.ndarray:
+            if pre_process is not None:
+                x = pre_process(x)
+            predictions = self.predict(x, **predict_kwargs)[PREDICTIONS_KEY]
+            if self.is_classification:
+                return np.argmax(predictions, axis=1)[..., None]
+            return predictions
+
+        def _predict_prob(x: np.ndarray) -> np.ndarray:
+            if not self.is_classification:
+                msg = "`predict_prob` should not be called in regression tasks"
+                raise ValueError(msg)
+            if pre_process is not None:
+                x = pre_process(x)
+            logits = self.predict(x, **predict_kwargs)[PREDICTIONS_KEY]
+            return MetricProtocol.softmax(logits)
+
+        return ModelPattern(
+            init_method=lambda: self,
+            predict_method=_predict,
+            predict_prob_method=_predict_prob,
+        )
+
+    def _save_misc(self, export_folder: str, retain_data: bool) -> float:
+        os.makedirs(export_folder, exist_ok=True)
+        # final results
+        final_results = self.trainer.final_results
+        if final_results is None:
+            raise ValueError("`final_results` are not generated yet")
+        with open(os.path.join(export_folder, self.final_results_file), "w") as f:
+            json.dump(final_results, f)
+        # config bundle
+        config_bundle = {
+            "config": shallow_copy_dict(self.config),
+            "device_info": self.device_info,
+            "input_dim": self.input_dim,
+        }
+        Saving.save_dict(config_bundle, self.config_bundle_name, export_folder)
+        return final_results.final_score
+
+    def save(
+        self,
+        export_folder: str,
+        *,
+        compress: bool = True,
+        retain_data: bool = False,
+        remove_original: bool = True,
+    ) -> "MLPipeline":
+        abs_folder = os.path.abspath(export_folder)
+        base_folder = os.path.dirname(abs_folder)
+        with lock_manager(base_folder, [export_folder]):
+            score = self._save_misc(export_folder, retain_data)
+            self.trainer.save_checkpoint(score, export_folder)
+            if self.inference is None:
+                raise ValueError("`inference` is not yet generated")
+            if compress:
+                Saving.compress(abs_folder, remove_original=remove_original)
+        return self
+
+    @classmethod
+    def _load_infrastructure(cls, export_folder: str) -> "MLPipeline":
+        config_bundle = Saving.load_dict(cls.config_bundle_name, export_folder)
+        config = config_bundle["config"]
+        config["in_loading"] = True
+        m = cls(**config)
+        m.device_info = DeviceInfo(*config_bundle["device_info"])
+        m.input_dim = config_bundle["input_dim"]
+        return m
+
+    @classmethod
+    def load(cls, export_folder: str, *, compress: bool = True) -> "MLPipeline":
+        base_folder = os.path.dirname(os.path.abspath(export_folder))
+        with lock_manager(base_folder, [export_folder]):
+            with Saving.compress_loader(export_folder, compress):
+                m = cls._load_infrastructure(export_folder)
+                m._prepare_modules()
+                # restore checkpoint
+                score_path = os.path.join(export_folder, SCORES_FILE)
+                checkpoints = get_sorted_checkpoints(score_path)
+                if not checkpoints:
+                    msg = f"{WARNING_PREFIX}no model file found in {export_folder}"
+                    raise ValueError(msg)
+                checkpoint_path = os.path.join(export_folder, checkpoints[0])
+                states = torch.load(checkpoint_path, map_location=m.device)
+                m.model.load_state_dict(states)
+        return m
+
+    def to_onnx(
+        self,
+        export_folder: str,
+        dynamic_axes: Optional[Union[List[int], Dict[int, str]]] = None,
+        *,
+        compress: bool = True,
+        remove_original: bool = True,
+        verbose: bool = True,
+        **kwargs: Any,
+    ) -> "MLPipeline":
+        # prepare
+        model = self.model.cpu()
+        input_sample = self.trainer.input_sample
+        input_sample.pop("batch_indices")
+        with eval_context(model):
+            forward_results = model(0, shallow_copy_dict(input_sample))
+        input_names = sorted(input_sample.keys())
+        output_names = sorted(forward_results.keys())
+        # setup
+        kwargs = shallow_copy_dict(kwargs)
+        kwargs["input_names"] = input_names
+        kwargs["output_names"] = output_names
+        kwargs["opset_version"] = 11
+        kwargs["export_params"] = True
+        kwargs["do_constant_folding"] = True
+        if dynamic_axes is None:
+            dynamic_axes = {}
+        elif isinstance(dynamic_axes, list):
+            dynamic_axes = {axis: f"axis.{axis}" for axis in dynamic_axes}
+        dynamic_axes[0] = "batch_size"
+        dynamic_axes_settings = {}
+        for name in input_names + output_names:
+            dynamic_axes_settings[name] = dynamic_axes
+        kwargs["dynamic_axes"] = dynamic_axes_settings
+        kwargs["verbose"] = verbose
+        # export
+        abs_folder = os.path.abspath(export_folder)
+        base_folder = os.path.dirname(abs_folder)
+
+        class ONNXWrapper(torch.nn.Module):
+            def __init__(self) -> None:
+                super().__init__()
+                self.model = model
+
+            def forward(self, batch: Dict[str, Any]) -> Any:
+                return self.model(0, batch)
+
+        with lock_manager(base_folder, [export_folder]):
+            self._save_misc(export_folder, False)
+            with open(os.path.join(export_folder, self.onnx_kwargs_file), "w") as f:
+                json.dump(kwargs, f)
+            onnx = ONNXWrapper()
+            onnx_path = os.path.join(export_folder, self.onnx_file)
+            with eval_context(onnx):
+                torch.onnx.export(
+                    onnx,
+                    (input_sample, {}),
+                    onnx_path,
+                    **shallow_copy_dict(kwargs),
+                )
+            if compress:
+                Saving.compress(abs_folder, remove_original=remove_original)
+        self.model.to(self.device)
+        return self
+
+    @classmethod
+    def from_onnx(cls, export_folder: str, *, compress: bool = True) -> "MLPipeline":
+        base_folder = os.path.dirname(os.path.abspath(export_folder))
+        with lock_manager(base_folder, [export_folder]):
+            with Saving.compress_loader(export_folder, compress):
+                m = cls._load_infrastructure(export_folder)
+                with open(os.path.join(export_folder, cls.onnx_kwargs_file), "r") as f:
+                    onnx_kwargs = json.load(f)
+                onnx = ONNX(
+                    onnx_path=os.path.join(export_folder, cls.onnx_file),
+                    output_names=onnx_kwargs["output_names"],
+                )
+                m.inference = MLInference(onnx=onnx)
+        return m
+
+
 class MLPipeline(PipelineProtocol):
     data: TabularData
     model: MLModel
@@ -558,4 +933,5 @@ class MLPipeline(PipelineProtocol):
 
 __all__ = [
     "MLPipeline",
+    "SimplePipeline",
 ]
---
+++ examples/ml/simple/iris.py
@@ -0,0 +1,25 @@
+import cflearn
+
+import numpy as np
+
+from cfdata.tabular import TabularDataset
+
+
+metrics = ["acc", "auc"]
+x, y = TabularDataset.iris().xy
+m = cflearn.ml.SimplePipeline(
+    output_dim=3,
+    is_classification=True,
+    loss_name="focal",
+    metric_names=metrics,
+)
+m.fit(x, y)
+cflearn.ml.evaluate(x, y, metrics=metrics, pipelines=m)
+
+predictions = m.predict(x)[cflearn.PREDICTIONS_KEY]
+m.save("iris")
+m2 = cflearn.ml.SimplePipeline.load("iris")
+assert np.allclose(predictions, m2.predict(x)[cflearn.PREDICTIONS_KEY])
+m.to_onnx("iris_onnx")
+m3 = cflearn.ml.SimplePipeline.from_onnx("iris_onnx")
+assert np.allclose(predictions, m3.predict(x)[cflearn.PREDICTIONS_KEY])
