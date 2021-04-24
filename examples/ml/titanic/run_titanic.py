c5828332184762671b495025c50846864cb79978
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-04-24 11:40:48 +0800

    ✅Enhanced `run_titanic`

--- examples/ml/titanic/run_titanic.py
+++ examples/ml/titanic/run_titanic.py
@@ -10,16 +10,16 @@ test_file = os.path.join(file_folder, "test.csv")
 m = cflearn.ml.CarefreePipeline(data_config={"label_name": "Survived"})
 m.fit(train_file)
 results = m.predict(test_file, make_loader_kwargs={"contains_labels": False})
-predictions = results["predictions"].argmax(1)
+predictions = results[cflearn.PREDICTIONS_KEY]
 
 export_folder = "titanic"
 m.save(export_folder)
 m2 = cflearn.ml.CarefreePipeline.load(export_folder)
 results = m.predict(test_file, make_loader_kwargs={"contains_labels": False})
-assert np.allclose(predictions, results["predictions"].argmax(1))
+assert np.allclose(predictions, results[cflearn.PREDICTIONS_KEY])
 
 onnx_folder = "titanic_onnx"
 m.to_onnx(onnx_folder)
 m3 = cflearn.ml.CarefreePipeline.from_onnx(onnx_folder)
 results = m.predict(test_file, make_loader_kwargs={"contains_labels": False})
-assert np.allclose(predictions, results["predictions"].argmax(1))
+assert np.allclose(predictions, results[cflearn.PREDICTIONS_KEY])
