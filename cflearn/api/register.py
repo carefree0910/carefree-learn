0dcde51287ccd5403eb90666cc7c250cc6be1ab4
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-04-26 16:45:17 +0800

    ✨Implemented `register_metric`

--- cflearn/api/register.py
+++ cflearn/api/register.py
@@ -3,6 +3,7 @@ from cfdata.tabular.processors.base import Processor
 
 from ..modules import *
 from ..protocol import LossProtocol
+from ..protocol import MetricProtocol
 from ..misc.toolkit import Initializer
 
 
@@ -22,12 +23,17 @@ def register_loss(name: str) -> Callable[[Type], Type]:
     return LossProtocol.register(name)
 
 
+def register_metric(name: str) -> Callable[[Type], Type]:
+    return MetricProtocol.register(name)
+
+
 __all__ = [
     "register_optimizer",
     "register_scheduler",
     "register_initializer",
     "register_processor",
     "register_loss",
+    "register_metric",
     "Initializer",
     "Processor",
 ]
