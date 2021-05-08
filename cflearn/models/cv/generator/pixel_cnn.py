2c2899267264d654aeba9957cc75821735a7f42a
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-05-08 14:48:07 +0800

    ✨💥Enhanced `PixelCNN`
    It now only supports indices inputs, and its outputs will also be indices

--- cflearn/models/cv/generator/pixel_cnn.py
+++ cflearn/models/cv/generator/pixel_cnn.py
@@ -16,6 +16,7 @@ from ....constants import INPUT_KEY
 from ....constants import PREDICTIONS_KEY
 from ....modules.blocks import get_conv_blocks
 from ....modules.blocks import Conv2d
+from ....modules.blocks import Lambda
 
 
 class MaskedConv2d(Conv2d):
@@ -68,23 +69,24 @@ class PixelCNN(ModelProtocol):
         self,
         in_channels: int,
         num_classes: int,
+        need_embedding: bool = False,
         latent_channels: int = 128,
         *,
         num_layers: int = 6,
         norm_type: str = "batch",
     ):
         super().__init__()
+        if in_channels != 1:
+            raise ValueError("`PixelCNN` requires `in_channels` to be 1")
         self.in_channels = in_channels
         self.num_classes = num_classes
         self.latent_channels = latent_channels
-        in_nc = in_channels
-        blocks: List[nn.Module] = []
-        for i in range(num_layers):
-            mask_type = "A" if i == 0 else "B"
-            blocks.extend(
-                get_conv_blocks(
+
+        def _get_block(in_nc: int, out_nc: int, mask_type: str) -> nn.Sequential:
+            return nn.Sequential(
+                *get_conv_blocks(
                     in_nc,
-                    latent_channels,
+                    out_nc,
                     7,
                     1,
                     bias=False,
@@ -95,7 +97,22 @@ class PixelCNN(ModelProtocol):
                     mask_type=mask_type,
                 )
             )
-            in_nc = latent_channels
+
+        if not need_embedding:
+            self.to_blocks = nn.Sequential(
+                Lambda(lambda t: t.float() / (num_classes - 1), name="normalize"),
+                _get_block(in_channels, latent_channels, "A"),
+            )
+        else:
+            self.to_blocks = nn.Sequential(
+                Lambda(lambda t: t.squeeze(1), name="squeeze"),
+                nn.Embedding(num_classes, latent_channels),
+                Lambda(lambda t: t.permute(0, 3, 1, 2), name="permute"),
+                _get_block(latent_channels, latent_channels, "A"),
+            )
+        blocks: List[nn.Module] = []
+        for i in range(num_layers - 1):
+            blocks.append(_get_block(latent_channels, latent_channels, "B"))
         blocks.append(Conv2d(latent_channels, num_classes, kernel_size=1))
         self.net = nn.Sequential(*blocks)
 
@@ -106,17 +123,18 @@ class PixelCNN(ModelProtocol):
         state: Optional[TrainerState] = None,
         **kwargs: Any,
     ) -> tensor_dict_type:
-        return {PREDICTIONS_KEY: self.net(batch[INPUT_KEY])}
+        net = batch[INPUT_KEY]
+        net = self.to_blocks(net)
+        return {PREDICTIONS_KEY: self.net(net)}
 
-    def sample(self, num_sample: int, img_size: int) -> Tensor:
+    def sample(self, num_sample: int, img_size: int, **kwargs: Any) -> Tensor:
         shape = num_sample, self.in_channels, img_size, img_size
-        sampled = torch.zeros(shape, device=self.device)
+        sampled = torch.zeros(shape, dtype=torch.long, device=self.device)
         for i in range(img_size):
             for j in range(img_size):
-                out = self.net(sampled)
+                out = self.forward(0, {INPUT_KEY: sampled}, **kwargs)[PREDICTIONS_KEY]
                 probabilities = F.softmax(out[:, :, i, j], dim=1).data
-                local_sampled = torch.multinomial(probabilities, 1)
-                sampled[:, :, i, j] = local_sampled.float() / (self.num_classes - 1)
+                sampled[:, :, i, j] = torch.multinomial(probabilities, 1)
         return sampled
 
 
--- examples/cv/generator/mnist_pixel_cnn.py
+++ examples/cv/generator/mnist_pixel_cnn.py
@@ -3,6 +3,7 @@
 import os
 import cflearn
 
+from torchvision.transforms import transforms
 from cflearn.misc.toolkit import to_device
 from cflearn.misc.toolkit import save_images
 from cflearn.misc.toolkit import eval_context
@@ -17,17 +18,23 @@ class PixelCNNCallback(cflearn.ArtifactCallback):
             return None
         batch = next(iter(trainer.validation_loader))
         batch = to_device(batch, trainer.device)
-        original = batch[cflearn.INPUT_KEY]
+        original = batch[cflearn.INPUT_KEY].float() / 255.0
         with eval_context(trainer.model):
-            sampled = trainer.model.sample(len(original), original.shape[2])
+            sampled_indices = trainer.model.sample(len(original), original.shape[2])
+            sampled = sampled_indices.float() / 255.0
         image_folder = self._prepare_folder(trainer)
         save_images(original, os.path.join(image_folder, "original.png"))
         save_images(sampled, os.path.join(image_folder, "sampled.png"))
 
 
 train_loader, valid_loader = cflearn.cv.get_mnist(
-    transform="for_classification",
-    label_callback=lambda batch: (batch[0] * 255).long(),
+    transform=transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Lambda(lambda t: (t * 255).long())
+        ]
+    ),
+    label_callback=lambda batch: batch[0],
 )
 
 m = cflearn.cv.CarefreePipeline(
