81cd5228175682eb3ab41162eca17bffd79b622d
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-05-13 00:10:47 +0800

    ✅Enhanced `mnist_vae` example

--- examples/cv/vae/mnist_vae.py
+++ examples/cv/vae/mnist_vae.py
@@ -18,7 +18,7 @@ class VAECallback(cflearn.ArtifactCallback):
         batch = next(iter(trainer.validation_loader))
         batch = to_device(batch, trainer.device)
         original = batch[cflearn.INPUT_KEY]
-        labels = batch[cflearn.LABEL_KEY]
+        labels = None if num_classes is None else batch[cflearn.LABEL_KEY]
         with eval_context(trainer.model):
             reconstructed = trainer.model.reconstruct(original, labels=labels)
             sampled = trainer.model.sample(len(original))
@@ -27,12 +27,15 @@ class VAECallback(cflearn.ArtifactCallback):
         save_images(reconstructed, os.path.join(image_folder, "reconstructed.png"))
         save_images(sampled, os.path.join(image_folder, "sampled.png"))
         # conditional sampling
+        if num_classes is None:
+            return None
         with eval_context(trainer.model):
-            for i in range(10):
+            for i in range(num_classes):
                 sampled = trainer.model.sample(len(original), class_idx=i)
                 save_images(sampled, os.path.join(image_folder, f"sampled_{i}.png"))
 
 
+num_classes = 10
 train_loader, valid_loader = cflearn.cv.get_mnist(transform="for_generation")
 
 m = cflearn.cv.CarefreePipeline(
@@ -40,7 +43,8 @@ m = cflearn.cv.CarefreePipeline(
     {
         "img_size": 28,
         "in_channels": 1,
-        "num_classes": 10,
+        "num_classes": num_classes,
     },
+    loss_metrics_weights={"kld": 0.001, "mse": 1.0},
 )
 m.fit(train_loader, valid_loader, cuda="1")
