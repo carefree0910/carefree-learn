41d910773a83a320578b3ab2d6b3f2c827928370
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-05-25 00:52:51 +0800

    ✅🚧Introduced `mnist_siren_gan` example

---
+++ examples/cv/gan/mnist_siren_gan.py
@@ -0,0 +1,23 @@
+# type: ignore
+
+import cflearn
+
+
+train_loader, valid_loader = cflearn.cv.get_mnist(transform="for_generation")
+
+m = cflearn.cv.CarefreePipeline(
+    "siren_gan",
+    {"img_size": 28, "in_channels": 1},
+    callback_names="sized_generator",
+    optimizer_settings={
+        "g_parameters": {
+            "optimizer": "adam",
+            "scheduler": "warmup",
+        },
+        "d_parameters": {
+            "optimizer": "adam",
+            "scheduler": "warmup",
+        },
+    },
+)
+m.fit(train_loader, valid_loader, cuda="0")
