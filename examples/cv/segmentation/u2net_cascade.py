fbb80f9a469d89b9a4d5d1cfa861845a7d421525
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-08-01 21:38:55 +0800

    ✅🚧Enhanced `u2net_cascade`

--- examples/cv/segmentation/u2net_cascade.py
+++ examples/cv/segmentation/u2net_cascade.py
@@ -33,18 +33,12 @@ if __name__ == "__main__":
         {
             "in_channels": 3,
             "out_channels": 1,
-            "lv1_model_ckpt_path": "pretrained/model_lite_finetuned.pt",
-            "lite": True,
+            "lv1_model_ckpt_path": "pretrained/model_269926.pt",
+            "lv2_model_config": {"lite": True},
+            "lite": False,
         },
         loss_name="multi_stage_iou",
-        lr=1.0e-4,
-        optimizer_name="sgd",
         scheduler_name="none",
-        optimizer_config={
-            "momentum": 0.9,
-            "weight_decay": 5.0e-4,
-            "nesterov": True,
-        },
         loss_metrics_weights={"iou0": 1.0},
     )
     m.fit(train_loader, valid_loader, cuda="6")
