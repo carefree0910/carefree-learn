a5611ef2fdb2776963eed9f74cafaef880d905b1
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-08-02 17:25:27 +0800

    ✅🚧Enhanced `u2net_cascade`

--- examples/cv/segmentation/u2net_cascade.py
+++ examples/cv/segmentation/u2net_cascade.py
@@ -23,22 +23,22 @@ if __name__ == "__main__":
     prepare()
     train_loader, valid_loader = cflearn.cv.get_image_folder_loaders(
         tgt_folder,
-        batch_size=16,
-        num_workers=4,
+        batch_size=4,
+        num_workers=2,
         transform="for_salient_object_detection",
     )
-    cflearn.MultiStageLoss.register_("iou")
+    cflearn.MultiStageLoss.register_(["bce", "iou"])
     m = cflearn.cv.CarefreePipeline(
         "cascade_u2net",
         {
             "in_channels": 3,
             "out_channels": 1,
             "lv1_model_ckpt_path": "pretrained/model_269926.pt",
+            "lv2_resolution": 512,
             "lv2_model_config": {"lite": True},
             "lite": False,
         },
-        loss_name="multi_stage_iou",
+        loss_name="multi_stage_bce_iou",
         scheduler_name="none",
-        loss_metrics_weights={"iou0": 1.0},
     )
     m.fit(train_loader, valid_loader, cuda="6")
