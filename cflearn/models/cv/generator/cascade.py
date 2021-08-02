c717959f82ec082c4fc89409168cfb37688d3ce9
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-08-02 08:32:36 +0800

    ✨Supported `lv2_resolution` in `CascadeU2Net`

--- cflearn/models/cv/generator/cascade.py
+++ cflearn/models/cv/generator/cascade.py
@@ -4,7 +4,6 @@ from torch import Tensor
 from typing import Any
 from typing import Dict
 from typing import Optional
-from cftool.misc import shallow_copy_dict
 
 from ...bases import CascadeBase
 from ....types import tensor_dict_type
@@ -12,6 +11,7 @@ from ....trainer import TrainerState
 from ....protocol import ModelProtocol
 from ....constants import INPUT_KEY
 from ....constants import PREDICTIONS_KEY
+from ....misc.toolkit import align_to
 from ....misc.toolkit import imagenet_normalize
 
 
@@ -23,6 +23,7 @@ class CascadeU2Net(CascadeBase):
         out_channels: int,
         *,
         lv1_model_ckpt_path: str,
+        lv2_resolution: Optional[int] = None,
         lv2_model_config: Optional[Dict[str, Any]] = None,
         latent_channels: int = 32,
         num_layers: int = 5,
@@ -38,6 +39,7 @@ class CascadeU2Net(CascadeBase):
             max_layers=max_layers,
             lite=lite,
         )
+        self.lv2_resolution = lv2_resolution
         if lv2_model_config is None:
             lv2_model_config = {}
         for k, v in lv1_model_config.items():
@@ -61,8 +63,18 @@ class CascadeU2Net(CascadeBase):
         lv1_outputs = self.lv1_net(batch_idx, batch, state, **kwargs)
         lv1_alpha = torch.sigmoid(lv1_outputs[PREDICTIONS_KEY][0])
         lv1_alpha = imagenet_normalize(lv1_alpha)
-        lv2_input = torch.cat([batch[INPUT_KEY], lv1_alpha], dim=1)
-        return self.lv2_net(batch_idx, {INPUT_KEY: lv2_input}, state, **kwargs)
+        inp = batch[INPUT_KEY]
+        resolution = lv1_alpha.shape[-1]
+        if self.lv2_resolution is not None:
+            inp = align_to(batch[INPUT_KEY], size=self.lv2_resolution)
+            lv1_alpha = align_to(lv1_alpha, size=self.lv2_resolution)
+        lv2_input = torch.cat([inp, lv1_alpha], dim=1)
+        lv2_outputs = self.lv2_net(batch_idx, {INPUT_KEY: lv2_input}, state, **kwargs)
+        if self.lv2_resolution is None:
+            return lv2_outputs
+        lv2_logits = lv2_outputs[PREDICTIONS_KEY]
+        lv2_outputs = [align_to(logits, size=resolution) for logits in lv2_logits]
+        return {PREDICTIONS_KEY: lv2_outputs}
 
     def generate_from(self, net: Tensor, **kwargs: Any) -> Tensor:
         return self.forward(0, {INPUT_KEY: net}, **kwargs)[PREDICTIONS_KEY][0]
