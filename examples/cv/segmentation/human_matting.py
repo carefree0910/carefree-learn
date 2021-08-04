3ac511f24e9a958b798d0454973d083867b5f7c0
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-08-04 17:22:37 +0800

    ✅🚧Enhanced `human_matting`

--- examples/cv/segmentation/human_matting.py
+++ examples/cv/segmentation/human_matting.py
@@ -7,11 +7,12 @@ import cflearn
 import numpy as np
 
 from typing import List
+from cflearn.misc.toolkit import min_max_normalize
 
 
-src_folder = "data"
-tgt_folder = "human_matting"
-label_folder = "human_matting_labels"
+src_folder = "data/human_matting"
+tgt_folder = "data/human_matting_data"
+label_folder = "data/human_matting_labels"
 
 
 def prepare() -> None:
@@ -19,14 +20,15 @@ def prepare() -> None:
         matting_path = os.path.abspath(
             os.path.join(
                 hierarchy[0],
+                hierarchy[1],
                 "matting",
-                hierarchy[2],
-                hierarchy[3].replace("clip", "matting"),
-                hierarchy[4].replace(".jpg", ".png"),
+                hierarchy[3],
+                hierarchy[4].replace("clip", "matting"),
+                hierarchy[5].replace(".jpg", ".png"),
             )
         )
         alpha = cv2.imread(matting_path, cv2.IMREAD_UNCHANGED)[..., -1:]
-        alpha = (alpha > 10).astype(np.int64).transpose([2, 0, 1])
+        alpha = min_max_normalize(alpha.astype(np.float32), global_norm=True)
         file = hierarchy[-1]
         file_id = os.path.splitext(file)[0]
         os.makedirs(label_folder, exist_ok=True)
@@ -38,7 +40,7 @@ def prepare() -> None:
         return label_path
 
     def filter_fn(hierarchy: List[str]) -> bool:
-        return hierarchy[1] == "clip_img"
+        return hierarchy[2] == "clip_img"
 
     cflearn.cv.prepare_image_folder(
         src_folder,
@@ -54,18 +56,20 @@ if __name__ == "__main__":
     prepare()
     train_loader, valid_loader = cflearn.cv.get_image_folder_loaders(
         tgt_folder,
-        batch_size=4,
-        num_workers=0,
-        transform="to_tensor",
+        batch_size=8,
+        num_workers=2,
+        transform="for_salient_object_detection",
     )
+    cflearn.MultiStageLoss.register_(["bce", "iou", "sigmoid_mae"])
     m = cflearn.cv.CarefreePipeline(
-        "unet",
+        "u2net",
         {
             "in_channels": 3,
-            "out_channels": 2,
+            "out_channels": 1,
         },
-        loss_name="cross_entropy",
-        metric_names="acc",
-        tqdm_settings={"use_tqdm": True},
+        loss_name="multi_stage_bce_iou_sigmoid_mae",
+        loss_metrics_weights={"bce0": 0.2, "iou0": 0.4, "sigmoid_mae0": 0.4},
+        callback_names=["u2net", "mlflow"],
+        callback_configs={"mlflow": {"experiment_name": "large_hm"}},
     )
-    m.fit(train_loader, valid_loader, cuda="1")
+    m.fit(train_loader, valid_loader, cuda="0")
