3a3c78b113c22720fcc5a7ee00aeeb2cf5db36f9
carefree0910 <syameimaru.saki@gmail.com>
2021-08-24 21:26:34 +0800

    ✨🚧Implemented `run_ddp`

--- cflearn/api/basic.py
+++ cflearn/api/basic.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+from typing import Any
+from typing import List
+from typing import Union
+
 from ..types import general_config_type
 from .internal_.pipeline import pipeline_dict
 from .internal_.pipeline import PipelineProtocol
@@ -9,6 +16,17 @@ def make(name: str, *, config: general_config_type = None) -> PipelineProtocol:
     return pipeline_dict[name](**config)
 
 
+def run_ddp(path: str, cuda_list: List[Union[int, str]], **kwargs: Any) -> None:
+    def _convert_config() -> str:
+        return " ".join([f"--{k}={v}" for k, v in kwargs.items()])
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, cuda_list))
+    kwargs["nproc_per_node"] = len(cuda_list)
+    prefix = f"{sys.executable} -m torch.distributed.run "
+    os.system(f"{prefix}{_convert_config()} {path}")
+
+
 __all__ = [
     "make",
+    "run_ddp",
 ]
