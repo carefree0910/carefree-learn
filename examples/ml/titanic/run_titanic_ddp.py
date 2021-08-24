c2e901cc9d1e13566d0d1ec4622223026a050b3a
carefree0910 <syameimaru.saki@gmail.com>
2021-08-24 20:45:49 +0800

    🐛🚧Fixed `run_titanic_ddp`

--- examples/ml/titanic/run_titanic_ddp.py
+++ examples/ml/titanic/run_titanic_ddp.py
@@ -1,15 +1,18 @@
 import os
 import cflearn
+import argparse
 
 from cflearn.api.ml import MLData
-from cflearn.api.ml import MLInferenceData
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_rank", type=int)
+    args = parser.parse_args()
+
     file_folder = os.path.dirname(__file__)
     train_file = os.path.join(file_folder, "train.csv")
     test_file = os.path.join(file_folder, "test.csv")
     data = MLData.with_cf_data(train_file, data_config={"label_name": "Survived"})
     m = cflearn.ml.CarefreePipeline()
-    m.ddp(data, cuda_list=[1, 2])
-    m.predict(MLInferenceData(test_file), make_loader_kwargs={"contains_labels": False})
+    m.fit(data, cuda=args.local_rank)
