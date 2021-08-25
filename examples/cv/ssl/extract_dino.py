fbfb24f6f15a9468dba722d151332dfe5c1d52bf
carefree0910 <syameimaru.saki@gmail.com>
2021-08-25 11:39:12 +0800

    ✅Introduced `extract_dino` example

---
+++ examples/cv/ssl/extract_dino.py
@@ -0,0 +1,40 @@
+import os
+
+import json
+import torch
+import cflearn
+
+from PIL import Image
+from typing import Any
+from typing import Dict
+
+
+def states_callback(_: Any, states: Dict[str, Any]) -> Dict[str, Any]:
+    for k in list(states.keys()):
+        if k.startswith("ddp"):
+            states.pop(k)
+    return states
+
+
+base = cflearn.cv.CarefreePipeline
+packed = base.pack(".versions/512_2000")
+dino = base.load(packed, states_callback=states_callback).model
+dino_api = cflearn.cv.DINOPredictor(dino.cuda())
+
+data_folder = "data/poster_data"
+features_folder = ".features"
+Image.MAX_IMAGE_PIXELS = None
+
+train_data_folder = os.path.join(data_folder, "train")
+valid_data_folder = os.path.join(data_folder, "valid")
+
+os.makedirs(features_folder, exist_ok=True)
+kw = dict(batch_size=4, num_workers=2)
+rs = dino_api.get_folder_latent(train_data_folder, **kw)  # type: ignore
+torch.save(rs[0], os.path.join(features_folder, "train.pt"))
+with open(os.path.join(features_folder, "train.json"), "w") as f:
+    json.dump(rs[1], f)
+rs = dino_api.get_folder_latent(valid_data_folder, **kw)  # type: ignore
+torch.save(rs[0], os.path.join(features_folder, "valid.pt"))
+with open(os.path.join(features_folder, "valid.json"), "w") as f:
+    json.dump(rs[1], f)
