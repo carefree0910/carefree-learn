f8fcceb1108d727160fb1673f45ba0cd0ab4887e
carefree0910 <syameimaru.saki@gmail.com>
2021-08-26 11:11:43 +0800

    ✨Enhanced `auto_num_layers`

--- cflearn/models/cv/toolkit.py
+++ cflearn/models/cv/toolkit.py
@@ -9,8 +9,14 @@ def get_latent_resolution(img_size: int, num_layer: int) -> int:
     return int(round(img_size / 2 ** num_layer))
 
 
-def auto_num_layers(img_size: int, min_size: int = 4, target_layers: int = 4) -> int:
+def auto_num_layers(
+    img_size: int,
+    min_size: int = 4,
+    target_layers: Optional[int] = 4,
+) -> int:
     max_layers = int(round(math.log2(img_size / min_size)))
+    if target_layers is None:
+        return max_layers
     return max(2, min(target_layers, max_layers))
 
 
