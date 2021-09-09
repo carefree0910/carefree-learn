71b02b998f5576d1ddc698d6aaea759bf392d15a
carefree0910 <syameimaru.saki@gmail.com>
2021-09-09 21:29:28 +0800

    ✨🚧💥Enhanced `VanillaEncoder`

--- cflearn/models/cv/encoder/vanilla.py
+++ cflearn/models/cv/encoder/vanilla.py
@@ -11,9 +11,7 @@ from ....protocol import TrainerState
 from ....constants import INPUT_KEY
 from ....constants import LATENT_KEY
 from ....misc.toolkit import squeeze
-from ....modules.blocks import _get_clones
 from ....modules.blocks import get_conv_blocks
-from ....modules.blocks import Conv2d
 from ....modules.blocks import ResidualBlock
 
 
@@ -28,30 +26,40 @@ class VanillaEncoder(EncoderBase):
         in_channels: int,
         num_downsample: int,
         latent_channels: int = 128,
+        *,
+        kernel_size: int = 4,
         first_kernel_size: int = 7,
+        start_channels: Optional[int] = None,
         num_residual_blocks: int = 0,
         residual_dropout: float = 0.0,
         residual_kwargs: Optional[Dict[str, Any]] = None,
-        norm_type: str = "instance",
+        norm_type: Optional[str] = "instance",
+        activation: str = "relu",
+        padding: str = "same",
     ):
         super().__init__(img_size, in_channels, num_downsample, latent_channels)
-        # downsample
         self.first_kernel_size = first_kernel_size
-        start_channels = int(round(latent_channels / (2 ** self.num_downsample)))
+        if start_channels is None:
+            start_channels = int(round(latent_channels / (2 ** self.num_downsample)))
         if start_channels <= 0:
             raise ValueError(
                 f"latent_channels ({latent_channels}) is too small "
                 f"for num_downsample ({self.num_downsample})"
             )
+        # in conv
         blocks = get_conv_blocks(
             in_channels,
             start_channels,
             first_kernel_size,
             1,
-            bias=False,
             norm_type=norm_type,
-            activation=nn.LeakyReLU(0.2, inplace=True),
+            activation=activation,
+            padding=padding,
         )
+        # downsample
+        downsample_padding = padding
+        if padding == "reflection":
+            downsample_padding = f"reflection{(kernel_size - 1) // 2}"
         in_nc = start_channels
         for i in range(self.num_downsample):
             is_last = i == self.num_downsample - 1
@@ -59,34 +67,28 @@ class VanillaEncoder(EncoderBase):
                 out_nc = latent_channels
             else:
                 out_nc = min(in_nc * 2, latent_channels)
-            if is_last:
-                blocks.append(
-                    Conv2d(in_nc, out_nc, kernel_size=3, stride=2, bias=False)
+            blocks.extend(
+                get_conv_blocks(
+                    in_nc,
+                    out_nc,
+                    kernel_size,
+                    2,
+                    norm_type=norm_type,
+                    activation=activation,
+                    padding=downsample_padding,
                 )
-            else:
-                blocks.extend(
-                    get_conv_blocks(
-                        in_nc,
-                        out_nc,
-                        3,
-                        2,
-                        bias=False,
-                        norm_type=norm_type,
-                        activation=nn.LeakyReLU(0.2, inplace=True),
-                    )
-                )
+            )
             in_nc = out_nc
         # residual
-        if num_residual_blocks > 0:
-            blocks.extend(
-                _get_clones(
-                    ResidualBlock(
-                        in_nc,
-                        residual_dropout,
-                        **(residual_kwargs or {}),
-                    ),
-                    num_residual_blocks,
-                    return_list=True,
+        for _ in range(num_residual_blocks):
+            blocks.append(
+                ResidualBlock(
+                    latent_channels,
+                    residual_dropout,
+                    norm_type=norm_type,
+                    activation=activation,
+                    padding=padding,
+                    **(residual_kwargs or {}),
                 )
             )
         # construct
@@ -110,11 +112,16 @@ class VanillaEncoder1D(Encoder1DBase):
         in_channels: int,
         num_downsample: int,
         latent_dim: int = 128,
+        *,
+        kernel_size: int = 4,
         first_kernel_size: int = 7,
+        start_channels: Optional[int] = None,
         num_residual_blocks: int = 0,
         residual_dropout: float = 0.0,
         residual_kwargs: Optional[Dict[str, Any]] = None,
-        norm_type: str = "batch",
+        norm_type: Optional[str] = "instance",
+        activation: str = "relu",
+        padding: str = "same",
     ):
         super().__init__(img_size, in_channels, latent_dim)
         self.encoder = VanillaEncoder(
@@ -122,11 +129,15 @@ class VanillaEncoder1D(Encoder1DBase):
             in_channels,
             num_downsample,
             latent_dim,
-            first_kernel_size,
-            num_residual_blocks,
-            residual_dropout,
-            residual_kwargs,
-            norm_type,
+            kernel_size=kernel_size,
+            first_kernel_size=first_kernel_size,
+            start_channels=start_channels,
+            num_residual_blocks=num_residual_blocks,
+            residual_dropout=residual_dropout,
+            residual_kwargs=residual_kwargs,
+            norm_type=norm_type,
+            activation=activation,
+            padding=padding,
         )
         self.pool = nn.AdaptiveAvgPool2d((1, 1))
 
