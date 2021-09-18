eaaf1b62d36d2343b72e384863f1bba4551ac73f
carefree0910 <syameimaru.saki@gmail.com>
2021-09-19 00:34:19 +0800

    🗑️Simplified `TransformForImagenet`

--- cflearn/api/cv/data/transforms/interface.py
+++ cflearn/api/cv/data/transforms/interface.py
@@ -30,7 +30,7 @@ class TransformForGeneration(Compose):
 @Transforms.register("for_imagenet")
 class TransformForImagenet(Compose):
     def __init__(self, img_size: int = 224):  # type: ignore
-        super().__init__([ToArray(), Resize(img_size), ANormalize(), ToTensor()])
+        super().__init__([Resize(img_size), ANormalize(), ToTensor()])
 
 
 @Transforms.register("ssl")
