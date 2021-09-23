628841e01696fc8b245c388b0c02578ce8f0209a
carefree0910 <syameimaru.saki@gmail.com>
2021-09-23 09:56:31 +0800

    ✅Simplified `mnist_vq_vae`

--- examples/cv/vae/mnist_vq_vae.py
+++ examples/cv/vae/mnist_vq_vae.py
@@ -10,10 +10,5 @@ is_ci = check_is_ci()
 num_classes = 10
 data = cflearn.cv.MNISTData(batch_size=4 if is_ci else 64, transform="for_generation")
 
-m = cflearn.api.vq_vae_gray_lite(
-    28,
-    model_config={"num_classes": num_classes},
-    callback_configs={"vq_vae": {"num_classes": num_classes}},
-    debug=is_ci,
-)
+m = cflearn.api.vq_vae_gray_lite(28, num_classes=num_classes, debug=is_ci)
 m.fit(data, cuda=None if is_ci else 1)
