787fea8bec8b03265e9bbb9c4ab02a8d6c5ee190
carefree0910 <syameimaru.saki@gmail.com>
2021-09-27 11:40:10 +0800

    ⚡️🚧Optimized `StyleVAE`

--- cflearn/models/cv/vae/style_gan.py
+++ cflearn/models/cv/vae/style_gan.py
@@ -8,11 +8,10 @@ from typing import Optional
 from ..encoder import Encoder1DBase
 from ..encoder import Encoder1DFromPatches
 from ..protocol import GaussianGeneratorMixin
-from ..generator import StyleGANGenerator
+from ..generator import StyleGANDecoder
 from ....types import tensor_dict_type
 from ....protocol import TrainerState
 from ....protocol import ModelProtocol
-from ....constants import INPUT_KEY
 from ....constants import LABEL_KEY
 from ....constants import PREDICTIONS_KEY
 from ..vae.losses import VAELoss
@@ -26,19 +25,20 @@ class StyleVAE(ModelProtocol, GaussianGeneratorMixin):
     def __init__(
         self,
         img_size: int,
-        latent_dim: int = 256,
+        latent_dim: int = 128,
         in_channels: int = 3,
         *,
+        # encoder
         encoder1d: str = "vanilla",
         encoder1d_config: Optional[Dict[str, Any]] = None,
         num_downsample: int = 4,
-        num_layers: int = 2,
-        channel_base: int = 32768,
-        channel_max: int = 512,
+        # decoder
+        first_channels: int = 64,
+        channel_max: int = 256,
         num_classes: Optional[int] = None,
         conv_clamp: Optional[float] = 256.0,
         block_kwargs: Optional[Dict[str, Any]] = None,
-        mapping_kwargs: Optional[Dict[str, Any]] = None,
+        # loss
         vae_loss_config: Optional[Dict[str, Any]] = None,
     ):
         super().__init__()
@@ -55,17 +55,15 @@ class StyleVAE(ModelProtocol, GaussianGeneratorMixin):
         encoder1d_config["latent_dim"] = latent_dim * 2
         encoder1d_config["num_downsample"] = num_downsample
         self.encoder = Encoder1DBase.make(encoder1d, encoder1d_config)
-        self.decoder = StyleGANGenerator(
+        self.decoder = StyleGANDecoder(
             img_size,
             latent_dim,
             self.out_channels,
-            num_layers=num_layers,
-            channel_base=channel_base,
+            channel_base=img_size * first_channels,
             channel_max=channel_max,
-            num_content_classes=num_classes,
+            num_classes=num_classes,
             conv_clamp=conv_clamp,
-            block_kwargs=block_kwargs,
-            mapping_kwargs=mapping_kwargs,
+            **(block_kwargs or {}),
         )
         self.vae_loss = VAELoss(**(vae_loss_config or {}))
 
@@ -73,11 +71,13 @@ class StyleVAE(ModelProtocol, GaussianGeneratorMixin):
     def can_reconstruct(self) -> bool:
         return True
 
+    def _z2ws(self, z: Tensor) -> Tensor:
+        return z.unsqueeze(1).repeat(1, self.decoder.num_ws, 1)
+
     def decode(self, z: Tensor, *, labels: Optional[Tensor], **kwargs: Any) -> Tensor:
         if labels is None and self.num_classes is not None:
             labels = torch.randint(self.num_classes, [len(z)], device=z.device)
-        rs = self.decoder(0, {INPUT_KEY: z, LABEL_KEY: labels}, **kwargs)
-        net = torch.tanh(rs[PREDICTIONS_KEY])
+        net = self.decoder(self._z2ws(z), labels=labels, **kwargs)
         return net
 
     def forward(
@@ -90,12 +90,7 @@ class StyleVAE(ModelProtocol, GaussianGeneratorMixin):
         net = self.encoder.encode(batch)
         z_mu, z_log_var = net.chunk(2, dim=1)
         z = reparameterize(z_mu, z_log_var)
-        decoder_batch = {INPUT_KEY: z}
-        content_labels = batch.get(LABEL_KEY)
-        if content_labels is not None:
-            decoder_batch[LABEL_KEY] = content_labels
-        net = self.decoder(batch_idx, decoder_batch, state, **kwargs)[PREDICTIONS_KEY]
-        net = torch.tanh(net)
+        net = self.decode(z, labels=batch.get(LABEL_KEY), **kwargs)
         return {PREDICTIONS_KEY: net, MU_KEY: z_mu, LOG_VAR_KEY: z_log_var}
 
 
