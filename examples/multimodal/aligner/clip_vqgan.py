9d464f9d3ce117a320c962c81adb77f35b307010
carefree0910 <syameimaru.saki@gmail.com>
2021-10-08 14:10:41 +0800

    🐛✅Fixed `clip_vqgan` example

--- examples/multimodal/aligner/clip_vqgan.py
+++ examples/multimodal/aligner/clip_vqgan.py
@@ -10,5 +10,6 @@ is_ci = check_is_ci()
 text = "a tree in the garden"
 
 if __name__ == "__main__":
-    m = cflearn.multimodal.CLIPWithVQGANTrainer(text, fixed_steps=1 if is_ci else None)
+    kwargs = {} if not is_ci else {"fixed_steps": 1}
+    m = cflearn.multimodal.CLIPWithVQGANTrainer(text, **kwargs)
     m.run(cuda=None if is_ci else 0)
