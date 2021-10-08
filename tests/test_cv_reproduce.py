fe054299a3ffc46de8513ed34c7c67a65ef93c32
carefree0910 <syameimaru.saki@gmail.com>
2021-10-08 11:13:34 +0800

    ✅Introduced `test_cv_reproduce`

---
+++ tests/test_cv_reproduce.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+
+
+file_folder = os.path.dirname(__file__)
+scripts_folder = os.path.join(file_folder, os.pardir, "examples", "cv", "reproduce")
+
+
+class TestReproduce(unittest.TestCase):
+    def _core(self, sub_folder: str) -> None:
+        folder = os.path.join(scripts_folder, sub_folder)
+        self.assertEqual(
+            os.system(f"python {os.path.join(folder, 'run.py')}"),
+            0,
+        )
+
+    def test_u2net(self) -> None:
+        self._core("u2net")
+
+
+if __name__ == "__main__":
+    unittest.main()
