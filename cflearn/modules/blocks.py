4a99f04ae7780b799933382bb5db7ce2284798ad
carefree0910 <syameimaru.saki@gmail.com>
2021-10-14 15:27:29 +0800

    ✨Implemented `MaxUnpool2d`
    Which is ONNX friendly

--- cflearn/modules/blocks.py
+++ cflearn/modules/blocks.py
@@ -25,8 +25,11 @@ from functools import partial
 from torch.nn import Module
 from torch.nn import ModuleList
 from torch.fft import fft
+from torch.autograd import Function
 from cftool.misc import update_dict
 from cftool.misc import shallow_copy_dict
+from torch.nn.modules.utils import _pair
+from torch.nn.modules.pooling import _MaxUnpoolNd
 
 from ..types import tensor_dict_type
 from ..misc.toolkit import adain_with_params
@@ -2401,6 +2404,114 @@ class AdaptiveAvgPool2d(Module):
         kh = h - (self.h - 1) * sh
         kw = w - (self.w - 1) * sw
         return F.avg_pool2d(net, kernel_size=(kh, kw), stride=(sh, sw))
+
+
+class MaxUnpool2d_op(Function):
+    @staticmethod
+    def forward(ctx: Any, *args: Any, **kwargs: Any) -> Any:
+        return F.max_unpool2d(*args)
+
+    @staticmethod
+    def symbolic(
+        g: Any,
+        net: Any,
+        indices: Any,
+        kernel_size: Any,
+        stride: Any,
+        padding: Any,
+        output_size: Any,
+    ) -> Any:
+        # get shape
+        input_shape = g.op("Shape", net)
+        const_0 = g.op("Constant", value_t=torch.tensor(0))
+        const_1 = g.op("Constant", value_t=torch.tensor(1))
+        batch_size = g.op("Gather", input_shape, const_0, axis_i=0)
+        channel = g.op("Gather", input_shape, const_1, axis_i=0)
+
+        # height = (height - 1) * stride + kernel_size
+        height = g.op(
+            "Gather",
+            input_shape,
+            g.op("Constant", value_t=torch.tensor(2)),
+            axis_i=0,
+        )
+        height = g.op("Sub", height, const_1)
+        height = g.op("Mul", height, g.op("Constant", value_t=torch.tensor(stride[1])))
+        height = g.op(
+            "Add",
+            height,
+            g.op("Constant", value_t=torch.tensor(kernel_size[1])),
+        )
+
+        # width = (width - 1) * stride + kernel_size
+        width = g.op(
+            "Gather",
+            input_shape,
+            g.op("Constant", value_t=torch.tensor(3)),
+            axis_i=0,
+        )
+        width = g.op("Sub", width, const_1)
+        width = g.op("Mul", width, g.op("Constant", value_t=torch.tensor(stride[0])))
+        width = g.op(
+            "Add",
+            width,
+            g.op("Constant", value_t=torch.tensor(kernel_size[0])),
+        )
+
+        # step of channel
+        channel_step = g.op("Mul", height, width)
+        # step of batch
+        batch_step = g.op("Mul", channel_step, channel)
+
+        # channel offset
+        range_channel = g.op("Range", const_0, channel, const_1)
+        range_channel = g.op(
+            "Reshape",
+            range_channel,
+            g.op("Constant", value_t=torch.tensor([1, -1, 1, 1])),
+        )
+        range_channel = g.op("Mul", range_channel, channel_step)
+        range_channel = g.op("Cast", range_channel, to_i=7)  # 7 is int64
+
+        # batch offset
+        range_batch = g.op("Range", const_0, batch_size, const_1)
+        range_batch = g.op(
+            "Reshape",
+            range_batch,
+            g.op("Constant", value_t=torch.tensor([-1, 1, 1, 1])),
+        )
+        range_batch = g.op("Mul", range_batch, batch_step)
+        range_batch = g.op("Cast", range_batch, to_i=7)  # 7 is int64
+
+        # update indices
+        indices = g.op("Add", indices, range_channel)
+        indices = g.op("Add", indices, range_batch)
+
+        return g.op(
+            "MaxUnpool",
+            net,
+            indices,
+            kernel_shape_i=kernel_size,
+            strides_i=stride,
+        )
+
+
+class MaxUnpool2d(_MaxUnpoolNd):
+    def __init__(self, kernel_size: Any, stride: Any = None, padding: Any = 0):
+        super().__init__()
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride or kernel_size)
+        self.padding = _pair(padding)
+
+    def forward(self, net: Tensor, indices: Tensor, output_size: Any = None):
+        return MaxUnpool2d_op.apply(
+            net,
+            indices,
+            self.kernel_size,
+            self.stride,
+            self.padding,
+            output_size,
+        )
 
 
 class Interpolate(Module):
