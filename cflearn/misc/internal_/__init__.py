9b26c869267501e8192d772414dee381a91f23b3
carefree0910 <syameimaru.saki@gmail.com>
2021-10-14 11:14:59 +0800

    ♿️🚧Implemented `register_module`

--- cflearn/misc/internal_/__init__.py
+++ cflearn/misc/internal_/__init__.py
@@ -2,5 +2,6 @@ from .losses import *
 from .data_ts import *
 from .metrics import *
 from .monitors import *
+from .register import *
 from .callbacks import *
 from .inference import *
---
+++ cflearn/misc/internal_/register.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+
+from typing import Any
+from typing import List
+from typing import Type
+from typing import Callable
+from typing import Optional
+
+from ...types import tensor_dict_type
+from ...protocol import TrainerState
+from ...protocol import ModelProtocol
+from ...constants import INPUT_KEY
+from ...constants import PREDICTIONS_KEY
+
+
+def register_module(
+    name: str,
+    *,
+    pre_bases: Optional[List[type]] = None,
+    post_bases: Optional[List[type]] = None,
+) -> Callable[[Type[nn.Module]], Type[nn.Module]]:
+    def _core(m: Type[nn.Module]) -> Type[nn.Module]:
+        @ModelProtocol.register(name)
+        class _(*bases):
+            def __init__(self, *args: Any, **kwargs: Any):
+                super().__init__()
+                self.core = m(*args, **kwargs)
+
+            def forward(
+                self,
+                batch_idx: int,
+                batch: tensor_dict_type,
+                state: Optional[TrainerState] = None,
+                **kwargs: Any,
+            ) -> tensor_dict_type:
+                return {PREDICTIONS_KEY: self.core(batch[INPUT_KEY], **kwargs)}
+
+        return m
+
+    bases = (pre_bases or []) + [ModelProtocol] + (post_bases or [])
+    return _core
+
+
+__all__ = [
+    "register_module",
+]
