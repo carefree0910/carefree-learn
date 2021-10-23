4dbb19ae883944cb0d5c53e68d9b82a40f281daf
carefree0910 <syameimaru.saki@gmail.com>
2021-10-23 23:21:51 +0800

    ✨Introduced `fit_cv`

--- cflearn/api/interface.py
+++ cflearn/api/interface.py
@@ -10,7 +10,6 @@ from typing import Optional
 from cftool.misc import update_dict
 
 from ..models import *
-from ..data import MLData
 from ..types import data_type
 from ..types import tensor_dict_type
 from ..types import general_config_type
@@ -18,11 +17,14 @@ from ..types import sample_weights_type
 from ..types import states_callback_type
 from ..pipeline import DLPipeline
 from ..protocol import ModelProtocol
+from .cv.pipeline import CarefreePipeline as CVCarefree
 from .ml.pipeline import SimplePipeline as MLSimple
 from .ml.pipeline import CarefreePipeline as MLCarefree
 from .ml.interface import repeat_with
 from .ml.interface import RepeatResult
 from .zoo.core import DLZoo
+from ..data.interface import MLData
+from ..data.interface import CVDataModule
 from ..misc.toolkit import inject_debug
 from ..misc.toolkit import _parse_config
 from ..misc.toolkit import download_model
@@ -417,6 +419,89 @@ def repeat_ml(
 
 
 # cv
+
+
+def fit_cv(
+    data: CVDataModule,
+    model_name: str,
+    model_config: Dict[str, Any],
+    *,
+    loss_name: Optional[str] = None,
+    loss_config: Optional[Dict[str, Any]] = None,
+    # trainer
+    state_config: Optional[Dict[str, Any]] = None,
+    num_epoch: int = 40,
+    max_epoch: int = 1000,
+    fixed_epoch: Optional[int] = None,
+    fixed_steps: Optional[int] = None,
+    log_steps: Optional[int] = None,
+    valid_portion: float = 1.0,
+    amp: bool = False,
+    clip_norm: float = 0.0,
+    cudnn_benchmark: bool = False,
+    metric_names: Optional[Union[str, List[str]]] = None,
+    metric_configs: Optional[Dict[str, Any]] = None,
+    use_losses_as_metrics: Optional[bool] = None,
+    loss_metrics_weights: Optional[Dict[str, float]] = None,
+    recompute_train_losses_in_eval: bool = True,
+    monitor_names: Optional[Union[str, List[str]]] = None,
+    monitor_configs: Optional[Dict[str, Any]] = None,
+    callback_names: Optional[Union[str, List[str]]] = None,
+    callback_configs: Optional[Dict[str, Any]] = None,
+    lr: Optional[float] = None,
+    optimizer_name: Optional[str] = None,
+    scheduler_name: Optional[str] = None,
+    optimizer_config: Optional[Dict[str, Any]] = None,
+    scheduler_config: Optional[Dict[str, Any]] = None,
+    optimizer_settings: Optional[Dict[str, Dict[str, Any]]] = None,
+    workplace: str = "_logs",
+    finetune_config: Optional[Dict[str, Any]] = None,
+    tqdm_settings: Optional[Dict[str, Any]] = None,
+    # misc
+    debug: bool = False,
+    # fit
+    sample_weights: sample_weights_type = None,
+    cuda: Optional[Union[int, str]] = None,
+) -> "CVCarefree":
+    pipeline_config = dict(
+        model_name=model_name,
+        model_config=model_config,
+        loss_name=loss_name,
+        loss_config=loss_config,
+        state_config=state_config,
+        num_epoch=num_epoch,
+        max_epoch=max_epoch,
+        fixed_epoch=fixed_epoch,
+        fixed_steps=fixed_steps,
+        log_steps=log_steps,
+        valid_portion=valid_portion,
+        amp=amp,
+        clip_norm=clip_norm,
+        cudnn_benchmark=cudnn_benchmark,
+        metric_names=metric_names,
+        metric_configs=metric_configs,
+        use_losses_as_metrics=use_losses_as_metrics,
+        loss_metrics_weights=loss_metrics_weights,
+        recompute_train_losses_in_eval=recompute_train_losses_in_eval,
+        monitor_names=monitor_names,
+        monitor_configs=monitor_configs,
+        callback_names=callback_names,
+        callback_configs=callback_configs,
+        lr=lr,
+        optimizer_name=optimizer_name,
+        scheduler_name=scheduler_name,
+        optimizer_config=optimizer_config,
+        scheduler_config=scheduler_config,
+        optimizer_settings=optimizer_settings,
+        workplace=workplace,
+        finetune_config=finetune_config,
+        tqdm_settings=tqdm_settings,
+    )
+    if debug:
+        inject_debug(pipeline_config)
+    fit_kwargs = dict(sample_weights=sample_weights, cuda=cuda)
+    return CVCarefree(**pipeline_config).fit(data, **fit_kwargs)  # type: ignore
+
 
 # clf
 
@@ -868,6 +953,7 @@ __all__ = [
     "pack_onnx",
     "fit_ml",
     "repeat_ml",
+    "fit_cv",
     "cct",
     "cct_model",
     "cct_lite",
