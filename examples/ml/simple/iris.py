1b903fbde010fc740ceccbf4bb390891c3de5e14
carefree0910 <syameimaru.saki@gmail.com>
2021-10-23 08:21:50 +0800

    ✅Utilized `cflearn.api` in `iris`

--- examples/ml/simple/iris.py
+++ examples/ml/simple/iris.py
@@ -7,35 +7,32 @@ from cfdata.tabular import TabularDataset
 from cflearn.misc.toolkit import get_latest_workplace
 
 
-base = cflearn.ml.SimplePipeline
 metrics = ["acc", "auc"]
 x, y = TabularDataset.iris().xy
-data = cflearn.MLData(x, y, is_classification=True)
-m = base(output_dim=3, metric_names=metrics).fit(data)
-assert isinstance(m, base)
+m = cflearn.api.fit_ml(x, y, is_classification=True, output_dim=3, metric_names=metrics)
 
 idata = cflearn.MLInferenceData(x, y)
 cflearn.ml.evaluate(idata, metrics=metrics, pipelines=m)
 
 p = m.predict(idata)[cflearn.PREDICTIONS_KEY]
 m.save("iris")
-m2 = base.load("iris")
+m2 = cflearn.api.load("iris")
 assert np.allclose(p, m2.predict(idata)[cflearn.PREDICTIONS_KEY])
 m.to_onnx("iris_onnx")
-m3 = base.from_onnx("iris_onnx")
+m3 = cflearn.ml.SimplePipeline.from_onnx("iris_onnx")
 assert np.allclose(p, m3.predict(idata)[cflearn.PREDICTIONS_KEY], atol=1.0e-4)
 workplace = get_latest_workplace("_logs")
 assert workplace is not None
-packed_path = base.pack(workplace)
-m4 = base.load(packed_path)
+packed_path = cflearn.api.pack(workplace)
+m4 = cflearn.api.load(packed_path)
 assert np.allclose(p, m4.predict(idata)[cflearn.PREDICTIONS_KEY])
 
-m = base(output_dim=3, metric_names=metrics).fit(data)
+m = cflearn.api.fit_ml(x, y, is_classification=True, output_dim=3, metric_names=metrics)
 p2 = m.predict(idata)[cflearn.PREDICTIONS_KEY]
 packed_paths = []
 for stuff in sorted(os.listdir("_logs"))[-2:]:
     folder = os.path.join("_logs", stuff)
-    packed_paths.append(base.pack(folder))
-fused = base.fuse_multiple(packed_paths)
+    packed_paths.append(cflearn.api.pack(folder))
+fused = cflearn.ml.SimplePipeline.fuse_multiple(packed_paths)
 p_fused = fused.predict(idata)[cflearn.PREDICTIONS_KEY]
 assert np.allclose(0.5 * (p + p2), p_fused)
