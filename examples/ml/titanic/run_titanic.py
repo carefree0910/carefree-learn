3f5e8006738c37d2c62cbc94945d308f969443b9
carefree0910 <syameimaru.saki@gmail.com>
2021-10-23 08:29:36 +0800

    ✅Utilized `cflearn.api` in `run_titanic`

--- examples/ml/titanic/run_titanic.py
+++ examples/ml/titanic/run_titanic.py
@@ -6,16 +6,11 @@ import numpy as np
 from cflearn.misc.toolkit import get_latest_workplace
 
 
-base = cflearn.ml.CarefreePipeline
 file_folder = os.path.dirname(__file__)
 train_file = os.path.join(file_folder, "train.csv")
 test_file = os.path.join(file_folder, "test.csv")
-data = cflearn.MLData.with_cf_data(
-    train_file,
-    cf_data_config={"label_name": "Survived"},
-)
-m = base().fit(data)
-assert isinstance(m, base)
+kwargs = dict(carefree=True, cf_data_config={"label_name": "Survived"})
+m = cflearn.api.fit_ml(train_file, **kwargs)
 
 idata = cflearn.MLInferenceData(test_file)
 results = m.predict(idata, make_loader_kwargs={"contains_labels": False})
@@ -23,13 +18,13 @@ predictions = results[cflearn.PREDICTIONS_KEY]
 
 export_folder = "titanic"
 m.save(export_folder)
-m2 = base.load(export_folder)
+m2 = cflearn.api.load(export_folder)
 results = m2.predict(idata, make_loader_kwargs={"contains_labels": False})
 assert np.allclose(predictions, results[cflearn.PREDICTIONS_KEY])
 
 latest = get_latest_workplace("_logs")
 assert latest is not None
-m3 = base.load(base.pack(latest))
+m3 = cflearn.api.load(cflearn.api.pack(latest))
 results = m3.predict(idata, make_loader_kwargs={"contains_labels": False})
 assert np.allclose(predictions, results[cflearn.PREDICTIONS_KEY])
 
