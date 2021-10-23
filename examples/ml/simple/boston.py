10107aeea40a09260e19ff7f5b8453e63a217781
carefree0910 <syameimaru.saki@gmail.com>
2021-10-23 08:23:06 +0800

    ✅Utilized `cflearn.api` in `boston`

--- examples/ml/simple/boston.py
+++ examples/ml/simple/boston.py
@@ -5,21 +5,18 @@ import numpy as np
 from cfdata.tabular import TabularDataset
 
 
-base = cflearn.ml.SimplePipeline
 metrics = ["mae", "mse"]
 x, y = TabularDataset.boston().xy
 y = (y - y.mean()) / y.std()
-data = cflearn.MLData(x, y, is_classification=False)
-m = cflearn.api.make("ml.simple", config={"metric_names": metrics}).fit(data)
-assert isinstance(m, cflearn.ml.SimplePipeline)
+m = cflearn.api.fit_ml(x, y, is_classification=False, metric_names=metrics)
 
 idata = cflearn.MLInferenceData(x, y)
 cflearn.ml.evaluate(idata, metrics=metrics, pipelines=m)
 
 predictions = m.predict(idata)[cflearn.PREDICTIONS_KEY]
 m.save("boston")
-m2 = base.load("boston")
+m2 = cflearn.api.load("boston")
 assert np.allclose(predictions, m2.predict(idata)[cflearn.PREDICTIONS_KEY])
 m.to_onnx("boston_onnx")
-m3 = base.from_onnx("boston_onnx")
+m3 = cflearn.ml.SimplePipeline.from_onnx("boston_onnx")
 assert np.allclose(predictions, m3.predict(idata)[cflearn.PREDICTIONS_KEY], atol=1.0e-4)
