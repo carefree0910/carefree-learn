02b159e765d1cd86d06d7de67eacdf930046628f
carefree0910 <syameimaru.saki@gmail.com>
2021-10-27 16:38:28 +0800

    ✨🚧Implemented `register_loss_module`

--- cflearn/misc/internal_/register.py
+++ cflearn/misc/internal_/register.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 
+from abc import abstractmethod
+from abc import ABCMeta
 from torch import Tensor
 from typing import Any
 from typing import List
@@ -7,9 +9,12 @@ from typing import Type
 from typing import Callable
 from typing import Optional
 
+from ...types import losses_type
 from ...types import tensor_dict_type
+from ...protocol import LossProtocol
 from ...protocol import TrainerState
 from ...protocol import ModelProtocol
+from ...constants import LOSS_KEY
 from ...constants import INPUT_KEY
 from ...constants import PREDICTIONS_KEY
 
@@ -45,6 +50,45 @@ def register_module(
     return _core
 
 
+class LossModule(nn.Module, metaclass=ABCMeta):
+    @abstractmethod
+    def forward(
+        self,
+        forward_results: tensor_dict_type,
+        batch: tensor_dict_type,
+        state: Optional[TrainerState] = None,
+        **kwargs: Any,
+    ) -> losses_type:
+        pass
+
+
+def register_loss_module(name: str) -> Callable[[Type[LossModule]], Type[LossModule]]:
+    def _core(loss_base: Type[LossModule]) -> Type[LossModule]:
+        @LossProtocol.register(name)
+        class _(LossProtocol):  # type: ignore
+            def __init__(self, reduction: str = "mean", **kwargs: Any):
+                super().__init__(reduction, **kwargs)
+                self.core = loss_base(**kwargs)
+
+            def _core(
+                self,
+                forward_results: tensor_dict_type,
+                batch: tensor_dict_type,
+                state: Optional[TrainerState] = None,
+                **kwargs: Any,
+            ) -> losses_type:
+                rs = self.core(forward_results, batch, state, **kwargs)
+                if isinstance(rs, Tensor):
+                    rs = {LOSS_KEY: rs}
+                return rs
+
+        return loss_base
+
+    return _core
+
+
 __all__ = [
     "register_module",
+    "register_loss_module",
+    "LossModule",
 ]
