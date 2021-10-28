1a0bdb154d8ea9b4478882bde62124bfa4b73d63
carefree0910 <syameimaru.saki@gmail.com>
2021-10-28 10:54:44 +0800

    🎨✅Utilized `debug` flag

--- examples/multimodal/aligner/clip_vqgan.py
+++ examples/multimodal/aligner/clip_vqgan.py
@@ -10,6 +10,5 @@ is_ci = check_is_ci()
 text = "a tree in the garden"
 
 if __name__ == "__main__":
-    kwargs = {} if not is_ci else {"fixed_steps": 1}
-    m = cflearn.multimodal.CLIPWithVQGANTrainer(text, **kwargs)
+    m = cflearn.multimodal.CLIPWithVQGANTrainer(text, debug=is_ci)
     m.run(cuda=None if is_ci else 0)
