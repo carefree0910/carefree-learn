8898bf9b26c88ce5c790d84f1290adcfdad07e6e
carefree0910 <syameimaru.saki@gmail.com>
2021-10-29 13:32:32 +0800

    🐛Fixed `setup.py`

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 DESCRIPTION = "Deep Learning with PyTorch made easy"
 with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
@@ -40,5 +40,6 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
+    include_package_data=True,
     keywords="python machine-learning deep-learning solution PyTorch",
 )
