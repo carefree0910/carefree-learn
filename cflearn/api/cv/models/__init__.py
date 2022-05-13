ef235a0e807b0010c376424a35c501a349ee290c
carefree0910 <syameimaru.saki@gmail.com>
2022-05-13 15:48:48 +0800

    ✨🚧Introduced `CLIPImageExtractor`

--- cflearn/api/cv/models/__init__.py
+++ cflearn/api/cv/models/__init__.py
@@ -1,2 +1,3 @@
+from .clip import *
 from .dino import *
 from .vq_vae import *
---
+++ cflearn/api/cv/models/clip.py
@@ -0,0 +1,44 @@
+from torch import Tensor
+from typing import List
+from typing import Tuple
+
+from ..pipeline import SimplePipeline
+from ..interface import predict_folder
+from ....constants import INPUT_KEY
+from ....constants import LATENT_KEY
+from ....misc.toolkit import to_torch
+
+
+class CLIPImageExtractor:
+    def __init__(self, m: SimplePipeline):
+        self.m = m
+        clip = m.model
+        self.img_size = clip.img_size
+        self.transform = clip.get_transform()
+        clip.forward = lambda _, batch, *args, **kwargs: {
+            LATENT_KEY: clip.encode_image(batch[INPUT_KEY]),
+        }
+
+    def get_folder_latent(
+        self,
+        src_folder: str,
+        *,
+        batch_size: int,
+        num_workers: int = 0,
+        use_tqdm: bool = True,
+    ) -> Tuple[Tensor, List[str]]:
+        results = predict_folder(
+            self.m,
+            src_folder,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            transform=self.transform,
+            use_tqdm=use_tqdm,
+        )
+        latent = to_torch(results.outputs[LATENT_KEY])
+        return latent, results.img_paths
+
+
+__all__ = [
+    "CLIPImageExtractor",
+]
