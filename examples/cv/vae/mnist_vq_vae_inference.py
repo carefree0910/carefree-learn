1d173037dd010cbba781d9e9f9c7053753555c52
carefree0910 <syameimaru.saki@gmail.com>
2022-07-26 20:49:14 +0800

    ✅Optimized some `vae` examples

--- examples/cv/vae/mnist_vq_vae_inference.py
+++ examples/cv/vae/mnist_vq_vae_inference.py
@@ -7,8 +7,8 @@ from cflearn.misc.toolkit import check_is_ci
 is_ci = check_is_ci()
 
 data = cflearn.cv.MNISTData(shuffle=False, transform="for_generation")
-workplace = "_logs" if is_ci else "c_vq_vae_inference_v4"
-vqvae_log_folder = get_latest_workplace(workplace) if is_ci else "c_vq_vae_v4"
+workplace = "_logs"
+vqvae_log_folder = get_latest_workplace(workplace)
 config = dict(
     num_codes=16,
     vqvae_log_folder=vqvae_log_folder,  # type: ignore
