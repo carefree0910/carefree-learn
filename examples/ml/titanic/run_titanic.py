9a45e4d33388ff727d5eb1d97071986799613873
carefree0910 <syameimaru.saki@gmail.com>
2022-08-13 14:47:25 +0800

    ✅Enhanced `run_titanic`

--- examples/ml/titanic/run_titanic.py
+++ examples/ml/titanic/run_titanic.py
@@ -25,7 +25,10 @@ assert np.allclose(predictions, results[cflearn.PREDICTIONS_KEY])
 
 latest = get_latest_workplace("_logs")
 assert latest is not None
-m3 = cflearn.api.load(cflearn.api.pack(latest))
+m3: cflearn.ml.MLPipeline = cflearn.api.load(cflearn.api.pack(latest))
 idata = m3.make_inference_data(test_file, contains_labels=False)
 results = m3.predict(idata)
 assert np.allclose(predictions, results[cflearn.PREDICTIONS_KEY])
+results = m3.predict(idata, return_probabilities=True)
+probabilities = results[cflearn.PREDICTIONS_KEY]
+assert np.allclose(probabilities.sum(1), np.ones(probabilities.shape[0]))
