4dd9a3fe00b52c06cb7d686d952f23ed21f67b65
carefree0910 <syameimaru.saki@gmail.com>
2022-08-30 19:35:45 +0800

    ✅Introduced `clip/run_base.py`

---
+++ examples/cv/reproduce/clip/run_base.py
@@ -0,0 +1,38 @@
+import os
+import cflearn
+import requests
+
+import numpy as np
+
+from PIL import Image
+from transformers import CLIPModel
+from transformers import CLIPProcessor
+from cflearn.misc.toolkit import eval_context
+
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+texts = ["a photo of a cat", "a photo of a dog"]
+
+inputs = processor(text=texts, images=image, return_tensors="pt", padding=True)
+
+with eval_context(model):
+    outputs = model(**inputs)
+
+text_embeds = outputs.text_embeds.numpy()
+image_embeds = outputs.image_embeds.numpy()
+
+m = cflearn.api.clip()
+cf_clip = cflearn.multimodal.CLIPExtractor(m)
+os.makedirs(".tmp", exist_ok=True)
+image.save(f".tmp/test.png")
+cf_text_embeds = cf_clip.get_texts_latent(texts)
+cf_image_embeds = cf_clip.get_folder_latent(".tmp", batch_size=1).latent
+
+text_diff = np.abs(text_embeds - cf_text_embeds)
+image_diff = np.abs(image_embeds - cf_image_embeds)
+assert text_diff.max().item() <= 1.0e-6
+assert image_diff.max().item() <= 1.0e-6
--- tests/test_cv_reproduce.py
+++ tests/test_cv_reproduce.py
@@ -1,18 +1,18 @@
 import os
 import unittest
 
+from typing import Optional
+
 
 file_folder = os.path.dirname(__file__)
 scripts_folder = os.path.join(file_folder, os.pardir, "examples", "cv", "reproduce")
 
 
 class TestReproduce(unittest.TestCase):
-    def _core(self, sub_folder: str) -> None:
+    def _core(self, sub_folder: str, name: Optional[str] = None) -> None:
         folder = os.path.join(scripts_folder, sub_folder)
-        self.assertEqual(
-            os.system(f"python {os.path.join(folder, f'run_{sub_folder}.py')}"),
-            0,
-        )
+        file = f"run_{name or sub_folder}.py"
+        self.assertEqual(os.system(f"python {os.path.join(folder, file)}"), 0)
 
     def test_u2net(self) -> None:
         self._core("u2net")
@@ -30,6 +30,7 @@ class TestReproduce(unittest.TestCase):
         except:
             return
         self._core("clip")
+        self._core("clip", name="base")
 
 
 if __name__ == "__main__":
