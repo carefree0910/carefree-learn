bf6caec7a307da077aeeec4f234c72b9994f8bdd
carefree0910 <syameimaru.saki@gmail.com>
2022-09-02 22:00:50 +0800

    ✨Enhanced `blocks/utils`

--- cflearn/modules/blocks/utils.py
+++ cflearn/modules/blocks/utils.py
@@ -1,3 +1,5 @@
+import torch.nn as nn
+
 from torch import Tensor
 from typing import Any
 from torch.nn import Module
@@ -12,6 +14,35 @@ class Residual(Module):
         return net + self.module(net, **kwargs)
 
 
+def zero_module(module: Module) -> Module:
+    for p in module.parameters():
+        p.detach().zero_()
+    return module
+
+
+def conv_nd(n: int, *args: Any, **kwargs: Any) -> Module:
+    if n == 1:
+        return nn.Conv1d(*args, **kwargs)
+    elif n == 2:
+        return nn.Conv2d(*args, **kwargs)
+    elif n == 3:
+        return nn.Conv3d(*args, **kwargs)
+    raise ValueError(f"unsupported dimensions: {n}")
+
+
+def avg_pool_nd(n: int, *args: Any, **kwargs: Any) -> Module:
+    if n == 1:
+        return nn.AvgPool1d(*args, **kwargs)
+    elif n == 2:
+        return nn.AvgPool2d(*args, **kwargs)
+    elif n == 3:
+        return nn.AvgPool3d(*args, **kwargs)
+    raise ValueError(f"unsupported dimensions: {n}")
+
+
 __all__ = [
     "Residual",
+    "conv_nd",
+    "avg_pool_nd",
+    "zero_module",
 ]
