6ec6649540ccbd538d9a48a38a61c03bf4a6fa0b
carefree0910 <syameimaru.saki@gmail.com>
2022-09-02 22:01:14 +0800

    ✨🚧Enhanced `ResidualBlockWithTimeEmbedding`

--- cflearn/modules/blocks/convs/residual.py
+++ cflearn/modules/blocks/convs/residual.py
@@ -1,4 +1,7 @@
+import torch
+
 import torch.nn as nn
+import torch.nn.functional as F
 
 from torch import Tensor
 from typing import Any
@@ -7,8 +10,12 @@ from torch.nn import Module
 from cftool.misc import shallow_copy_dict
 
 from .basic import get_conv_blocks
+from ..utils import conv_nd
+from ..utils import avg_pool_nd
+from ..utils import zero_module
 from ..utils import Residual
 from ..activations import Swish
+from ....misc.toolkit import gradient_checkpoint
 
 
 class ResidualBlock(Module):
@@ -74,31 +81,122 @@ class ResidualBlockV2(Module):
         return self.net(net)
 
 
+class ResDownsample(Module):
+    def __init__(
+        self,
+        in_channels: int,
+        use_conv: bool,
+        *,
+        signal_dim: int = 2,
+        out_channels: Optional[int] = None,
+        padding: int = 1,
+    ):
+        super().__init__()
+        out_channels = out_channels or in_channels
+        stride = 2 if signal_dim != 3 else (1, 2, 2)
+        if not use_conv:
+            if in_channels != out_channels:
+                raise ValueError(
+                    "`in_channels` should be equal to `out_channels` "
+                    "when `use_conv` is set to False"
+                )
+            self.net = avg_pool_nd(signal_dim, kernel_size=stride, stride=stride)
+        else:
+            self.net = conv_nd(
+                signal_dim,
+                in_channels,
+                out_channels,
+                3,
+                stride=stride,
+                padding=padding,
+            )
+
+    def forward(self, net: Tensor) -> Tensor:
+        return self.net(net)
+
+
+class ResUpsample(Module):
+    def __init__(
+        self,
+        in_channels: int,
+        use_conv: bool,
+        *,
+        signal_dim: int = 2,
+        out_channels: Optional[int] = None,
+        padding: int = 1,
+    ):
+        super().__init__()
+        self.signal_dim = signal_dim
+        if not use_conv:
+            self.conv = None
+        else:
+            self.conv = conv_nd(
+                signal_dim,
+                in_channels,
+                out_channels,
+                3,
+                padding=padding,
+            )
+
+    def forward(self, net: Tensor) -> Tensor:
+        if self.signal_dim == 3:
+            _, _, c, h, w = net.shape
+            net = F.interpolate(net, (c, h * 2, w * 2), mode="nearest")
+        else:
+            net = F.interpolate(net, scale_factor=2, mode="nearest")
+        if self.conv is not None:
+            net = self.conv(net)
+        return net
+
+
 class ResidualBlockWithTimeEmbedding(Module):
     def __init__(
         self,
         in_channels: int,
         out_channels: Optional[int] = None,
         *,
+        signal_dim: int = 2,
         dropout: float = 0.0,
         use_conv_shortcut: bool = False,
+        integrate_upsample: bool = False,
+        integrate_downsample: bool = False,
         time_embedding_channels: int = 512,
+        use_scale_shift_norm: bool = False,
+        use_checkpoint: bool = False,
     ):
         super().__init__()
         self.in_channels = in_channels
-        self.out_channels = out_channels or in_channels
+        out_channels = out_channels or in_channels
+        self.out_channels = out_channels
         self.use_conv_shortcut = use_conv_shortcut
+        self.use_scale_shift_norm = use_scale_shift_norm
+        self.use_checkpoint = use_checkpoint
+
+        self.resample = integrate_upsample or integrate_downsample
+        if not self.resample:
+            self.inp_resample = self.net_resample = None
+        elif integrate_upsample:
+            self.inp_resample = ResUpsample(in_channels, False, signal_dim=signal_dim)
+            self.net_resample = ResUpsample(in_channels, False, signal_dim=signal_dim)
+        else:
+            self.inp_resample = ResDownsample(in_channels, False, signal_dim=signal_dim)
+            self.net_resample = ResDownsample(in_channels, False, signal_dim=signal_dim)
 
         make_norm = lambda nc: nn.GroupNorm(num_groups=32, num_channels=nc, eps=1.0e-6)
 
         self.swish = Swish()
         self.norm1 = make_norm(in_channels)
-        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, 1, 1)
+        self.conv1 = conv_nd(signal_dim, in_channels, out_channels, 3, 1, 1)
         if time_embedding_channels > 0:
-            self.time_embedding = nn.Linear(time_embedding_channels, out_channels)
+            if use_scale_shift_norm:
+                time_out_channels = 2 * out_channels
+            else:
+                time_out_channels = out_channels
+            self.time_embedding = nn.Linear(time_embedding_channels, time_out_channels)
         self.norm2 = make_norm(out_channels)
         self.dropout = nn.Dropout(dropout)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, 1, 1)
+        conv2 = conv_nd(signal_dim, out_channels, out_channels, 3, 1, 1)
+        self.conv2 = zero_module(conv2)
         if in_channels != out_channels:
             if use_conv_shortcut:
                 self.shortcut = nn.Conv2d(in_channels, out_channels, 3, 1, 1)
@@ -106,22 +204,44 @@ class ResidualBlockWithTimeEmbedding(Module):
                 self.shortcut = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
 
     def forward(self, net: Tensor, time_net: Optional[Tensor] = None) -> Tensor:
+        return gradient_checkpoint(
+            self._forward,
+            inputs=(net, time_net),
+            params=self.parameters(),
+            enabled=self.use_checkpoint,
+        )
+
+    def _forward(self, net: Tensor, time_net: Optional[Tensor] = None) -> Tensor:
         inp = net
         net = self.norm1(net)
         net = self.swish(net)
-        net = self.conv1(net)
+        if self.inp_resample is None or self.net_resample is None:
+            net = self.conv1(net)
+        else:
+            inp = self.inp_resample(inp)
+            net = self.net_resample(net)
+            net = self.conv1(net)
+        if self.in_channels != self.out_channels:
+            inp = self.shortcut(inp)
 
         if time_net is not None:
             time_net = self.swish(time_net)
-            net = net + self.time_embedding(time_net)[:, :, None, None]
+            time_net = self.time_embedding(time_net)
+            while len(time_net.shape) < len(net.shape):
+                time_net = time_net[..., None]
+            if self.use_scale_shift_norm:
+                scale, shift = torch.chunk(time_net, 2, dim=1)
+                net = self.norm2(net) * (1.0 + scale) + shift
+                net = self.swish(net)
+                net = self.dropout(net)
+                net = self.conv2(net)
+                return inp + net
+            net = net + time_net
 
         net = self.norm2(net)
         net = self.swish(net)
         net = self.dropout(net)
         net = self.conv2(net)
-
-        if self.in_channels != self.out_channels:
-            inp = self.shortcut(inp)
 
         return inp + net
 
