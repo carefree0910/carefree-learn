7ca98de315be0bf23efafeafd8397490cc882b6c
carefree0910 <syameimaru.saki@gmail.com>
2022-09-04 18:57:52 +0800

    ✅Introduced `mnist_ddpm` & `mnist_ldm`

---
+++ examples/cv/diffusion/mnist_ddpm.py
@@ -0,0 +1,27 @@
+# type: ignore
+
+import cflearn
+
+from cflearn.misc.toolkit import check_is_ci
+
+
+is_ci = check_is_ci()
+
+img_size = 32
+batch_size = 4 if is_ci else 16
+data = cflearn.MNISTData(
+    batch_size=batch_size,
+    transform="ae_kl",
+    test_transform="ae_kl_test",
+    transform_config={"img_size": img_size},
+)
+
+m = cflearn.api.ddpm(
+    img_size,
+    model_config=dict(
+        # start_channels=64,
+    ),
+    pretrained=False,
+    debug=is_ci,
+)
+m.fit(data, cuda=None if is_ci else 2)
