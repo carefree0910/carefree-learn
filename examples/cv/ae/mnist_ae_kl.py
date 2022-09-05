ba852d9d4d425ee4872936cb4cac9a172bd859b7
carefree0910 <syameimaru.saki@gmail.com>
2022-09-05 13:03:36 +0800

    ✅Used `ae_kl_f8` in `mnist_ae_kl`

--- examples/cv/ae/mnist_ae_kl.py
+++ examples/cv/ae/mnist_ae_kl.py
@@ -7,7 +7,7 @@ from cflearn.misc.toolkit import check_is_ci
 
 is_ci = check_is_ci()
 
-img_size = 28
+img_size = 32
 batch_size = 4 if is_ci else 16
 data = cflearn.MNISTData(
     batch_size=batch_size,
@@ -16,5 +16,10 @@ data = cflearn.MNISTData(
     transform_config={"img_size": img_size},
 )
 
-m = cflearn.api.ae_kl_f4(img_size, pretrained=False, debug=is_ci)
+m = cflearn.api.ae_kl_f8(
+    img_size,
+    workplace="_kl",
+    pretrained=False,
+    debug=is_ci,
+)
 m.fit(data, cuda=None if is_ci else 1)
