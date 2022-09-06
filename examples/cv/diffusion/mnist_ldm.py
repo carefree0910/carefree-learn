ea6cc070d55af4899eb7a8c15894c3bc8f6dcaeb
carefree0910 <syameimaru.saki@gmail.com>
2022-09-06 09:01:25 +0800

    ✅⚡️Optimized test speed

--- examples/cv/diffusion/mnist_ldm.py
+++ examples/cv/diffusion/mnist_ldm.py
@@ -10,24 +10,27 @@ is_ci = check_is_ci()
 img_size = 256
 latent_size = 32
 batch_size = 4 if is_ci else 16
+
+kw = {}
+model_config = {}
+if is_ci:
+    img_size = 64
+    latent_size = 2
+    kw["latent_in_channels"] = 16
+    kw["latent_out_channels"] = 16
+    model_config["start_channels"] = 32
+    model_config["channel_multipliers"] = (1,)
+    model_config["default_start_T"] = 1
+    model_config["first_stage"] = "ae/kl.f16"
+    first_stage_config = model_config["first_stage_config"] = {}
+    first_stage_config["pretrained"] = False
+
 data = cflearn.MNISTData(
     batch_size=batch_size,
     transform="ae_kl",
     test_transform="ae_kl_test",
     transform_config={"img_size": img_size},
 )
-
-kw = {}
-model_config = {}
-if is_ci:
-    latent_size = 16
-    kw["latent_in_channels"] = 16
-    kw["latent_out_channels"] = 16
-    model_config["start_channels"] = 32
-    model_config["default_start_T"] = 1
-    model_config["first_stage"] = "ae/kl.f16"
-    first_stage_config = model_config["first_stage_config"] = {}
-    first_stage_config["pretrained"] = False
 
 m = cflearn.api.ldm(
     latent_size,
