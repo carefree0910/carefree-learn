f75e3737ad787f8b69ba44108ab5214306727b11
carefree0910 <syameimaru.saki@gmail.com>
2022-11-07 10:04:36 +0800

    📦️Upgraded `VERSION`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-VERSION = "0.3.3.15"
+VERSION = "0.3.3.16"
 DESCRIPTION = "Deep Learning with PyTorch made easy"
 with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
