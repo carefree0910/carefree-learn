63ff0ee139498756e9325845620c53b031abe228
carefree0910 <syameimaru.saki@gmail.com>
2023-03-22 19:40:08 +0800

    ✅Introduced `third_party` examples

---
+++ examples/cv/third_party/run_isnet.py
@@ -0,0 +1,12 @@
+import os
+import cflearn
+
+from PIL import Image
+from cfcv.misc.toolkit import to_uint8
+
+
+file_folder = os.path.dirname(__file__)
+api = cflearn.cv.third_party.ISNetAPI("cpu")
+out = api.segment(Image.open(f"{file_folder}/assets/original.png"))
+out = to_uint8(out)
+Image.fromarray(out).save("out.png")
