3ba353b4066c7370d05208b11512621f8928259b
carefree0910 <syameimaru.saki@gmail.com>
2023-03-22 19:36:29 +0800

    ✨Enhanced `ImageHarmonizationAPI`

--- cflearn/api/cv/third_party/iharm/api.py
+++ cflearn/api/cv/third_party/iharm/api.py
@@ -2,8 +2,12 @@ import torch
 
 import numpy as np
 
+from typing import Union
+from PIL.Image import Image
+
 from .inference.utils import load_model
 from .inference.predictor import Predictor
+from .....misc.toolkit import read_image
 from .....misc.toolkit import download_model
 
 
@@ -18,8 +22,22 @@ class ImageHarmonizationAPI:
         self.predictor.to(device)
 
     # return uint8 image array
-    def run(self, image: np.ndarray, mask: np.ndarray) -> np.ndarray:
-        return self.predictor.predict(image, mask)
+    def run(self, image: Union[str, Image], mask: Union[str, Image]) -> np.ndarray:
+        image_arr = read_image(
+            image,
+            None,
+            anchor=None,
+            normalize=False,
+            to_torch_fmt=False,
+        ).image
+        mask_arr = read_image(
+            mask,
+            None,
+            anchor=None,
+            to_mask=True,
+            to_torch_fmt=False,
+        ).image
+        return self.predictor.predict(image_arr, mask_arr)
 
 
 __all__ = [
