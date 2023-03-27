2f80a7593fb21568841e0d0c09e6ad2f1ed2a27d
carefree0910 <syameimaru.saki@gmail.com>
2023-03-27 13:54:07 +0800

    ✨🚧Introduced `Numpy*` data stuffs

--- cflearn/data/array.py
+++ cflearn/data/array.py
@@ -9,6 +9,34 @@ from ..schema import DataArgs
 from ..misc.toolkit import np_batch_to_tensor
 
 
+# numpy array
+
+
+class NumpyDataset(IArrayDataset):
+    def before_load(self, npd: np_dict_type) -> np_dict_type:
+        return npd
+
+
+class NumpyDictDataset(IArrayDictDataset):
+    def before_load(self, npd: np_dict_type) -> np_dict_type:
+        return npd
+
+
+@IData.register("numpy")
+class NumpyData(IArrayDataMixin, IData):  # type: ignore
+    def get_dataset(self, data_args: DataArgs) -> NumpyDataset:
+        return NumpyDataset(*data_args.xy, self.processor, data_args.others)  # type: ignore
+
+
+@IData.register("numpy_dict")
+class NumpyDictData(IArrayDataMixin, IData):  # type: ignore
+    def get_dataset(self, data_args: DataArgs) -> NumpyDictDataset:
+        return NumpyDictDataset(*data_args.xy, self.processor)  # type: ignore
+
+
+# pytorch tensor
+
+
 class TensorDataset(IArrayDataset):
     def before_load(self, npd: np_dict_type) -> tensor_dict_type:
         return np_batch_to_tensor(npd)
@@ -32,6 +60,10 @@ class TensorDictData(IArrayDataMixin, IData):  # type: ignore
 
 
 __all__ = [
+    "NumpyDataset",
+    "NumpyDictDataset",
+    "NumpyData",
+    "NumpyDictData",
     "TensorDataset",
     "TensorDictDataset",
     "TensorData",
