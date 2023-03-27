38d8c2e60379aa2c55284ee1f8ec3f7dade24a4a
carefree0910 <syameimaru.saki@gmail.com>
2023-03-27 21:08:49 +0800

    🎨Exposed some stuffs

--- cflearn/data/__init__.py
+++ cflearn/data/__init__.py
@@ -2,3 +2,9 @@ from .blocks import *
 from .ml import *
 from .torch import *
 from .array import *
+
+# expose some other stuffs for convenience
+
+from ..schema import DataConfig
+from ..schema import DataProcessor
+from ..schema import DataProcessorConfig
