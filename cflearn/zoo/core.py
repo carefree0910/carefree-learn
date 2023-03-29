082843e7a9ad366b19c2147c3bb3382da172c468
carefree0910 <syameimaru.saki@gmail.com>
2023-03-29 19:29:28 +0800

    🐛✨Extracted `_load_config`

--- cflearn/zoo/core.py
+++ cflearn/zoo/core.py
@@ -6,6 +6,7 @@ from typing import Any
 from typing import Dict
 from typing import List
 from typing import Type
+from typing import Tuple
 from typing import Union
 from typing import Optional
 from typing import NamedTuple
@@ -62,6 +63,8 @@ def _parse_config(json_path: str) -> Dict[str, Any]:
 class DLZoo:
     model_dir = "models_v0.4.x"
 
+    # api
+
     @classmethod
     def load_config(
         cls,
@@ -71,6 +74,73 @@ class DLZoo:
         download_name: Optional[str] = None,
         **kwargs: Any,
     ) -> DLConfig:
+        return cls._load_config(
+            model,
+            json_path=json_path,
+            download_name=download_name,
+            **kwargs,
+        )[0]
+
+    @classmethod
+    def load_pipeline(
+        cls,
+        model: Optional[str] = None,
+        *,
+        data: Optional[IData] = None,
+        states: Optional[tensor_dict_type] = None,
+        pretrained: bool = False,
+        pipeline_type: PipelineTypes = PipelineTypes.DL_INFERENCE,
+        download_name: Optional[str] = None,
+        model_dir: Optional[str] = None,
+        json_path: Optional[str] = None,
+        **kwargs: Any,
+    ) -> TPipeline:
+        states_callback = kwargs.pop("states_callback", None)
+        # get config
+        config, download_name = cls._load_config(
+            model,
+            json_path=json_path,
+            download_name=download_name,
+            **kwargs,
+        )
+        # handle states
+        if states is None and pretrained:
+            if download_name is None:
+                err_msg = f"`{'tag'}` should be provided in '{json_path}' when `pretrained` is True"
+                raise ValueError(err_msg)
+            if model_dir is None:
+                model_dir = cls.model_dir
+            root = os.path.join(OPT.cache_dir, model_dir)
+            states_path = download_model(download_name, root=root)
+            states = torch.load(states_path, map_location="cpu")
+        if states is not None and states_callback is not None:
+            states = states_callback(states)
+        # build
+        m_base: Type[Pipeline] = Pipeline.get(pipeline_type)
+        if issubclass(m_base, DLInferencePipeline):
+            return m_base.build_with(config, states)
+        m = m_base.init(config)
+        if isinstance(m, TrainingPipeline):
+            if states is not None:
+                if data is None:
+                    msg = "`data` needs to be provided when loading `TrainingPipeline` with `states`"
+                    raise ValueError(msg)
+                m.data = data
+                m.prepare()
+                m.build_model.model.load_state_dict(states)
+        return m
+
+    # internal
+
+    @classmethod
+    def _load_config(
+        cls,
+        model: Optional[str] = None,
+        *,
+        json_path: Optional[str] = None,
+        download_name: Optional[str] = None,
+        **kwargs: Any,
+    ) -> Tuple[DLConfig, str]:
         # loader
         if model is not None:
             loader_base = dl_zoo_model_loaders.get(model)
@@ -136,56 +206,8 @@ class DLZoo:
         update_dict(kwargs, raw_config)
         update_dict(increment, raw_config)
         config_type = raw_config.pop("config_type", "dl")
-        return safe_execute(DLConfig.get(config_type), raw_config)
-
-    @classmethod
-    def load_pipeline(
-        cls,
-        model: Optional[str] = None,
-        *,
-        data: Optional[IData] = None,
-        states: Optional[tensor_dict_type] = None,
-        pretrained: bool = False,
-        pipeline_type: PipelineTypes = PipelineTypes.DL_INFERENCE,
-        download_name: Optional[str] = None,
-        model_dir: Optional[str] = None,
-        json_path: Optional[str] = None,
-        **kwargs: Any,
-    ) -> TPipeline:
-        states_callback = kwargs.pop("states_callback", None)
-        # get config
-        config = cls.load_config(
-            model,
-            json_path=json_path,
-            download_name=download_name,
-            **kwargs,
-        )
-        # handle states
-        if states is None and pretrained:
-            if download_name is None:
-                err_msg = f"`{'tag'}` should be provided in '{json_path}' when `pretrained` is True"
-                raise ValueError(err_msg)
-            if model_dir is None:
-                model_dir = cls.model_dir
-            root = os.path.join(OPT.cache_dir, model_dir)
-            states_path = download_model(download_name, root=root)
-            states = torch.load(states_path, map_location="cpu")
-        if states is not None and states_callback is not None:
-            states = states_callback(states)
-        # build
-        m_base: Type[Pipeline] = Pipeline.get(pipeline_type)
-        if issubclass(m_base, DLInferencePipeline):
-            return m_base.build_with(config, states)
-        m = m_base.init(config)
-        if isinstance(m, TrainingPipeline):
-            if states is not None:
-                if data is None:
-                    msg = "`data` needs to be provided when loading `TrainingPipeline` with `states`"
-                    raise ValueError(msg)
-                m.data = data
-                m.prepare()
-                m.build_model.model.load_state_dict(states)
-        return m
+        config = safe_execute(DLConfig.get(config_type), raw_config)
+        return config, download_name
 
 
 __all__ = [
