7711b5e1a2a178b891231c16bd8c1f4e8a13614b
carefree0910 <syameimaru.saki@gmail.com>
2023-12-05 18:09:25 +0800

    ✅Introduced `test_mnist_vae`

--- tests/test_cv_example.py
+++ tests/test_cv_example.py
@@ -14,6 +14,13 @@ class TestExample(unittest.TestCase):
             0,
         )
 
+    def test_mnist_vae(self) -> None:
+        folder = os.path.join(examples_folder, "vae")
+        self.assertEqual(
+            os.system(f"python {os.path.join(folder, 'mnist_vae.py')} --ci 1"),
+            0,
+        )
+
     def test_mnist_gan(self) -> None:
         folder = os.path.join(examples_folder, "gan")
         self.assertEqual(
