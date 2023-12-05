8bc6cb9c45e5745f04c5cfb567305008b3fbea0e
carefree0910 <syameimaru.saki@gmail.com>
2023-12-05 14:04:47 +0800

    ✅Re-introduced `mnist_gan`

---
+++ examples/cv/gan/mnist_gan.py
@@ -0,0 +1,35 @@
+# type: ignore
+
+import cflearn
+
+from cflearn.data.blocks import *
+from cflearn.toolkit import check_is_ci
+
+
+is_ci = check_is_ci()
+
+data_config = cflearn.TorchDataConfig()
+data_config.batch_size = 4 if is_ci else 64
+processor_config = cflearn.DataProcessorConfig()
+processor_config.set_blocks(
+    TupleToBatchBlock(),
+    ToNumpyBlock(),
+    AffineNormalizeBlock(127.5, 127.5),
+    HWCToCHWBlock(),
+)
+data = cflearn.mnist_data(data_config, processor_config)
+
+config = cflearn.DLConfig(
+    model="gan",
+    module_name="vanilla_gan",
+    module_config=dict(
+        img_size=28,
+        in_channels=1,
+        generator_config=dict(final_activation="tanh"),
+    ),
+)
+if is_ci:
+    config.to_debug()
+
+device = None if is_ci else 0
+m = cflearn.DLTrainingPipeline.init(config).fit(data, device=device)
