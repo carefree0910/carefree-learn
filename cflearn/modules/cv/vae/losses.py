248b05192fcf407f2795cb04db43bbfee2c238dc
carefree0910 <syameimaru.saki@gmail.com>
2023-12-06 21:19:26 +0800

    ✨Re-introduced `VQVAELoss`

--- cflearn/modules/cv/vae/losses.py
+++ cflearn/modules/cv/vae/losses.py
@@ -74,8 +74,62 @@ class VAELoss(ILoss):
         return {"mse": mse, "kld": kld_loss, "kld_w": self.kld_w, LOSS_KEY: loss}
 
 
+@register_loss("vq_vae")
+class VQVAELoss(ILoss):
+    def __init__(
+        self,
+        *,
+        lb_vq: float = 1.0,
+        lb_recon: float = 1.0,
+        lb_commit: float = 1.0,
+        loss_type: str = "l2",
+    ):
+        super().__init__()
+        self.lb_vq = lb_vq
+        self.lb_recon = lb_recon
+        self.lb_commit = lb_commit
+        self.loss_type = loss_type
+
+    def get_forward_args(
+        self,
+        forward_results: tensor_dict_type,
+        batch: tensor_dict_type,
+        state: Optional[TrainerState] = None,
+    ) -> Tuple[Any, ...]:
+        return forward_results, batch
+
+    def forward(
+        self,
+        forward_results: tensor_dict_type,
+        batch: tensor_dict_type,
+    ) -> losses_type:
+        # reconstruction loss
+        original = batch[INPUT_KEY]
+        reconstruction = forward_results[PREDICTIONS_KEY]
+        if self.loss_type == "l2":
+            recon = F.mse_loss(reconstruction, original)
+        elif self.loss_type == "l1":
+            recon = F.l1_loss(reconstruction, original)
+        else:
+            raise ValueError(f"unrecognized loss_type '{self.loss_type}' occurred")
+        # vq & commit loss
+        z_e = forward_results["z_e"]
+        z_q_g = forward_results["z_q_g"]
+        vq_loss = F.mse_loss(z_q_g, z_e.detach())
+        commit_loss = F.mse_loss(z_e, z_q_g.detach())
+        codebook_loss = self.lb_vq * vq_loss + self.lb_commit * commit_loss
+        losses = {
+            self.loss_type: recon,
+            "commit": commit_loss,
+            "codebook": codebook_loss,
+        }
+        losses[LOSS_KEY] = self.lb_recon * recon + codebook_loss
+        return losses
+
+
 __all__ = [
     "MU_KEY",
     "LOG_VAR_KEY",
     "VAELoss",
+    "VQVAELoss",
 ]
