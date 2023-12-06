9ba8632b805de2ebf3eb342302f72ffecb8b986c
carefree0910 <syameimaru.saki@gmail.com>
2023-12-06 20:22:02 +0800

    ✨🚧Implemented `IAlbumentationsBlock`

--- cflearn/data/blocks/common.py
+++ cflearn/data/blocks/common.py
@@ -2,11 +2,25 @@ from abc import abstractmethod
 from abc import ABCMeta
 from typing import Any
 from typing import List
+from typing import Tuple
+from typing import Union
+from typing import Optional
 from cftool.types import np_dict_type
 
 from ...schema import DataBundle
 from ...schema import IDataBlock
 from ...constants import INPUT_KEY
+
+try:
+    from albumentations import BasicTransform
+except:
+    BasicTransform = None
+
+
+def get_wh(size: Union[int, List[int]]) -> Tuple[int, int]:
+    if isinstance(size, int):
+        return size, size
+    return tuple(size)  # type: ignore
 
 
 class IRuntimeDataBlock(IDataBlock, metaclass=ABCMeta):
@@ -28,6 +42,26 @@ class IRuntimeDataBlock(IDataBlock, metaclass=ABCMeta):
     @abstractmethod
     def postprocess_item(self, item: Any, for_inference: bool) -> Any:
         """changes can happen inplace"""
+
+
+class IAlbumentationsBlock(IRuntimeDataBlock):
+    fn: Optional[BasicTransform] = None
+
+    @abstractmethod
+    def init_fn(self, for_inference: bool) -> BasicTransform:
+        pass
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        if BasicTransform is None:
+            name = self.__class__.__name__
+            raise ValueError(f"`albumentations` is required for `{name}`")
+        super().__init__(*args, **kwargs)
+
+    def postprocess_item(self, item: np_dict_type, for_inference: bool) -> np_dict_type:
+        if self.fn is None:
+            self.fn = self.init_fn(for_inference)
+        item[INPUT_KEY] = self.fn(image=item[INPUT_KEY])["image"]
+        return item
 
 
 @IRuntimeDataBlock.register("flatten")
