c0d3dda105eeb46fe2776b47636605c3bb267657
carefree0910 <syameimaru.saki@gmail.com>
2023-12-06 20:22:31 +0800

    🐛⚡️`crop` should utilize `IAlbumentationsBlock`

--- cflearn/data/blocks/cv/crop.py
+++ cflearn/data/blocks/cv/crop.py
@@ -1,51 +1,64 @@
-import torch
-
 from typing import List
 from typing import Union
-from torchvision import transforms
-from cftool.types import np_dict_type
 
-from ..common import IRuntimeDataBlock
-from ....constants import INPUT_KEY
+from ..common import get_wh
+from ..common import IAlbumentationsBlock
+
+try:
+    from albumentations import CenterCrop
+    from albumentations import RandomCrop
+except:
+    CenterCrop = RandomCrop = None
 
 
-@IRuntimeDataBlock.register("center_crop")
-class CenterCropBlock(IRuntimeDataBlock):
+@IAlbumentationsBlock.register("center_crop")
+class CenterCropBlock(IAlbumentationsBlock):
     size: Union[int, List[int]]
+    always_apply: bool
+    p: float
 
-    def __init__(self, size: Union[int, List[int]] = 512) -> None:
-        super().__init__(size=size)
+    def __init__(
+        self,
+        size: Union[int, List[int]] = 512,
+        *,
+        always_apply: bool = False,
+        p: float = 1.0
+    ) -> None:
+        super().__init__(size=size, always_apply=always_apply, p=p)
 
     @property
     def fields(self) -> List[str]:
-        return ["size"]
+        return ["size", "always_apply", "p"]
 
-    def postprocess_item(self, item: np_dict_type, for_inference: bool) -> np_dict_type:
-        tensor = torch.from_numpy(item[INPUT_KEY])
-        tensor = transforms.CenterCrop(self.size)(tensor)
-        item[INPUT_KEY] = tensor.numpy()
-        return item
+    def init_fn(self, for_inference: bool) -> CenterCrop:
+        w, h = get_wh(self.size)
+        return CenterCrop(h, w, self.always_apply, self.p)
 
 
-@IRuntimeDataBlock.register("random_crop")
-class RandomCropBlock(IRuntimeDataBlock):
+@IAlbumentationsBlock.register("random_crop")
+class RandomCropBlock(IAlbumentationsBlock):
     size: Union[int, List[int]]
+    always_apply: bool
+    p: float
 
-    def __init__(self, size: Union[int, List[int]] = 512) -> None:
-        super().__init__(size=size)
-        self.center_crop = CenterCropBlock(size)
+    def __init__(
+        self,
+        size: Union[int, List[int]] = 512,
+        *,
+        always_apply: bool = False,
+        p: float = 1.0
+    ) -> None:
+        super().__init__(size=size, always_apply=always_apply, p=p)
 
     @property
     def fields(self) -> List[str]:
-        return ["size"]
+        return ["size", "always_apply", "p"]
 
-    def postprocess_item(self, item: np_dict_type, for_inference: bool) -> np_dict_type:
+    def init_fn(self, for_inference: bool) -> Union[CenterCrop, RandomCrop]:
+        w, h = get_wh(self.size)
         if for_inference:
-            return self.center_crop.postprocess_item(item, True)
-        tensor = torch.from_numpy(item[INPUT_KEY])
-        tensor = transforms.RandomCrop(self.size)(tensor)
-        item[INPUT_KEY] = tensor.numpy()
-        return item
+            return CenterCrop(h, w, self.always_apply, self.p)
+        return RandomCrop(h, w, self.always_apply, self.p)
 
 
 __all__ = [
