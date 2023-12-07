f8a7380d0dc115872fcfe39ad46e135016f7f84d
carefree0910 <syameimaru.saki@gmail.com>
2023-12-07 08:47:18 +0800

    🗑️Cleaned up redundant codes

--- cflearn/modules/cv/vae/vector_quantized.py
+++ cflearn/modules/cv/vae/vector_quantized.py
@@ -6,70 +6,19 @@ from typing import Any
 from typing import Dict
 from typing import Tuple
 from typing import Optional
-from typing import NamedTuple
 from cftool.types import tensor_dict_type
 
 from ..common import build_encoder
 from ..common import build_decoder
 from ..common import get_latent_resolution
 from ..common import DecoderInputs
+from ..common import VQCodebook
 from ...core import ChannelPadding
 from ...common import register_module
 from ....toolkit import get_device
 from ....toolkit import auto_num_layers
 from ....constants import INPUT_KEY
 from ....constants import PREDICTIONS_KEY
-
-
-class VQCodebookOut(NamedTuple):
-    z_e: Tensor
-    z_q: Tensor
-    indices: Tensor
-    z_q_g: Optional[Tensor] = None
-
-    def to_dict(self) -> tensor_dict_type:
-        return self._asdict()
-
-
-class VQCodebook(nn.Module):
-    def __init__(self, num_code: int, code_dimension: int):
-        super().__init__()
-        self.num_code = num_code
-        self.code_dimension = code_dimension
-        self.embedding = nn.Embedding(num_code, code_dimension)
-        span = 1.0 / num_code
-        self.embedding.weight.data.uniform_(-span, span)
-
-    # z_q_g : z_q with embedding gradient
-    def forward(self, z_e: Tensor, *, return_z_q_g: bool = False) -> VQCodebookOut:
-        inp = z_e
-        z_e = z_e.permute(0, 2, 3, 1).contiguous()
-
-        codebook = self.embedding.weight.detach()
-        with torch.no_grad():
-            z_e_flattened = z_e.view(-1, self.code_dimension)
-            distances = (
-                torch.sum(z_e_flattened**2, dim=1, keepdim=True)
-                + torch.sum(codebook**2, dim=1)
-                - 2.0 * torch.einsum("bd,dn->bn", z_e_flattened, codebook.t())
-            )
-            indices = torch.argmin(distances, dim=1)
-
-        z_q_flattened = codebook[indices]
-        z_q = z_q_flattened.view_as(z_e)
-        # VQSTE in one line of code
-        z_q = z_e + (z_q - z_e).detach()
-
-        indices = indices.view(*z_q.shape[:-1])
-        z_q = z_q.permute(0, 3, 1, 2).contiguous()
-
-        if not return_z_q_g:
-            return VQCodebookOut(inp, z_q, indices)
-
-        # z_q with embedding gradient
-        z_q_g_flattened = self.embedding.weight[indices]
-        z_q_g = z_q_g_flattened.view_as(inp)
-        return VQCodebookOut(inp, z_q, indices, z_q_g)
 
 
 @register_module("vq_vae")
