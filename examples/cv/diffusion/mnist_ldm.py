4aa93c26097afbd22e9e327e268eab2a3ad6d332
carefree0910 <syameimaru.saki@gmail.com>
2023-12-11 01:38:13 +0800

    🐛✅Enhanced `ldm` examples

--- examples/cv/diffusion/mnist_ldm.py
+++ examples/cv/diffusion/mnist_ldm.py
@@ -10,12 +10,13 @@ from cflearn.toolkit import check_is_ci
 is_ci = check_is_ci()
 
 
-img_size = 64
+img_size = 32
 data_config = cflearn.TorchDataConfig()
 data_config.batch_size = 4 if is_ci else 16
 processor_config = cflearn.DataProcessorConfig()
 processor_config.set_blocks(
     TupleToBatchBlock(),
+    ToRGBBlock(),
     ToNumpyBlock(),
     ResizeBlock(img_size),
     AffineNormalizeBlock(127.5, 127.5),
@@ -24,25 +25,28 @@ processor_config.set_blocks(
 data = cflearn.mnist_data(data_config, processor_config)
 
 d = load_predefined_config("diffusion/ldm")
+first_stage = "ae/kl.f4"
+first_stage_config = load_predefined_config(first_stage).module_config
+embedding_channels = first_stage_config["embedding_channels"]
 d.model = "ddpm"
-d.module_config["img_size"] = img_size // 4
-d.module_config["in_channels"] = 3
-d.module_config["out_channels"] = 3
+d.module_config["in_channels"] = embedding_channels
+d.module_config["out_channels"] = embedding_channels
 d.module_config["start_channels"] = 32
-d.module_config["attention_downsample_rates"] = [1, 2]
-d.module_config["channel_multipliers"] = [1, 1, 2]
-d.module_config["first_stage"] = "ae/kl.f4"
-first_stage_config = load_predefined_config("ae/kl.f4").module_config
+d.module_config["attention_downsample_rates"] = [1]
+d.module_config["channel_multipliers"] = [1, 2]
+d.module_config["first_stage"] = first_stage
 first_stage_config["img_size"] = img_size
-first_stage_config["in_channels"] = 1
-first_stage_config["out_channels"] = 1
-first_stage_config["inner_channels"] = 32
-first_stage_config["channel_multipliers"] = [1, 1, 2]
+first_stage_config["pretrained"] = True
 d.module_config["first_stage_config"] = first_stage_config
 if is_ci:
     d.to_debug()
+    first_stage_config["pretrained"] = False
+    first_stage_config["inner_channels"] = 32
+    first_stage_config["channel_multipliers"] = [1, 1, 2]
     d.module_config["sampler_config"] = {"default_steps": 1}
 
+num_downsamples = len(first_stage_config["channel_multipliers"]) - 1
+d.module_config["img_size"] = img_size // 2**num_downsamples
 
 device = None if is_ci else 0
 m = cflearn.DLTrainingPipeline.init(d).fit(data, device=device)
