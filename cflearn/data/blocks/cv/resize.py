9dc55dae9555ea206e8808d0659bd73a77f312b7
carefree0910 <syameimaru.saki@gmail.com>
2023-12-12 11:54:20 +0800

    ✨Implemented `AnchoredResizeBlock`

--- cflearn/data/blocks/cv/resize.py
+++ cflearn/data/blocks/cv/resize.py
@@ -1,15 +1,22 @@
 from typing import List
 from typing import Union
+from typing import Optional
+from cftool.cv import restrict_wh
+from cftool.cv import get_suitable_size
+from cftool.types import np_dict_type
 
 from ..common import get_wh
+from ..common import IRuntimeDataBlock
 from ..common import IAlbumentationsBlock
+from ....constants import INPUT_KEY
 
 
 try:
     from cv2 import INTER_LINEAR
     from albumentations import Resize
+    from albumentations.augmentations.geometric.functional import resize
 except:
-    INTER_LINEAR = Resize = None
+    INTER_LINEAR = Resize = resize = None
 
 
 @IAlbumentationsBlock.register("resize")
@@ -33,6 +40,41 @@ class ResizeBlock(IAlbumentationsBlock):
         return Resize(h, w, self.interpolation)
 
 
+@IRuntimeDataBlock.register("anchored_resize")
+class AnchoredResizeBlock(IRuntimeDataBlock):
+    anchor: Optional[int]
+    max_wh: Optional[int]
+    interpolation: int
+
+    def __init__(
+        self,
+        *,
+        anchor: Optional[int] = None,
+        max_wh: Optional[int] = None,
+        interpolation: int = INTER_LINEAR,
+    ) -> None:
+        if resize is None:
+            name = self.__class__.__name__
+            raise ValueError(f"`albumentations` is required for `{name}`")
+        super().__init__(anchor=anchor, max_wh=max_wh, interpolation=interpolation)
+
+    @property
+    def fields(self) -> List[str]:
+        return ["anchor", "max_wh", "interpolation"]
+
+    def postprocess_item(self, item: np_dict_type, for_inference: bool) -> np_dict_type:
+        image = item[INPUT_KEY]
+        h, w = image.shape[-2:]
+        if self.max_wh is not None:
+            h, w = restrict_wh(h, w, self.max_wh)
+        if self.anchor is not None:
+            h, w = map(get_suitable_size, [h, w], [self.anchor, self.anchor])
+        image = resize(image, h, w, self.interpolation)
+        item[INPUT_KEY] = image
+        return item
+
+
 __all__ = [
     "ResizeBlock",
+    "AnchoredResizeBlock",
 ]
