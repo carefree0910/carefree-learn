07a5b01fc7e88db4c09f08400850f25d41122d6f
carefree0910 <syameimaru.saki@gmail.com>
2024-01-06 15:59:03 +0800

    ✨🚧Utilized `IPoolItem`

--- cflearn/api/common.py
+++ cflearn/api/common.py
@@ -1,9 +1,11 @@
 import torch
 
+from typing import Any
 from pathlib import Path
 from torch.nn import Module
 from cftool.types import tensor_dict_type
 from cftool.data_structures import Pool
+from cftool.data_structures import IPoolItem
 from torch.cuda.amp.autocast_mode import autocast
 
 from ..schema import device_type
@@ -13,7 +15,7 @@ from ..toolkit import empty_cuda_cache
 from ..toolkit import get_torch_device
 
 
-class IAPI:
+class IAPI(IPoolItem):
     m: Module
     device: torch.device
     use_amp: bool
@@ -65,6 +67,19 @@ class IAPI:
 
     def empty_cuda_cache(self) -> None:
         empty_cuda_cache(self.device)
+
+    # pool managements
+
+    def load(self, *, device: device_type = None, **kwargs: Any) -> None:
+        if device is not None and torch.cuda.is_available():
+            kwargs.setdefault("use_half", True)
+            self.to(device, **kwargs)
+
+    def unload(self) -> None:
+        device = self.device
+        if not is_cpu(device):
+            self.to("cpu")
+        empty_cuda_cache(device)
 
 
 class Weights(Pool[tensor_dict_type]):
