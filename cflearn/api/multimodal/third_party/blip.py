a409bccb10e91180d0c094392387723ff5c14300
carefree0910 <syameimaru.saki@gmail.com>
2024-01-09 12:58:31 +0800

    ✨Enhanced `BLIPAPI`
    By inheriting `IAPI`.

--- cflearn/api/multimodal/third_party/blip.py
+++ cflearn/api/multimodal/third_party/blip.py
@@ -3,14 +3,24 @@ import torch
 from PIL import Image
 from cftool.cv import to_rgb
 
+from ...common import IAPI
+from ....schema import device_type
+
 try:
     from lavis.models import load_model_and_preprocess
 except:
     load_model_and_preprocess = None
 
 
-class BLIPAPI:
-    def __init__(self, device: str = "cpu", *, use_half: bool = False) -> None:
+class BLIPAPI(IAPI):
+    def __init__(
+        self,
+        device: device_type = None,
+        *,
+        use_amp: bool = False,
+        use_half: bool = False,
+        force_not_lazy: bool = False
+    ):
         if load_model_and_preprocess is None:
             raise RuntimeError("`salesforce-lavis` is needed for `BLIPAPI`")
         kw = dict(
@@ -19,18 +29,20 @@ class BLIPAPI:
             is_eval=True,
             device=device,
         )
-        self.device = device
-        self.model, self.processors, _ = load_model_and_preprocess(**kw)
-
-    def to(self, device: str, *, use_half: bool = False) -> None:
-        self.device = device
-        self.model.to(device)
+        m, self.processors, _ = load_model_and_preprocess(**kw)
+        super().__init__(
+            m,
+            device,
+            use_amp=use_amp,
+            use_half=use_half,
+            force_not_lazy=force_not_lazy,
+        )
 
     @torch.no_grad()
     def caption(self, image: Image.Image) -> str:
         rgb_image = to_rgb(image)
-        net = self.processors["eval"](rgb_image)[None].to(self.device)
-        return self.model.generate(dict(image=net))[0]
+        net = self.processors["eval"](rgb_image)[None].to(self.device, self.dtype)
+        return self.m.generate(dict(image=net))[0]
 
 
 __all__ = [
